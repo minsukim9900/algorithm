5bc8c1f0406109be4b4cccfff361ffb6d4fd2b93
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-27 03:24:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 72 ms, Memory: 11812 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = N; j>i; j--) {
+				sb.append(" ");
+			}
+			for(int j = 0; j<2*i-1; j++) {
+				sb.append("*");
+			}
+			
+			sb.append("\n");
+			
+		}
+		
+		
+		for (int i = N; i > 0; i--) {
+			for (int j=i; j<=N; j++) {
+				sb.append(" ");
+			}
+			for(int j=2; j<2*i-1; j++) {
+				sb.append("*");
+			}
+			
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.close();
+	}
+		
+}
