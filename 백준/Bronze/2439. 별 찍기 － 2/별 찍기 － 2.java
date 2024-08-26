ba526083b02a709325a75da2d4869307f3f38867
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-26 16:57:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 168 ms, Memory: 13348 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i < N+1; i++) {
+			for(int j = 0; j < N-i; j++) {
+				System.out.print(" ");
+				}
+			for(int k = 0; k<i; k++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+			}
+		}
+		
+}
