a9713ce59d88aeb5be3587cc00a14f87ac02512b
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-26 16:29:47 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int Sum = A + B;
+			
+			System.out.println("Case #"+(i+1)+": "+A+" + "+B+" = "+Sum);
+		}
+		
+	}
+}
