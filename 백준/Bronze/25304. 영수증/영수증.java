bba01d9a7a028c0e948cc12c19754b8fd84d384f
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-26 11:30:30 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 13056 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		long X; int N;
+		int sum = 0;
+		X = sc.nextLong();
+		N = sc.nextInt();
+		for(int i = 0; i<N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sum += (a * b);
+		}
+		if ( X == sum) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+}
