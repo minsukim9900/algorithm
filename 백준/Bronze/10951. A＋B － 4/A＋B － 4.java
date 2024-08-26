b0fd5279360b54e4a3954a4d5961179f6221f895
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-26 18:39:38 +0900

    [Bronze V] Title: A+B - 4, Time: 68 ms, Memory: 11760 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str;
+		
+		while((str=br.readLine()) != null) {
+		st = new StringTokenizer(str, " ");
+		int k = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		sb.append(k+m).append("\n");
+		
+		
+		}
+		System.out.print(sb);
+	}
+		
+}
