f43bb91bb80c499f7ec10a4052baff825c2b3623
Kim Minsu <mmsskksw9900@gmail.com>
2024-08-30 01:04:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		char word = br.readLine().charAt(Integer.parseInt(br.readLine())-1);
+		bw.write(word);
+		bw.close();
+	}
+}
