6428b1de6a42a13e3e48f4b78cda9b430a33b114
Kim Minsu <mmsskksw9900@gmail.com>
2024-09-07 19:38:44 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 200 ms, Memory: 16524 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] A = new int[N][M];
+		int[][] B = new int[N][M];
+		int[][] sum = new int[N][M];
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<M; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<M; j++) {
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				sum[i][j] = A[i][j]+B[i][j];
+				System.out.print(sum[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
