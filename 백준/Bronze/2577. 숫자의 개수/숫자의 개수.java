2518507713f249c25d8f265262b4022c47985406
Kim Minsu <mmsskksw9900@gmail.com>
2024-09-15 00:52:44 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,31 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int A = Integer.parseInt(br.readLine());
 		int B = Integer.parseInt(br.readLine());
 		int C = Integer.parseInt(br.readLine());
-		int[] nums = new int [10];
-		String s = String.valueOf(A*B*C);
-		
-		for(int i = 0; i<10; i++) {
-			for(int j =0; j<s.length(); j++) {
-				if(i == s.charAt(j)-'0') {
-					nums[i]++;
-				}
-			}
+
+		int result = A * B * C;
+
+		int[] nums = new int[10];
+
+		while (result != 0) {
+			nums[result % 10]++;
+			result /= 10;
 		}
-		
-		for(int i = 0; i<nums.length; i++) {
-			System.out.println(nums[i]);
+
+		for (int i = 0; i<nums.length; i++) {
+			sb.append(nums[i]).append("\n");
 		}
-		
+
+		System.out.println(sb.toString());
+
 	}
 }
