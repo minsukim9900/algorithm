a85b82d15132d333c459fa043dfe434dbc148f66
Kim Minsu <mmsskksw9900@gmail.com>
2024-09-16 01:48:45 +0900

    [Bronze II] Title: OX퀴즈, Time: 76 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			String quiz = br.readLine();
+			int weight = 1;
+			int sum = 0;
+			for (int j = 0; j < quiz.length(); j++) {
+				if (j > 0 && quiz.charAt(j - 1) == 'O' && quiz.charAt(j) == 'O') {
+					weight++;
+					sum +=weight;
+				}
+				else if(quiz.charAt(j)=='O') {
+					weight = 1;
+					sum+=weight;
+				}
+				else {
+					weight = 1;
+				}
+			}
+			System.out.println(sum);
+		}
+	}
+
+}
