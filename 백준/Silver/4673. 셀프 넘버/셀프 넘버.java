5cd9298c556f6cef2e33698337917dfe50071f1b
Kim Minsu <mmsskksw9900@gmail.com>
2024-09-19 17:07:15 +0900

    [Silver V] Title: 셀프 넘버, Time: 100 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		
+		int[] count = new int[10001];
+		for(int i = 1; i<=10000; i++) {
+			int n = d(i);
+			
+			if(n<10001) {
+				count[n]=1;
+			}
+			if(count[i]==0) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static int d(int num) {
+		int sum = num;
+		
+		while(num != 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		
+		return sum;
+	}
+}
