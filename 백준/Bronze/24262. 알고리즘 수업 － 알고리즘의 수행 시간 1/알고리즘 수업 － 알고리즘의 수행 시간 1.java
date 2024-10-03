4a4b0d5c4d293f2736919f7c5261f68e21c1cfb6
Kim Minsu <mmsskksw9900@gmail.com>
2024-10-04 00:59:26 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(1).append("\n");
+		sb.append(0).append("\n");
+		System.out.println(sb.toString());
+		
+		
+	}
+}
