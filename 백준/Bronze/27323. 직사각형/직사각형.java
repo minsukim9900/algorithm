31b1de64da1df5e9571ae90cdde0a4bdb7dca8f2
Kim Minsu <mmsskksw9900@gmail.com>
2024-10-04 01:01:07 +0900

    [Bronze V] Title: 직사각형, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(a*b);
+	}
+}
