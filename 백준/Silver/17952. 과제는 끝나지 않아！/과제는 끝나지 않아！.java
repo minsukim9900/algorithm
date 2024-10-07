89e61c3ec2c0e10452ac4a2333306c0805506a45
Kim Minsu <mmsskksw9900@gmail.com>
2024-10-07 15:05:24 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 916 ms, Memory: 213348 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -4,55 +4,56 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	
+	private static Stack<Integer> score = new Stack<>();
+	private static Stack<Integer> time = new Stack<>();
+	private static int totalScore = 0;
+	
+	static void homework(int process, int subject_score, int subject_time) {
+		
+		if (process == 1) {
+			int subjectScore = subject_score;
+			int subjectTime = subject_time - 1;
+			if (subjectTime == 0) {
+				totalScore += subjectScore;
+			} else {
+				score.push(subjectScore);
+				time.push(subjectTime);
+			}
+		} else if (process == 0) {
+			if (score.isEmpty()) {
+				return;
+			} else {
+				int tmp = time.pop() - 1;
+				if (tmp == 0) {
+					totalScore += score.pop();
+				} else {
+					time.push(tmp);
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		Stack<Integer> score = new Stack<>();
-		Stack<Integer> time = new Stack<>();
-
-		int homework = 0;
-		int totalScore = 0;
 		int N = Integer.parseInt(br.readLine());
-		int totalTime = N;
 
-		out: for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			// 과제 여부 확인 ( 1이면 과제가 있고, 0이면 과제가 들어오지 않음)
 			int process = Integer.parseInt(st.nextToken());
-			if (process == 1) {
-				int subjectScore = Integer.parseInt(st.nextToken());
-				int subjectTime = Integer.parseInt(st.nextToken()) - 1;
-				if(subjectTime == 0) {
-					totalScore += subjectScore;
-				}
-				else {
-					score.push(subjectScore);
-					time.push(subjectTime);
-				}
-				totalTime--;
-			} else if (process == 0) {
-				if(score.isEmpty()) {
-					continue;
-				}
-				else {
-					int tmp = time.pop();
-					tmp--;
-					if(totalTime >=0  && tmp == 0) {
-						totalScore += score.pop();
-					}
-					else {
-						time.push(tmp);
-					}
-					totalTime--;
-				}
+			int subjectScore = 0;
+			int subjectTime = 0;
+			if(process == 1) {
+			subjectScore = Integer.parseInt(st.nextToken());
+			subjectTime = Integer.parseInt(st.nextToken());
 			}
+			homework(process, subjectScore, subjectTime);
 
 		}
 
-		// System.out.println(score + " " + time);
 		System.out.println(totalScore);
 
 	}
