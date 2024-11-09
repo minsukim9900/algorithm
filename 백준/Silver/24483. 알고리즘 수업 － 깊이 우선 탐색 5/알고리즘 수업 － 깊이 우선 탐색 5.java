955b9d139a067a6453f9c9569ea9049132e27f52
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-09 22:36:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 848 ms, Memory: 90516 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int N, M, R;
+	private static long[] depth;
+	private static long[] visited;
+	private static List<Integer>[] adj;
+	private static int cnt = 0;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		depth = new long[N+1];
+		visited = new long[N+1];
+		
+		adj = new ArrayList[N+1];
+		for(int i = 1; i<=N; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			adj[from].add(to);
+			adj[to].add(from);
+		}
+		
+		for(int i = 1; i<=N; i++) {
+			Collections.sort(adj[i]);
+		}
+		
+		dfs(R,0);
+		
+		long sum = 0;
+		for(int i = 1; i<=N; i++) {
+			long tmp = depth[i] * visited[i];
+			sum += tmp;
+		}
+		
+		System.out.println(sum);
+
+	}
+	
+	public static void dfs(int v, long d) {
+		depth[v] = d;
+		visited[v] = ++cnt;
+		
+		for(int w : adj[v]) {
+			if( w != R && visited[w] == 0) {
+				dfs(w,d+1);
+			}
+		}
+	}
+
+}
