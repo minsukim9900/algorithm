3c2fd9e112a49cb32b0c08a6d5437d9c12565e3a
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-11 12:51:44 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 64 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static boolean check;
+	private static char[] duple;
+	private static StringBuilder sb = new StringBuilder();
+	private static String str;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			str = br.readLine();
+			if (str.equals("end")) {
+				break;
+			} else {
+				check(str);
+			}
+		}
+		
+
+	}
+
+	public static void check(String str) {
+		check = false;
+		duple = new char[1];
+		int cntC = 0; // 자음 갯수
+		int cntG = 0; // 모음 갯수
+		int n = str.length();
+		for (int i = 0; i < n; i++) {
+			char c = str.charAt(i);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+				check = true;
+				cntG++;
+				cntC = 0;
+			} else {
+				cntC++;
+				cntG = 0;
+			}
+			if(duple[0] != c || (duple[0] == 'e' && c =='e') || (duple[0] == 'o' && c =='o')) {
+				duple[0] = c;
+			}else {
+				check = false;
+				output(str);
+				return;
+			}
+			
+			if(cntC >= 3 || cntG >=3) {
+				check = false;
+				output(str);
+				return;
+			}
+		}
+		output(str);
+	}
+	
+	public static void output(String str) {
+		if(check) {
+			System.out.println("<"+str+"> is acceptable.");
+		}else {
+			System.out.println("<"+str+"> is not acceptable.");
+		}
+	}
+}
