79e8f323c39d6b10770113633ed62bd0b1f67522
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-12 20:41:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 868 ms, Memory: 92668 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	private static int N, M, R;
+	private static List<Integer>[] adj;
+	private static int[] visited;
+	private static int cnt = 0;
+
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		adj = new ArrayList[N+1];
+		visited = new int[N+1];
+		for(int i = 1; i<=N; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			adj[from].add(to);
+			adj[to].add(from);
+		}
+		
+		for(int i = 1;i<=N; i++) {
+			Collections.sort(adj[i]);
+		}
+		
+		bfs(R);
+		
+		for(int i = 1; i<= N; i++) {
+			sb.append(visited[i]).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	public static void bfs(int R) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(R);
+		visited[R] = ++cnt;
+		
+		while(!q.isEmpty()) {
+			int curr = q.poll();
+			for(int w : adj[curr]) {
+				if(visited[w] == 0) {
+					q.add(w);
+					visited[w] = ++cnt;
+				}
+			}
+		}
+		
+	}
+}
