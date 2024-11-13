2d9faa148f85b973d352deb6c0db4ddf43994367
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-13 22:15:08 +0900

    [Gold V] Title: 토마토, Time: 516 ms, Memory: 109944 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,7 +6,6 @@ public class Main {
 	private static int N, M;
 	private static int[][] tomato;
 	private static boolean[][] check;
-	private static boolean result = true;
 	private static int[] dr = { -1, 1, 0, 0 };
 	private static int[] dc = { 0, 0, -1, 1 };
 	private static Queue<int[]> q = new ArrayDeque<>();
@@ -27,43 +26,38 @@ public class Main {
 				if (tomato[r][c] == 1) {
 					q.add(new int[] { r, c, 0 });
 					check[r][c] = true;
-				}
-				else if(tomato[r][c] == -1 ) check[r][c] = true;
-					
+				} else if (tomato[r][c] == -1)
+					check[r][c] = true;
+
 			}
 		}
-		
+
 		bfs();
-		out: for(int r = 0; r<M; r++) {
-			for(int c = 0; c<N; c++) {
-				if(!check[r][c]) {
-					result = false;
-					break out;
+		for (int r = 0; r < M; r++) {
+			for (int c = 0; c < N; c++) {
+				if (!check[r][c]) {
+					System.out.println(-1);
+					return;
 				}
 			}
 		}
-		
-		if(!result) {
-			System.out.println(-1);
-		}else {
-			System.out.println(max);
-		}
+
+		System.out.println(max);
 
 	}
 
 	private static void bfs() {
 		while (!q.isEmpty()) {
 			int curr[] = q.poll();
-			if(max < curr[2]) {
+			if (max < curr[2]) {
 				max = curr[2];
 			}
 			for (int i = 0; i < 4; i++) {
 				int nr = curr[0] + dr[i];
 				int nc = curr[1] + dc[i];
-				if (nr >= 0 && nr < M && nc >= 0 && nc < N &&
-						tomato[nr][nc] == 0 && !check[nr][nc]) {
+				if (nr >= 0 && nr < M && nc >= 0 && nc < N && tomato[nr][nc] == 0 && !check[nr][nc]) {
 					check[nr][nc] = true;
-					q.add(new int[] { nr, nc, curr[2]+1});
+					q.add(new int[] { nr, nc, curr[2] + 1 });
 				}
 			}
 		}
