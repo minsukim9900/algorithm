d6d8eef78d814141de8034e3a0851d5c1c2aaae7
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-13 22:18:09 +0900

    [Gold V] Title: 토마토, Time: 468 ms, Memory: 109196 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,7 +5,6 @@ public class Main {
 
 	private static int N, M;
 	private static int[][] tomato;
-	private static boolean[][] check;
 	private static int[] dr = { -1, 1, 0, 0 };
 	private static int[] dc = { 0, 0, -1, 1 };
 	private static Queue<int[]> q = new ArrayDeque<>();
@@ -17,7 +16,6 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		tomato = new int[M][N];
-		check = new boolean[M][N];
 
 		for (int r = 0; r < M; r++) {
 			st = new StringTokenizer(br.readLine());
@@ -25,17 +23,14 @@ public class Main {
 				tomato[r][c] = Integer.parseInt(st.nextToken());
 				if (tomato[r][c] == 1) {
 					q.add(new int[] { r, c, 0 });
-					check[r][c] = true;
-				} else if (tomato[r][c] == -1)
-					check[r][c] = true;
-
+				}
 			}
 		}
 
 		bfs();
 		for (int r = 0; r < M; r++) {
 			for (int c = 0; c < N; c++) {
-				if (!check[r][c]) {
+				if (tomato[r][c] == 0) {
 					System.out.println(-1);
 					return;
 				}
@@ -55,8 +50,8 @@ public class Main {
 			for (int i = 0; i < 4; i++) {
 				int nr = curr[0] + dr[i];
 				int nc = curr[1] + dc[i];
-				if (nr >= 0 && nr < M && nc >= 0 && nc < N && tomato[nr][nc] == 0 && !check[nr][nc]) {
-					check[nr][nc] = true;
+				if (nr >= 0 && nr < M && nc >= 0 && nc < N && tomato[nr][nc] == 0) {
+					tomato[nr][nc] = 1;
 					q.add(new int[] { nr, nc, curr[2] + 1 });
 				}
 			}
