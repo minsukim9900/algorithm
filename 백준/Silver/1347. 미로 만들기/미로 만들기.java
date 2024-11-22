aa0b940b5d2c330e427f71e0e26a53089eb6d9b7
Kim Minsu <mmsskksw9900@gmail.com>
2024-11-23 03:06:11 +0900

    [Silver II] Title: 미로 만들기, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int[] dr = { -1, 0, 1, 0 };
+	private static int[] dc = { 0, -1, 0, 1 };
+	private static int currR = 50;
+	private static int currC = 50;
+	private static char[][] maze = new char[101][101];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		maze[currR][currC] = '.';
+		String order = br.readLine();
+		
+		int dir = 2;
+		int[] max = {currR, currC};
+		int[] min = {currR, currC};
+		for(int i = 0; i<N; i++) {
+			char commit =  order.charAt(i);
+			if(commit == 'R') {
+				dir = (4+(dir-1)) % 4;
+			}else if(commit == 'L') {
+				dir = (4 + (dir+1)) % 4;
+			}else {
+				move(dir, max, min);
+			}
+		}
+		
+		
+		for(int r = min[0]; r<=max[0]; r++) {
+			for(int c = min[1]; c<=max[1]; c++) {
+				if(maze[r][c] =='.') continue;
+				else maze[r][c] = '#';
+			}
+		}
+		
+		for(int r = min[0]; r<=max[0]; r++) {
+			for(int c = min[1]; c<=max[1]; c++) {
+				sb.append(maze[r][c]);
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	public static void move(int dir, int[] max, int[] min) {
+		currR = currR + dr[dir];
+		currC = currC + dc[dir];
+		if(max[0] < currR) {
+			max[0] = currR;
+		}
+		if(max[1] < currC) {
+			max[1] = currC;
+		}
+		if(min[0] > currR) {
+			min[0] = currR;
+		}
+		if(min[1] > currC) {
+			min[1] = currC;
+		}
+		maze[currR][currC] = '.';
+	}
+	
+}
