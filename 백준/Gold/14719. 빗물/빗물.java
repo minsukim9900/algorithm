27e631f0c2d854c978f65aea34e073922ed64916
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-08 01:16:07 +0900

    [Gold V] Title: 빗물, Time: 72 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int H, W, max_idx;
+	private static int[] room;
+	private static int result = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		H = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+
+		room = new int[W];
+
+		st = new StringTokenizer(br.readLine());
+
+		int max = 0;
+		max_idx = 0;
+
+		for (int i = 0; i < W; i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			if (max < tmp) {
+				max = tmp;
+				max_idx = i;
+			}
+			room[i] = tmp;
+		}
+		
+		leftSide();
+		rightSide();
+		System.out.println(result);
+
+	}
+
+	private static void leftSide() {
+		int idx = 0;
+		int max = 0;
+		while (idx < max_idx) {
+			if (max < room[idx])
+				max = room[idx];
+			result += (max - room[idx]);
+			idx++;
+		}
+	}
+
+	private static void rightSide() {
+		int idx = W - 1;
+		int max = 0;
+		while (idx > max_idx) {
+			if (max < room[idx])
+				max = room[idx];
+			result += (max - room[idx]);
+			idx--;
+		}
+	}
+}
