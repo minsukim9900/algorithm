d979d81a1bb6b22fbec1a2ebaeff1d1bed16caa7
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-08 21:05:31 +0900

    [Silver I] Title: 트럭, Time: 76 ms, Memory: 11892 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,59 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+	private static int N, W, L;
+	private static int[] trucks;
+	private static int count = 0;
+	private static Queue<Integer> bridge = new ArrayDeque<>();
+
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 다리를 건너는 트럭 수
-		int w = Integer.parseInt(st.nextToken()); // 다리의 길이
-		int L = Integer.parseInt(st.nextToken()); // 다리의 최대하중
+		N = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
 
-		// 다리 만들기
-		Queue<Integer> bridge = new ArrayDeque<>();
-		for (int i = 0; i < w; i++) {
-			bridge.add(0);
-		}
+		trucks = new int[N];
 
 		st = new StringTokenizer(br.readLine());
 
-		// 배열에 트럭 넣기
-		int[] trucks = new int[n];
-		for (int i = 0; i < n; i++) {
-			trucks[i] = Integer.parseInt(st.nextToken());
+		for (int t = 0; t < N; t++) {
+			trucks[t] = Integer.parseInt(st.nextToken());
 		}
 
-		int truck = 0; // 트럭의 인덱스 번호
-		int loadWeight = 0; // 다리에 올라온 트럭 하중
-		int time = 0;
-		while (truck < n) {
-			
-			// 앞에 있는 값 빼 loadWeight에서도 빼
-			loadWeight -= bridge.poll();
-			time++;
-			if(loadWeight + trucks[truck] >L) {
-				bridge.add(0);
-			}
-			else {
-				bridge.add(trucks[truck]);
-				loadWeight += trucks[truck];
-				truck++;
-			}
-			
-			
-			
-			if(truck >=n) {
-				break;
-			}
+		for (int w = 0; w < W; w++) {
+			bridge.add(0);
 		}
-		time += w;
-		System.out.println(time);
+		
+		cross();
+		
+		System.out.println(count);
+
 	}
+
+	private static void cross() {
+		int t = 0;
+		int weight = 0;
+
+		while (t < N) {
+			weight -= bridge.poll();
+			count++;
+			if (weight + trucks[t] > L) {
+				bridge.add(0);
+			} else {
+				bridge.add(trucks[t]);
+				weight += trucks[t];
+				t++;
+			}
+
+		}
+		
+		count+= W;
+
+	}
+
 }
