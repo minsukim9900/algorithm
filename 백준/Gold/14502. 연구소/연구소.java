f5b40fe6852e6e3ee11bb055ca647c79628d87c6
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-14 21:39:10 +0900

    [Gold IV] Title: 연구소, Time: 292 ms, Memory: 96640 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -7,8 +7,9 @@ public class Main {
 	private static int[] dx = { -1, 1, 0, 0 };
 	private static int[] dy = { 0, 0, -1, 1 };
 	private static int[][] lab;
-	private static Queue<int[]> virus = new ArrayDeque<>();
-	private static int[][] realviruse;
+	private static ArrayList<int[]> virus = new ArrayList<>();
+	private static ArrayList<int[]> wall = new ArrayList<>();
+	
 	private static int max = Integer.MIN_VALUE;
 
 	public static void main(String[] args) throws IOException {
@@ -30,45 +31,36 @@ public class Main {
 
 				if (state == 2) {
 					virus.add(new int[] { n, m });
+				}else if(state == 0) {
+					wall.add(new int[] {n, m});
 				}
 				lab[n][m] = state;
 			}
 		}
 		
 		
-		int size = virus.size();
-		realviruse = new int[size][2];
-		
-		for(int x = 0; x<size; x++) {
-			int[] tmp = virus.poll();
-			realviruse[x] = tmp;
-		}
 
-		bruteforce(0);
+		bruteforce(0, 0);
 		System.out.println(max);
 
 	}
 
-	private static void bruteforce(int depth) {
+	private static void bruteforce(int num, int depth) {
 
 		if (depth == 3) {
 
 			infectViruse();
 
 		} else {
-			for (int r = 0; r < N; r++) {
-
-				for (int c = 0; c < M; c++) {
-
-					if (lab[r][c] == 0) {
-						
-						lab[r][c] = 1;
-						bruteforce(depth + 1);
-						lab[r][c] = 0;
-					}
-
-				}
+			
+			for(int i = num; i<wall.size(); i++) {
+				int[] curr = wall.get(i);
+				lab[curr[0]][curr[1]] = 1;
+				bruteforce(i+1, depth+1);
+				lab[curr[0]][curr[1]] = 0;
+				
 			}
+			
 		}
 
 	}
@@ -83,8 +75,8 @@ public class Main {
 
 		Queue<int[]> copyVirus = new ArrayDeque<>();
 		
-		for(int r = 0; r<realviruse.length; r++) {
-			int[] tmp = realviruse[r];
+		for(int r = 0; r<virus.size(); r++) {
+			int[] tmp = virus.get(r);
 			copyVirus.add(tmp);
 		}
 		
