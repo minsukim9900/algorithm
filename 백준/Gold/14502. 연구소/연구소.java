a446e6960c39bd4cbe56a5cca3ee8dd33fa8cdda
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-14 21:31:39 +0900

    [Gold IV] Title: 연구소, Time: 668 ms, Memory: 295016 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -8,6 +8,7 @@ public class Main {
 	private static int[] dy = { 0, 0, -1, 1 };
 	private static int[][] lab;
 	private static Queue<int[]> virus = new ArrayDeque<>();
+	private static int[][] realviruse;
 	private static int max = Integer.MIN_VALUE;
 
 	public static void main(String[] args) throws IOException {
@@ -33,55 +34,38 @@ public class Main {
 				lab[n][m] = state;
 			}
 		}
+		
+		
+		int size = virus.size();
+		realviruse = new int[size][2];
+		
+		for(int x = 0; x<size; x++) {
+			int[] tmp = virus.poll();
+			realviruse[x] = tmp;
+		}
 
-		dfs(0);
+		bruteforce(0);
 		System.out.println(max);
 
 	}
 
-	private static void dfs(int depth) {
+	private static void bruteforce(int depth) {
 
 		if (depth == 3) {
-			int[][] cloneLab = new int[N][M];
 
-			for (int i = 0; i < N; i++) {
-				cloneLab[i] = lab[i].clone();
-			}
-
-			Queue<int[]> copyVirus = new ArrayDeque<>();
-			
-			int size = virus.size();
-			for (int i = 0; i < size; i++) {
-				int[] tmp = virus.poll();
-				copyVirus.add(new int[] {tmp[0], tmp[1]});
-				virus.add(tmp);
-			}
-
-			bfs(cloneLab, copyVirus);
-			int cnt = 0;
-
-			for(int r = 0; r<N; r++) {
-				
-				for(int c = 0; c<M; c++) {
-					if(cloneLab[r][c] == 0) {
-						cnt++;
-					}
-				}
-				
-			}
-			max = Math.max(max, cnt);
+			infectViruse();
 
 		} else {
 			for (int r = 0; r < N; r++) {
 
 				for (int c = 0; c < M; c++) {
 
-					if (lab[r][c] == 1 || lab[r][c] == 2) {
-						continue;
+					if (lab[r][c] == 0) {
+						
+						lab[r][c] = 1;
+						bruteforce(depth + 1);
+						lab[r][c] = 0;
 					}
-					lab[r][c] = 1;
-					dfs(depth + 1);
-					lab[r][c] = 0;
 
 				}
 			}
@@ -89,7 +73,22 @@ public class Main {
 
 	}
 
-	private static void bfs(int[][] cloneLab, Queue<int[]> copyVirus) {
+	private static void infectViruse() {
+		
+		int[][] cloneLab = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			cloneLab[i] = lab[i].clone();
+		}
+
+		Queue<int[]> copyVirus = new ArrayDeque<>();
+		
+		for(int r = 0; r<realviruse.length; r++) {
+			int[] tmp = realviruse[r];
+			copyVirus.add(tmp);
+		}
+		
+		
 		while (!copyVirus.isEmpty()) {
 			int[] curr = copyVirus.poll();
 
@@ -106,6 +105,25 @@ public class Main {
 			}
 
 		}
+		
+		compareMax(cloneLab);
+	}
+	
+	private static void compareMax(int[][] cloneLab) {
+		int cnt = 0;
+
+		for(int r = 0; r<N; r++) {
+			
+			for(int c = 0; c<M; c++) {
+				
+				if(cloneLab[r][c] == 0) {
+					cnt++;
+				}
+				
+			}
+			
+		}
+		max = Math.max(max, cnt);
 	}
 
 }
