5f174742c7b7e17e4a01c10311cf62d71231d00c
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-17 18:09:10 +0900

    [Gold III] Title: 달팽이3, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Gold/1959. 달팽이3/달팽이3.java
+++ 백준/Gold/1959. 달팽이3/달팽이3.java
@@ -1,40 +1,48 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	private static long M, N;
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long m = Long.parseLong(st.nextToken());
-		long n = Long.parseLong(st.nextToken());
-		
 		StringBuilder sb = new StringBuilder();
-		if(m>n) {
-			sb.append(((n-1)*2+1)+"\n");
-		}else {
-			sb.append(((m-1)*2)+"\n");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Long.parseLong(st.nextToken());
+		N = Long.parseLong(st.nextToken());
+
+		if (M > N) {
+			sb.append((N - 1) * 2 + 1).append("\n");
+		} else {
+			sb.append((M - 1) * 2).append("\n");
 		}
-		
-		if(m==n) {
-			if(m%2==1) {
-				sb.append((m/2+1)+" "+(n/2+1) +"\n");
-			}else {
-				sb.append((m/2+1)+" "+(n/2) +"\n");
+
+		if (M == N) {
+			if (M % 2 == 1) {
+				sb.append(M / 2 + 1).append(" ").append(N / 2 + 1);
+			} else {
+				sb.append(M / 2 + 1).append(" ").append(N / 2);
+
 			}
-	 	}else if(m>n) {
-	 		if(n%2==0) {
-	 			sb.append((n/2+1) +" "+(n/2)+"\n");
-	 		}else {
-	 			sb.append((n/2+1+(m-n)) +" "+(n/2+1)+"\n");
-	 		}
-	 	}else {
-	 		if(m%2==0) {
-	 			sb.append((m/2+1) +" "+(m/2)+"\n");
-	 		}else {
-	 			sb.append((m/2+1)+" "+(m/2+1+(n-m))+"\n");
-	 		}
-	 	}
+		} else if (M > N) {
+			if (N % 2 == 0) {
+				sb.append(N / 2 + 1).append(" ").append(N / 2);
+			} else {
+				sb.append(N / 2 + 1 + (M - N)).append(" ").append(N / 2 + 1);
+			}
+
+		} else {
+			if (M % 2 == 0) {
+				sb.append(M / 2 + 1).append(" ").append(M / 2);
+			} else {
+				sb.append(M / 2 + 1).append(" ").append(M / 2 + 1 + (N - M));
+			}
+
+		}
 		System.out.println(sb.toString());
+
 	}
+
 }
