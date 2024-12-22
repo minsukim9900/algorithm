693e168698bff7002264c5778fe91cc4da41ff61
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-22 23:50:48 +0900

    [Gold V] Title: 치킨 배달, Time: 148 ms, Memory: 13908 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,74 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M, min;
+
 	private static List<int[]> house = new ArrayList<>();
 	private static List<int[]> chicken = new ArrayList<>();
-	private static boolean[] visited;
+	private static int[] result;
+	private static int N, M;
+	private static int total = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		min = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
+
+		result = new int[M];
+
+		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-
-			for (int j = 0; j < N; j++) {
+			for (int c = 0; c < N; c++) {
 				int tmp = Integer.parseInt(st.nextToken());
-
 				if (tmp == 1) {
-					house.add(new int[] { i + 1, j + 1 });
+					house.add(new int[] { r, c });
 				} else if (tmp == 2) {
-					chicken.add(new int[] { i + 1, j + 1 });
+					chicken.add(new int[] { r, c });
 				}
 			}
 
 		}
-		visited = new boolean[chicken.size()];
 		
-		dfs(0,0);
-		
-		System.out.println(min);
+		comb(0, 0);
+		System.out.println(total);
+
 	}
 
-	public static void dfs(int num, int depth) {
-
+	private static void comb(int num, int depth) {
 		if (depth == M) {
+			List<int[]> choice = new ArrayList<>();
 			
-			int sum = 0;
-			
-			for (int i = 0; i < house.size(); i++) {
-				int[] curr_house = house.get(i);
-				int min_dis = Integer.MAX_VALUE;
-
-				for (int j = 0; j < chicken.size(); j++) {
-					if(visited[j]) {
-						int[] curr_chicken = chicken.get(j);
-						int tmp = Math.abs(curr_chicken[0] - curr_house[0]) + Math.abs(curr_chicken[1] - curr_house[1]);
-						if (min_dis > tmp)
-							min_dis = tmp;
-					}
-				}
-				sum += min_dis;
+			for (int i = 0; i < M; i++) {
+				int idx = result[i];
+				choice.add(new int[] { chicken.get(idx)[0], chicken.get(idx)[1] });
 			}
-			if(min > sum) min = sum;
 			
-			
+			calLength(choice);
 
 		} else {
-
 			for (int i = num; i < chicken.size(); i++) {
-				if (visited[i])
-					continue;
-				visited[i] = true;
-				dfs(i+1, depth + 1);
-				visited[i] = false;
+				result[depth] = i;
+				comb(i + 1, depth + 1);
 			}
-
 		}
+	}
+
+	private static void calLength(List<int[]> choice) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < house.size(); i++) {
+			int dist = Integer.MAX_VALUE;
+			int[] home = house.get(i);
+
+			for (int j = 0; j < choice.size(); j++) {
+				int[] curr = choice.get(j);
+				int tmp = Math.abs(home[0] - curr[0]) + Math.abs(home[1] - curr[1]);
+				dist = Math.min(dist, tmp);
+			}
+			sum += dist;
+		}
+		
+		total = Math.min(total, sum);
 
 	}
 
