6de58f89b297d5eb5638056711c4aa5641b3bae2
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-30 03:06:21 +0900

    [Silver II] Title: 마인크래프트, Time: 676 ms, Memory: 287136 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -5,80 +5,96 @@ public class Main {
 
 	private static int N, M, B;
 	private static int[][] land;
-	private static int[]result = new int[] {Integer.MAX_VALUE,0};
-	private static int time;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
+
 		land = new int[N][M];
 
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
+		int min = 257;
+		int max = -1;
 
 		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-
 			for (int c = 0; c < M; c++) {
 				int tmp = Integer.parseInt(st.nextToken());
+				if (tmp > max) {
+					max = tmp;
+				}
+				if (tmp < min) {
+					min = tmp;
+				}
+				
 				land[r][c] = tmp;
-				max = Math.max(max, tmp);
-				min = Math.min(min, tmp);
 			}
-
 		}
+
+		int time = Integer.MAX_VALUE;
+		int high = 0;
+
 		for (int i = min; i <= max; i++) {
-			cntTime(i);
-		}
-		
-		for(int w : result) {
-			sb.append(w).append(" ");
-		}
-		System.out.println(sb.toString());
-		
-
-	}
-
-	public static void cntTime(int blockHeight) {
-		time = 0;
-		int inventory = B;
-		
-		for(int r = 0; r<N; r++) {
-			
-			for(int c = 0; c<M; c++) {
-				
-				if(land[r][c] == blockHeight) continue;
-				
-				else if(land[r][c] > blockHeight) {
-					
-					time += (land[r][c] - blockHeight) * 2;
-					inventory += (land[r][c] - blockHeight);
+			int curr = even(i);
+			if (curr == -1) {
+				continue;
+			} else {
+				if (time > curr) {
+					time = curr;
+					high = i;
+				} else if (time == curr) {
+					high = i;
 				}
-				
-				else if(land[r][c] < blockHeight) {
-					
-					time += (blockHeight - land[r][c]);
-					inventory -= (blockHeight-land[r][c]);
-				}
-				
 			}
-			
 		}
-		
-		if(time < result[0] && inventory >= 0) {
-			result[0] = time;
-			result[1] = blockHeight;
-		}
-		else if(time == result[0] && blockHeight >= result[1] && inventory >= 0) {
-			result[1] = blockHeight;
-		}
-		
+
+		System.out.println(time + " " + high);
+
 	}
 
+	private static int even(int high) {
+
+		int[][] copy = new int[N][M];
+		
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				copy[r][c] = land[r][c];
+			}
+		}
+
+		int inventory = B;
+		int time = 0;
+
+		for (int r = 0; r < N; r++) {
+
+			for (int c = 0; c < M; c++) {
+				if (copy[r][c] > high) {
+					int dif = copy[r][c] - high;
+					inventory += dif;
+					copy[r][c] -= dif;
+					time += (dif * 2);
+				} else if (copy[r][c] < high) {
+					int dif = high - copy[r][c];
+					inventory -= dif;
+					copy[r][c] += dif;
+					time += dif;
+				}
+			}
+
+		}
+		
+		
+		if (inventory >= 0) {
+			return time;
+		}
+		return -1;
+
+	}
 
 }
