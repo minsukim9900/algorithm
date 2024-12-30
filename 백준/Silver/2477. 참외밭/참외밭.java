aff313f161305e629b495bb3a71ee90c11d3c07d
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-30 23:17:56 +0900

    [Silver II] Title: 참외밭, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -3,126 +3,87 @@ import java.util.*;
 
 public class Main {
 
-	private static int N;
-	private static int[][] square = new int[5][2];
-	private static int[] check = new int[5];
-	private static int[] num = new int[5];
-	private static int[] index = new int[2];
-	private static int max_width = Integer.MIN_VALUE;
-	private static int max_hight = Integer.MIN_VALUE;
+	private static int[] nums = new int[6];
 
-	public static void main(String[] args) throws Exception {
+	private static PriorityQueue<Integer>[] pq = new PriorityQueue[2];
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < 2; i++) {
+			pq[i] = new PriorityQueue<>(new Comparator<Integer>() {
+
+				@Override
+				public int compare(Integer o1, Integer o2) {
+					return o2 - o1;
+				}
+			});
+		}
 
 		for (int i = 0; i < 6; i++) {
-			int tmp = 0;
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			int dir = Integer.parseInt(st.nextToken());
-			int len = Integer.parseInt(st.nextToken());
-			if(num[dir] == 0) {
-				num[dir] = i+1;
-			}
-			if (dir == 4 || dir == 3) {
-				if (max_hight < len) {
-					max_hight = len;
-				}
-			} else if (dir == 1 || dir == 2) {
-				if (max_width < len) {
-					max_width = len;
-				}
+			int length = Integer.parseInt(st.nextToken());
+
+			if (dir == 1 || dir == 2) {
+				pq[0].add(length);
+			} else {
+				pq[1].add(length);
 			}
 
-			if (check[dir] == 1) {
-				tmp++;
-			}
-			check[dir]++;
-			square[dir][tmp] = len;
+			nums[i] = length;
+
 		}
 		
-		
-		int idx = 0;
-		for (int i = 1; i <= 4; i++) {
-			if (check[i] == 2) {
-				index[idx++] = i;
-			}
-		}
-		
-		
-		
-		
-		int minuseArea = clearArea();
-		System.out.println((max_width * max_hight - minuseArea)*N);
+		System.out.println(process() * N);
 
 	}
 
-	private static int clearArea() {
+	private static int process() {
 
-		if (index[0] == 1 && index[1] == 3) {
-			if(num[3] == 1 && num[1] == 2 && num[4] == 3) {
-				int tmp = square[1][1] * square[3][0];
-				return tmp;
-			}else if(num[1] == 1 && num[4] == 2 && num[2] == 3) {
-				int tmp = square[1][1] * square[3][1];
-				return tmp;
-			}
-			else if(num[index[0]] < num[index[1]]) { // 1 - 3 - 1 - 3 순서
-				int tmp = square[1][0] * square[3][0];
-				return tmp;
-			}else if(num[index[0]] > num[index[1]]) { // 3 - 1 - 3 - 1 순서
-				int tmp = square[1][0] * square[3][1];
-				return tmp;
-			}
+		int heightMax = pq[0].poll();
+		int widthMax = pq[1].poll();
 
-		} else if (index[0] == 2 && index[1] == 3) {
-			if(num[3] == 1  && num[1] == 2 && num[4] == 3) {
-				int tmp = square[2][1] * square[3][1];
-				return tmp;
-			}else if(num[2] == 1 && num[3] == 2 && num[1] == 3) {
-				int tmp = square[2][0] * square[3][1];
-				return tmp;
-			}
-			else if(num[index[0]] < num[index[1]]) { // 2 - 3 - 2 - 3 순서
-				int tmp = square[2][1] * square[3][0];
-				return tmp;
-			}else if(num[index[0]] > num[index[1]]) { // 3 - 2 - 3 - 2 순서
-				int tmp = square[2][0] * square[3][0];
-				return tmp;
-			}
+		int hidx = 0;
+		int widx = 0;
 
-		} else if (index[0] == 1 && index[1] == 4) {
-			if(num[4] == 1 && num[2] == 2 && num[3] == 3 ) {
-				int tmp = square[1][1] * square[4][1];
-				return tmp;
-			}else if(num[1] == 1 && num[4] == 2 && num[2] == 3) {
-				int tmp = square[1][0] * square[4][1];
-				return tmp;
+		for (int i = 0; i < 6; i++) {
+			if (heightMax == nums[i]) {
+				hidx = i;
 			}
-			else if(num[index[0]] < num[index[1]]) { // 1 - 4 - 1 - 4
-				int tmp = square[1][1] * square[4][0];
-				return tmp;
-			}else if(num[index[0]] > num[index[1]]){ 
-				int tmp = square[1][0] * square[4][0];
-				return tmp;
-			}
-
-		} else if (index[0] == 2 && index[1] == 4) {
-			if(num[2] == 1 && num[3] == 2 && num[1] == 3) {
-				int tmp = square[2][1] * square[4][1];
-				return tmp;
-			}else if(num[4] == 1 && num[2] == 2 && num[3] == 3) {
-				int tmp = square[2][1] * square[4][0];
-				return tmp;
-			}
-			else if(num[index[0]] < num[index[1]]) { // 2 - 4 - 2 - 4
-				int tmp = square[2][0] * square[4][0];
-				return tmp;
-			}else if(num[index[0]] > num[index[1]]) { // 4 - 2 - 4 - 2
-				int tmp = square[2][0] * square[4][1];
-				return tmp;
+			if (widthMax == nums[i]) {
+				widx = i;
 			}
 		}
-		return 0;
+
+		int extent = heightMax * widthMax;
+
+		int left_Hidx = hidx - 1;
+		if (left_Hidx == -1)
+			left_Hidx = 5;
+		int right_Hidx = hidx + 1;
+		if (right_Hidx == 6)
+			right_Hidx = 0;
+
+		int subHeight = Math.abs(nums[left_Hidx] - nums[right_Hidx]);
+
+		int left_Widx = widx - 1;
+		if (left_Widx == -1)
+			left_Widx = 5;
+		int right_Widx = widx + 1;
+		if (right_Widx == 6)
+			right_Widx = 0;
+
+		int subWidth = Math.abs(nums[left_Widx] - nums[right_Widx]);
+
+		int subArea = subHeight * subWidth;
+
+		return extent - subArea;
 	}
 
 }
