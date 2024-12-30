9980fdb375fb97312a2212dc8fa47304c64765a0
Kim Minsu <mmsskksw9900@gmail.com>
2024-12-30 10:46:51 +0900

    [Gold IV] Title: 빙산, Time: 484 ms, Memory: 140604 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -4,159 +4,143 @@ import java.util.*;
 public class Main {
 
 	private static int N, M;
-	private static Queue<int[]> ice = new ArrayDeque<>();
 	private static int[][] ocean;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-	private static boolean[][] visited;
+	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+
 		ocean = new int[N + 2][M + 2];
 
 		for (int r = 1; r <= N; r++) {
 			st = new StringTokenizer(br.readLine());
-
 			for (int c = 1; c <= M; c++) {
 				ocean[r][c] = Integer.parseInt(st.nextToken());
+
 			}
 		}
-		int years = 0;
-		
+
+		int time = 0;
 		while (true) {
-			visited = new boolean[N + 2][M + 2];
-			int[] start = new int[2];
-			
-			set(start);
-			dfs(start);
-			
-			if(!verify1()) break;
-			
-			bfs();
-			
-			if(!verify2()) {
-				years = 0;
+
+			boolean[][] visited = new boolean[N + 1][M + 1];
+			Queue<int[]> q = new ArrayDeque<>();
+			boolean first = true;
+
+			for (int r = 1; r <= N; r++) {
+
+				for (int c = 1; c <= M; c++) {
+
+					if (first && ocean[r][c] > 0) {
+						q.offer(new int[] { r, c, ocean[r][c] });
+						first = false;
+					}
+
+				}
+
+			}
+
+			select(q, visited);
+			time++;
+			if (diviedCheck(visited)) {
+				time--;
 				break;
 			}
 			
-			years++;
-
-		}
-		
-		System.out.println(years);
-
-
-	}
-	
-	private static void set(int[] start) {
-		boolean check = false;
-		
-		for (int r = 1; r <= N; r++) {
-			
-			for (int c = 1; c <= M; c++) {
-				
-				if (!check && ocean[r][c] > 0) {
-					start[0] = r;
-					start[1] = c;
-				}
-
+			if(disappearCheck()) {
+				time = 0;
+				break;
 			}
-			
 		}
 		
-	}
-	
-	private static boolean verify1() {
-		
-		for(int r = 1; r<=N; r++ ) {
-			
-			for(int c = 1; c<=M; c++) {
-				
-				if( !visited[r][c] && ocean[r][c] > 0) {
-					return false;
-				}
-				
-			}
-			
-		}
-		return true;
-		
-	}
-	
-	private static boolean verify2() {
-		int sum = 0;
-		
-		for(int r = 1; r<=N; r++ ) {
-			
-			for(int c = 1; c<=M; c++) {
-				sum += ocean[r][c];
-			}
-			
-		}
-		
-		if(sum == 0) {
-			return false;
-		}
-		return true;
+		System.out.println(time);
 	}
 
-	private static void dfs(int[] start) {
-		
-		ice.add(start);
-		visited[start[0]][start[1]] = true;
-		
-		for (int i = 0; i < 4; i++) {
-			int nr = start[0] + dr[i];
-			int nc = start[1] + dc[i];
+	private static void select(Queue<int[]> q, boolean[][] visited) {
+		Queue<int[]> change = new ArrayDeque<>();
+		visited[q.peek()[0]][q.peek()[1]] = true;
 
-			if (!visited[nr][nc] && ocean[nr][nc] > 0) {
-				int[] iceMount = new int[] { nr, nc };
-				dfs(iceMount);
-			}
-			
-		}
-
-	}
-
-	private static void bfs() {
-
-		Queue<int[]> changeIce = new ArrayDeque<>();
-
-		while (!ice.isEmpty()) {
-			int cnt = 0;
-			int[] curr = ice.poll();
+		while (!q.isEmpty()) {
+			int[] curr = q.poll();
+			int count = 0;
 
 			for (int i = 0; i < 4; i++) {
+				int nr = curr[0] + delta[i][0];
+				int nc = curr[1] + delta[i][1];
 
-				int nr = curr[0] + dr[i];
-				int nc = curr[1] + dc[i];
 				if (ocean[nr][nc] == 0) {
-					cnt++;
+					count++;
+				} else if (!visited[nr][nc] && ocean[nr][nc] > 0) {
+					q.offer(new int[] { nr, nc, ocean[nr][nc] });
+					visited[nr][nc] = true;
 				}
+
 			}
 
-			changeIce.add(new int[] { curr[0], curr[1], cnt });
+			change.offer(new int[] { curr[0], curr[1], count });
 
 		}
 
-		process(changeIce);
+		later(change);
+		
 
 	}
 
-	private static void process(Queue<int[]> changeIce) {
+	private static void later(Queue<int[]> change) {
 
-		while (!changeIce.isEmpty()) {
-			int[] curr = changeIce.poll();
-			ocean[curr[0]][curr[1]] -= curr[2];
-			if (ocean[curr[0]][curr[1]] < 0) {
-				ocean[curr[0]][curr[1]] = 0;
+		while (!change.isEmpty()) {
+			int[] curr = change.poll();
+
+			int r = curr[0];
+			int c = curr[1];
+			ocean[r][c] -= curr[2];
+
+			if (ocean[r][c] < 0) {
+				ocean[r][c] = 0;
 			}
+
 		}
 
+	}
+
+	private static boolean diviedCheck(boolean[][] visited) {
+
+		for (int r = 1; r <= N; r++) {
+
+			for (int c = 1; c <= M; c++) {
+
+				if (!visited[r][c] && ocean[r][c] > 0) {
+					return true;
+				}
+
+			}
+
+		}
+
+		return false;
+	}
+
+	private static boolean disappearCheck() {
+		int sum = 0;
+		
+		for (int r = 1; r <= N; r++) {
+			for (int c = 1; c <= M; c++) {
+				sum += ocean[r][c];
+			}
+		}
+		
+		if(sum <= 0) {
+			return true;
+		}
+		return false;
 	}
 
 }
