4957a4419cab13b669bef02ce20cde5d16f025f6
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-01 01:14:58 +0900

    [Gold IV] Title: 치즈, Time: 100 ms, Memory: 13540 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,95 +1,102 @@
+
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
-	private static int R, C, cheese;
+	private static int R, C;
 	private static int[][] board;
+	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 	private static boolean[][] visited;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-	private static int result;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		board = new int[R][C];
 
-		cheese = 0;
+		board = new int[R][C];
+		boolean check = false;
+		int[] start = new int[2];
 
 		for (int r = 0; r < R; r++) {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < C; c++) {
-				int tmp = Integer.parseInt(st.nextToken());
-				if (tmp == 1) {
-					cheese++;
+				board[r][c] = Integer.parseInt(st.nextToken());
+
+				if (!check && board[r][c] == 0) {
+					start[0] = r;
+					start[1] = c;
+					check = true;
 				}
-				board[r][c] = tmp;
+
 			}
 		}
-		
-		result = cheese;
+
 		int time = 0;
-		
-		if(cheese == 0) {
-			System.out.println(time);
-			System.out.println(result);
-			return;
-		}
-		
-		while(true) {
-			
+		int cheese = cnt();
+		while (true) {
 			visited = new boolean[R][C];
-			
-			bfs();
-			
+			bfs(start);
 			time++;
-			
-			if(cheese == 0) {
+			int tmp = cnt();
+			if(tmp == 0) {
 				System.out.println(time);
-				System.out.println(result);
+				System.out.println(cheese);
 				return;
+			}else {
+				cheese = tmp;
 			}
-			
 		}
+
 	}
 
-	private static void bfs() {
+	private static void bfs(int[] start) {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] { 0, 0 });
-		visited[0][0] = true;
+
+		q.add(start);
 
 		while (!q.isEmpty()) {
 			int[] curr = q.poll();
+
 			for (int i = 0; i < 4; i++) {
-				int nr = curr[0] + dr[i];
-				int nc = curr[1] + dc[i];
+				int nr = curr[0] + delta[i][0];
+				int nc = curr[1] + delta[i][1];
 
 				if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-					if (!visited[nr][nc] && board[nr][nc] == 1) {
+
+					if (!visited[nr][nc] && board[nr][nc] == 0) {
+
+						visited[nr][nc] = true;
+						q.add(new int[] { nr, nc });
+
+					} else if (!visited[nr][nc] && board[nr][nc] == 1) {
+
 						board[nr][nc] = 0;
 						visited[nr][nc] = true;
-						
-						cheese--;
-						
-					} else if (!visited[nr][nc] && board[nr][nc] == 0) {
-						visited[nr][nc] = true;
-						q.offer(new int[] { nr, nc });
+
 					}
+
 				}
 			}
+		}
+	}
 
+	private static int cnt() {
+		
+		int sum = 0;
+		
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				sum += board[r][c];
+			}
 		}
 		
-		if(cheese != 0) {
-			result = cheese;
-		}
+		return sum;
 	}
 
 }
