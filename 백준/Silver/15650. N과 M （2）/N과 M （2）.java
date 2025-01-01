f1d541feea6b02c4761c5ff332df21370d1bbb5e
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-01 22:22:25 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,42 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	private static int N, M;
-	private static int[] nums;
 	private static int[] result;
 	private static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		nums = new int[N+1];
-		
-		for(int i = 0; i< N; i++) {
-			nums[i] = i+1;
-		}
 		result = new int[M];
+
+		comp(1, 0);
+		System.out.println(sb);
 		
-		combination(0, 0);
-		System.out.println(sb.toString());
-	}
-	
-	public static void combination(int idx, int sidx) {
-		
-		if(sidx == M) {
-			for(int i = 0; i<M; i++) {
-				sb.append(result[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i = idx; i<=N-M+sidx; i++) {
-			result[sidx] = nums[i];
-			combination(i+1, sidx+1);
-		}
 		
 	}
 
+	private static void comp(int idx, int depth) {
+
+		if (depth == M) {
+			
+			for(int i = 0; i<result.length; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+
+		for (int i = idx; i <= N; i++) {
+			result[depth] = i;
+			comp(i + 1, depth + 1);
+		}
+
+	}
 }
