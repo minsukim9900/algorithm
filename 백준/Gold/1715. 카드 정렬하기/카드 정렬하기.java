f741c1e123abe96a0e2ca2e58f7c6c6ba95f452b
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-09 02:02:35 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 312 ms, Memory: 25136 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,30 +1,29 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Main {
- 
-    static int N;
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            pq.add(Integer.parseInt(st.nextToken()));
-        }
-        int sum = 0;
- 
-        while ((pq.size() > 1)) {
-            int a = pq.poll();
-            int b = pq.poll();
-            sum += (a + b);
-            pq.add(a + b);
-        }
-        
-        System.out.println(sum);
-    }
+
+	private static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			pq.add(Integer.parseInt(br.readLine()));
+		}
+
+		int sum = 0;
+		while (pq.size() > 1) {
+			int a = pq.poll();
+			int b = pq.poll();
+			sum += (a + b);
+			pq.add(a+b);
+		}
+		System.out.println(sum);
+
+	}
+
 }
