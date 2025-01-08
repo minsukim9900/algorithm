1bebe1edf3d652b878b3e4af987b3c0e2eadfd9f
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-09 01:21:22 +0900

    [Silver III] Title: N과 M (4), Time: 88 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,41 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	private static int N, M;
 	private static int[] result;
 	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		result = new int[M + 1];
 
-		permutation(1, 1);
+		result = new int[M];
+		dfs(1, 0);
 		System.out.println(sb.toString());
 
 	}
 
-	private static void permutation(int num, int depth) {
+	private static void dfs(int num, int depth) {
+		if (depth == M) {
 
-		if (depth == M + 1) {
-
-			for (int i = 1; i <= M; i++) {
+			for (int i = 0; i < M; i++) {
 				sb.append(result[i]).append(" ");
 			}
 			sb.append("\n");
-			
 
 		} else {
+
 			for (int i = num; i <= N; i++) {
 				result[depth] = i;
-				permutation(i , depth + 1);
+				dfs(i, depth + 1);
 			}
 
 		}
-
 	}
 
 }
