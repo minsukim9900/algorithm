20ba2cb214471abd3e8fc26202137a6217e3015c
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-11 01:23:13 +0900

    [Silver III] Title: N과 M (6), Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -2,51 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	private static int N, M;
-	private static int[] result;
-	private static int[] nums;
+	private static int[] nums, result;
 	private static boolean[] visited;
 	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		result = new int[M + 1];
-		nums = new int[N + 1];
-		visited = new boolean[N + 1];
+
+		nums = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
 
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
+		
 		Arrays.sort(nums);
-		dfs(1, 1);
+		
+		dfs(0, 0);
 		System.out.println(sb.toString());
 
 	}
 
-	public static void dfs(int num, int depth) {
+	private static void dfs(int num, int depth) {
 
-		if (depth == M + 1) {
+		if (depth == M) {
 
-			for (int i = 1; i <= M; i++) {
+			for (int i = 0; i < M; i++) {
 				sb.append(result[i]).append(" ");
 			}
 			sb.append("\n");
 
 		} else {
 
-			for (int i = num; i <= N; i++) {
+			for (int i = num; i < N; i++) {
+
 				if (visited[i])
 					continue;
 				visited[i] = true;
 				result[depth] = nums[i];
 				dfs(i + 1, depth + 1);
 				visited[i] = false;
+
 			}
 
 		}
