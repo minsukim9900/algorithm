2a168757346b3c55db265295d80817f741c51b66
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-12 22:17:52 +0900

    [Silver II] Title: N과 M (9), Time: 132 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -2,62 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static int[] nums;
-	private static int[] result;
-	private static boolean[] visited;
-	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	private static int N, M;
+	private static int[] result, nums;
+	private static StringBuilder sb = new StringBuilder();
+	private static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		nums = new int[N + 1];
-		result = new int[M + 1];
-		visited = new boolean[N + 1];
+
+		result = new int[M];
+		nums = new int[N];
+		visited = new boolean[N];
 
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
+
 		Arrays.sort(nums);
-		dfs(1);
+
+		dfs(0);
 		System.out.println(sb.toString());
 
 	}
 
-	public static void dfs(int depth) {
+	private static void dfs(int depth) {
 
-		if (depth == M + 1) {
-			for (int i = 1; i <= M; i++) {
-				System.out.print(result[i] + " ");
-				// sb.append(result[i]).append(" ");
+		if (depth == M) {
+
+			for (int i = 0; i < M; i++) {
+				sb.append(result[i]).append(" ");
 			}
-			// sb.append("\n");
-			System.out.println();
+			sb.append("\n");
 
 		} else {
-			int tmp = 0;
 
-			for (int i = 1; i <= N; i++) {
+			int pre = -1;
+			for (int i = 0; i < N; i++) {
 
-				if (visited[i]) {
-					continue;
-				}
-
-				if (tmp != nums[i]) {
-					tmp = nums[i];
+				if (nums[i] != pre && !visited[i]) {
+					pre = nums[i];
 					visited[i] = true;
 					result[depth] = nums[i];
 					dfs(depth + 1);
 					visited[i] = false;
+
 				}
 
 			}
+
 		}
+
 	}
 
 }
