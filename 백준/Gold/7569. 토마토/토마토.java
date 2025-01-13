c0aca1fc13540964ac65e6e239959db257a954d6
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-14 08:24:06 +0900

    [Gold V] Title: 토마토, Time: 540 ms, Memory: 106532 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -3,75 +3,105 @@ import java.util.*;
 
 public class Main {
 
-	private static int x, y, z;
-	private static Queue<int[]> q = new ArrayDeque<>();
-	private static int[][][] tomatoBox;
-	// 상, 하, 좌, 우, 위, 아래
-	private static int[] dx = { -1, 1, 0, 0, 0, 0 };
-	private static int[] dy = { 0, 0, -1, 1, 0, 0 };
-	private static int[] dz = { 0, 0, 0, 0, -1, 1 };
+	private static class tomato {
+		int y, x, z, t;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		x = Integer.parseInt(st.nextToken());
-		y = Integer.parseInt(st.nextToken());
-		z = Integer.parseInt(st.nextToken());
-		tomatoBox = new int[z][y][x];
-		
-		for (int i = 0; i < z; i++) {
-			for (int j = 0; j < y; j++) {
-				st = new StringTokenizer(br.readLine());
-				for (int k = 0; k < x; k++) {
-					int tmp = Integer.parseInt(st.nextToken());
-					tomatoBox[i][j][k] = tmp;
-					if (tmp == 1)
-						q.add(new int[] { k, j, i, 0 });
-				}
-			}
+		public tomato(int y, int x, int z, int t) {
+			super();
+			this.y = y;
+			this.x = x;
+			this.z = z;
+			this.t = t;
 		}
-		
-		int result = bfs();
-		
-		for (int i = 0; i < z; i++) {
-			for (int j = 0; j < y; j++) {
-				for (int k = 0; k < x; k++) {
-					if(tomatoBox[i][j][k] == 0) {
-						System.out.println(-1);
-						return;
-					}
-				}
-			}
-		}
-		System.out.println(result);
+
 	}
 
-	public static int bfs() {
-		int max = 0;
-		while (!q.isEmpty()) {
-			int[] curr = q.poll();
-			max = curr[3];
-			for (int i = 0; i < 6; i++) {
-				int nx = curr[0] + dx[i];
-				int ny = curr[1] + dy[i];
-				int nz = curr[2] + dz[i];
-				
-				if (nx >= 0 && nx < x && ny >= 0 && ny < y && nz >= 0 && nz < z) {
-					
-					if (tomatoBox[nz][ny][nx] == 0) {
-						q.add(new int[] { nx, ny, nz, curr[3]+1} );
-						tomatoBox[nz][ny][nx] = 1;
-					}
-					
-				}
-				
-			}
-			
-		}
-		
-		return max;
+	private static int M, N, H;
+	private static int[][][] board;
+	private static int[] dy = { -1, 1, 0, 0, 0, 0 };
+	private static int[] dx = { 0, 0, -1, 1, 0, 0 };
+	private static int[] dz = { 0, 0, 0, 0, -1, 1 };
+	private static int time = 0;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		board = new int[H][N][M];
+		Queue<tomato> q = new ArrayDeque<>();
+
+		for (int z = 0; z < H; z++) {
+
+			for (int y = 0; y < N; y++) {
+				st = new StringTokenizer(br.readLine());
+
+				for (int x = 0; x < M; x++) {
+					board[z][y][x] = Integer.parseInt(st.nextToken());
+
+					if (board[z][y][x] == 1) {
+						q.add(new tomato(y, x, z, 0));
+					}
+
+				}
+
+			}
+
+		}
+
+		bfs(q);
+
+		if (isClear()) {
+			System.out.println(time);
+		} else {
+			System.out.println(-1);
+		}
+
+	}
+
+	private static void bfs(Queue<tomato> q) {
+
+		while (!q.isEmpty()) {
+
+			tomato curr = q.poll();
+
+			time = curr.t;
+
+			for (int i = 0; i < 6; i++) {
+
+				int ny = curr.y + dy[i];
+				int nx = curr.x + dx[i];
+				int nz = curr.z + dz[i];
+
+				if (ny >= 0 && ny < N && nx >= 0 && nx < M && nz >= 0 && nz < H && board[nz][ny][nx] == 0) {
+					board[nz][ny][nx] = 1;
+					q.add(new tomato(ny, nx, nz, curr.t + 1));
+				}
+			}
+
+		}
+
+	}
+
+	private static boolean isClear() {
+		for (int z = 0; z < H; z++) {
+
+			for (int y = 0; y < N; y++) {
+
+				for (int x = 0; x < M; x++) {
+
+					if (board[z][y][x] == 0) {
+						return false;
+					}
+
+				}
+
+			}
+		}
+
+		return true;
 	}
 
 }
