d333ef1d658b21f8ec3ed2e3dfc9a8c18d7fdaa5
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-17 00:50:43 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 868 ms, Memory: 91876 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -2,71 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	private static int N, M, R;
+
+	private static int V, E, R;
 	private static List<Integer>[] adj;
-	private static int[] order;
-	private static int cnt = 0;
-	
-	public static void main(String[] args) throws Exception {
+	private static int[] result;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-	
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
-		adj = new ArrayList[N+1];
-		order = new int[N+1];
-		
-		for(int i = 1; i<=N; i++) {
+
+		adj = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
 			adj[i] = new ArrayList<>();
 		}
-		
-		for(int i = 0; i<M; i++) {
+
+		result = new int[V+1];
+
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			
+
 			adj[from].add(to);
 			adj[to].add(from);
 		}
-		
-		for(int i = 1; i<=N; i++) {
+
+		for (int i = 1; i <= V; i++) {
 			Collections.sort(adj[i], Collections.reverseOrder());
 		}
-		
+
 		bfs(R);
-		
-		for(int i = 1; i<=N; i++) {
-			sb.append(order[i]).append("\n");
+		for(int i = 1; i<=V; i++) {
+			sb.append(result[i]).append("\n");
 		}
 		
 		System.out.println(sb.toString());
-		
-		
+
 	}
-	
-	public static void bfs(int R) {
+
+	private static void bfs(int start) {
 		Queue<Integer> q = new ArrayDeque<>();
-		q.add(R);
-		order[R] = ++cnt;
+		int cnt = 1;
+		q.add(start);
+		result[R] = 1;
 		
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			int curr = q.poll();
-			
-			for(int w : adj[curr]) {
-				if(order[w] == 0) {
+
+			for (int w : adj[curr]) {
+				if (result[w] == 0) {
+					result[w] = ++cnt;
 					q.add(w);
-					order[w] = ++cnt;
 				}
 			}
 		}
+
 	}
-	
-	
-	
-	
-	
 }
