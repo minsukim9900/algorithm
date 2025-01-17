d9810a88cbefed0a13edcf8c77a6fae6a1eb7a67
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-17 09:12:06 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 648 ms, Memory: 89396 KB -BaekjoonHub

--- 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
@@ -2,72 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	private static int N, M, R;
+
+	private static int V, E, R;
 	private static List<Integer>[] adj;
-	private static int[] depth;
-	private static int len = 0;
+	private static int[] result;
 	private static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-	
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
-		adj = new ArrayList[N+1];
-		depth = new int[N+1];
-		
-		for(int i = 1; i<=N; i++) {
+
+		adj = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
 			adj[i] = new ArrayList<>();
 		}
-		
-		for(int i = 0; i<M; i++) {
+
+		result = new int[V + 1];
+
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			
+
 			adj[from].add(to);
 			adj[to].add(from);
 		}
-		
+
+		for (int i = 1; i <= V; i++) {
+			result[i] = -1;
+		}
+
 		bfs(R);
-		
-		output();
-		
-		
+		for (int i = 1; i <= V; i++) {
+			sb.append(result[i]).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
 	}
-	
-	public static void bfs(int R) {
+
+	private static void bfs(int start) {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {R, 0});
-		depth[R] = 0;
-		
-		
-		while(!q.isEmpty()) {
+		q.add(new int[] { start, 0 });
+		result[R] = 0;
+
+		while (!q.isEmpty()) {
 			int[] curr = q.poll();
-			for(int w : adj[curr[0]]) {
-				if(depth[w] == 0 && w != R) {
+
+			for (int w : adj[curr[0]]) {
+				if (result[w] == -1) {
+					result[w] = curr[1]+1;
 					q.add(new int[] {w, curr[1]+1});
-					depth[w] = curr[1]+1;
 				}
 			}
 		}
+
 	}
-	
-	
-	public static void output() {
-		for(int i = 1; i<=N; i++) {
-			if(depth[i] == 0 && i != R) {
-				depth[i] = -1;
-			}
-			sb.append(depth[i]).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
-	
-	
 }
