2a97c4b8c85e39550855493ad804fd414dda369e
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-19 01:16:36 +0900

    [Silver II] Title: N과 M (10), Time: 64 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -4,12 +4,15 @@ import java.util.*;
 public class Main {
 
 	private static int N, M;
-	private static int[] nums;
 	private static int[] result;
+	private static int[] nums;
+	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
@@ -17,46 +20,43 @@ public class Main {
 		result = new int[M];
 
 		st = new StringTokenizer(br.readLine());
-		
 		for (int i = 0; i < N; i++) {
-			
 			nums[i] = Integer.parseInt(st.nextToken());
-			
 		}
-
+		
 		Arrays.sort(nums);
 
 		dfs(0, 0);
+		System.out.println(sb.toString());
 
 	}
 
 	private static void dfs(int num, int depth) {
 
 		if (depth == M) {
-			
-			for(int i = 0; i<M; i++) {
-				
-				System.out.print(result[i]+" ");
-				
+
+			for (int w : result) {
+				sb.append(w).append(" ");
 			}
-			
-			System.out.println();
-			
+			sb.append("\n");
+
 		} else {
 
-			int tmp = 0;
+			int pre = -1;
 
 			for (int i = num; i < N; i++) {
-				
-				if (tmp == nums[i])
-					continue;
-				tmp = nums[i];
-				result[depth] = nums[i];
-				dfs(i+1, depth+1);
-				
+
+				if (pre != nums[i]) {
+					pre = nums[i];
+					result[depth] = nums[i];
+					dfs(i + 1, depth + 1);
+
+				}
+
 			}
 
 		}
 
 	}
+
 }
