715c068a5f7e71b6d2e65e4c04232b4751b2e71c
Kim Minsu <mmsskksw9900@gmail.com>
2025-01-20 13:18:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 880 ms, Memory: 93444 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -2,58 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int V, E, R; // 노드, 간선의 갯수, 시작 정점
-	private static List<Integer>[] adj;
-	private static int[] visited;
-	private static StringBuilder sb = new StringBuilder();
-	private static int count = 0;
 
-	public static void main(String[] args) throws Exception {
+	private static int N, M, R;
+	private static List<Integer>[] adj;
+	private static int[] result;
+	private static int depth = 1;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
 		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
-		adj = new ArrayList[V+1];
-		for(int i = 1; i<=V; i++) {
+
+		result = new int[N + 1];
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
 			adj[i] = new ArrayList<>();
 		}
 
-		visited = new int[V + 1];
-
-		for (int i = 0; i < E; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
+
 			adj[from].add(to);
 			adj[to].add(from);
-
 		}
-		
-		for(int i = 1; i<=V; i++) {
+
+		for (int i = 1; i <= N; i++) {
 			Collections.sort(adj[i]);
 		}
 		
+		result[R] = depth;
 		dfs(R);
-		
-		for(int i = 1; i<visited.length; i++) {
-			sb.append(visited[i]).append("\n");
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(result[i]).append("\n");
 		}
+
 		System.out.println(sb.toString());
 
 	}
 
-	public static void dfs(int v) {
-		visited[v] = ++count;
+	private static void dfs(int V) {
 		
-		for(int value : adj[v]) {
-			if(visited[value] == 0) {
-				dfs(value);
+		
+		for(int w : adj[V]) {
+			if(result[w] == 0) {
+				result[w] = ++depth;
+				dfs(w);
 			}
 		}
+
 	}
 
 }
