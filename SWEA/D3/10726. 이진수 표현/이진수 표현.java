052279019bd292c05a0eef087768dfb6d89b3a83
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-02 00:05:30 +0900

    [D3] Title: 이진수 표현, Time: 118 ms, Memory: 32,896 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -6,28 +6,24 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		StringBuilder sb = new StringBuilder();
+        
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
+
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				if ((M & (1 << i)) != 0) {
-					cnt++;
-				}
-			}
-			
-			if(cnt == N) {
-				System.out.println("#"+t+" "+"ON");
-			}else {
-				System.out.println("#"+t+" "+"OFF");
-			}
-		}
-	}
+			String ans = "OFF";
 
+			if ((M & ((1 << N) - 1)) == ((1 << N) - 1)) {
+				ans = "ON";
+			}
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+
+	}
 }
