7730e35158276df0d21e1b89cea6de54f81bd603
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-05 16:52:29 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 72 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -2,75 +2,79 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	private static int N;
+	private static int[] lights;
+
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-		int nLight = Integer.parseInt(br.readLine());// 스위치 갯수
-		int[] situation = new int[nLight + 1]; // 스위치 배열 생성
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		lights = new int[N + 1];
 
-		for (int i = 1; i < situation.length; i++) {
-			situation[i] = Integer.parseInt(st.nextToken()); // 스위치 배열 넣기
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i ++) {
+			lights[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int totalStudent = Integer.parseInt(br.readLine()); // 총 학생 수
+		int T = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < totalStudent; i++) {
+		for (int t = 0; t < T; t++) {
+
 			st = new StringTokenizer(br.readLine());
-			int student = Integer.parseInt(st.nextToken()); // 성별
-			int select_index = Integer.parseInt(st.nextToken()); // 스위치 정보
-			if (student == 1) { // 남학생일 경우 선택한 index의 배수
-				for (int j = 1; j < situation.length; j++) {
-					if (j % select_index == 0) {
-						if (situation[j] == 0) {
-							situation[j] = 1;
-						} else {
-							situation[j] = 0;
-						}
-					}
-				}
+			int student = Integer.parseInt(st.nextToken());
+			int light = Integer.parseInt(st.nextToken());
+			order(student, light);
+		}
 
-			} else { // 여학생일 경우
-				if(situation[select_index] == 0) {
-					situation[select_index] = 1;
-				}
-				else {
-					situation[select_index] = 0;
-				}
-				for (int j = 1; j < nLight / 2; j++) {
-					if (select_index + j > nLight || select_index - j < 1) {
-						break;
-					}
-					if(situation[select_index-j]==situation[select_index+j]) {
-						if (situation[select_index-j] == 0) {
-							situation[select_index-j] = 1;
-						} else {
-							situation[select_index-j] = 0;
-						}
-						if (situation[select_index+j] == 0) {
-							situation[select_index+j] = 1;
-						} else {
-							situation[select_index+j] = 0;
-						}
-					}
-					else {
-						break;
-					}
-				}
+		for (int i = 1; i <= N; i++) {
+			sb.append(lights[i]).append(" ");
+			if (i % 20 == 0) {
+				sb.append("\n");
 			}
 		}
 
-		for (int i = 1; i < situation.length; i++) {
-			System.out.print(situation[i] + " ");
-			if((i%20) == 0) {
-				System.out.println();
+		System.out.println(sb.toString());
+
+	}
+
+	private static void order(int student, int light) {
+
+		if (student == 1) {
+
+			for (int i = light; i <= N; i += light) {
+				change(i);
 			}
+
+		} else {
+
+			change(light);
+			int left = light - 1;
+			int right = light + 1;
+
+			while (left >= 1 && right <= N) {
+
+				if ((lights[left] ^ lights[right]) != 0) {
+					break;
+				}
+				change(left);
+				change(right);
+				left--;
+				right++;
+			}
+
 		}
 
 	}
+
+	private static void change(int idx) {
+		lights[idx] ^= 1;
+	}
+
 }
