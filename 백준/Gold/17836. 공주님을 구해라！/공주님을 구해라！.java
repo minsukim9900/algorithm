9c5f9272adb23d198e1103032a1230f79d88584f
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-10 14:18:27 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 104 ms, Memory: 13084 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -5,10 +5,8 @@ public class Main {
 
 	private static int N, M, T;
 	private static int[][] maze;
-	private static int[][] bugMaze;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 	private static boolean[][] preVisited;
-	private static boolean[][] postVisited;
 	private static int cnt = 987654321;
 
 	public static void main(String[] args) throws IOException {
@@ -23,9 +21,7 @@ public class Main {
 		T = Integer.parseInt(st.nextToken());
 
 		maze = new int[N][M];
-		bugMaze = new int[N][M];
 		preVisited = new boolean[N][M];
-		postVisited = new boolean[N][M];
 
 		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
@@ -69,9 +65,7 @@ public class Main {
 							q.offer(new int[] { nr, nc, curr[2] + 1 });
 						} else if (maze[nr][nc] == 2) {
 							preVisited[nr][nc] = true;
-							bugBfs(nr, nc, curr[2] + 1);
-							q.offer(new int[] { nr, nc, curr[2] + 1 });
-
+							cnt = Math.min(cnt,  curr[2] + 1 + Math.abs((N-1) - nr +  (M-1) - nc));
 						}
 
 					}
@@ -81,30 +75,5 @@ public class Main {
 
 	}
 
-	private static void bugBfs(int r, int c, int len) {
-
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { r, c, len });
-		postVisited[r][c] = true;
-
-		while (!q.isEmpty()) {
-			int[] curr = q.poll();
-			
-			if ((curr[0] == N - 1) && (curr[1] == M - 1)) {
-				cnt = Math.min(cnt, curr[2]);
-			}
-
-			for (int i = 0; i < 4; i++) {
-				int nr = curr[0] + delta[i][0];
-				int nc = curr[1] + delta[i][1];
-
-				if (nr >= 0 && nr < N && nc >= 0 && nc < M && bugMaze[nr][nc] == 0 && !postVisited[nr][nc]) {
-					postVisited[nr][nc] = true;
-					q.offer(new int[] { nr, nc, curr[2] + 1 });
-				}
-			}
-		}
-
-	}
 
 }
