bea2de9efeb4c5bd8e09358c7c4f24be5bd6124e
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-10 10:50:35 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int[] dp;
+	private static int N;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		N = Integer.parseInt(br.readLine());
+
+		dp = new int[N + 2];
+
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+
+		System.out.println(dp[N]);
+
+	}
+
+}
