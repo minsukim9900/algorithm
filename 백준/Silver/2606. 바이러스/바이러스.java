903c1339eb371b47f6cf245df044ecb21755eac5
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-14 16:27:47 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,47 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	private static int V, E;
+
 	private static List<Integer>[] adj;
 	private static boolean[] visited;
-	private static int cnt = 0;
-	
-	public static void main(String[] args) throws Exception {
+	private static int cnt = -1;
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int V = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
 		int E = Integer.parseInt(br.readLine());
-		
-		adj = new ArrayList[V+1];
-		visited	= new boolean[V+1];
-		for(int i = 1; i<=V; i++) {
+		visited = new boolean[N + 1];
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
 			adj[i] = new ArrayList<>();
 		}
-		
-		
-		for(int i = 0; i<E; i++) {
+
+		for (int i = 0; i < E; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			
+
 			adj[from].add(to);
 			adj[to].add(from);
-			
+
 		}
-		
+
 		dfs(1);
-		cnt--;
 		System.out.println(cnt);
+
 	}
-	
-	public static void dfs(int v) {
+
+	private static void dfs(int num) {
+		visited[num] = true;
 		cnt++;
-		visited[v] = true;
-		
-		for(int curr : adj[v]) {
-			if(!visited[curr])
-				dfs(curr);
+
+		for (int w : adj[num]) {
+			if (!visited[w])
+				dfs(w);
 		}
+
 	}
+
 }
