87cb8cb7dfe5bf2cdc74648afc90f1531a4534ed
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-18 16:38:59 +0900

    [Gold V] Title: 집합의 표현, Time: 360 ms, Memory: 51648 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int N, M;
+	private static int[] p;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		p = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			p[i] = i;
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int state = Integer.parseInt(st.nextToken());
+			int numA = Integer.parseInt(st.nextToken());
+			int numB = Integer.parseInt(st.nextToken());
+
+			if (state == 0) {
+				union(numA, numB);
+			} else {
+				String str = "NO";
+				if (isSameP(numA, numB)) {
+					str = "YES";
+				}
+
+				sb.append(str).append("\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
+
+	}
+
+	private static boolean isSameP(int A, int B) {
+		if (findP(A) == findP(B))
+			return true;
+		return false;
+	}
+
+	private static void union(int A, int B) {
+		p[findP(B)] = findP(A);
+	}
+
+	private static int findP(int x) {
+		if (x != p[x]) {
+			p[x] = findP(p[x]);
+		}
+
+		return p[x];
+	}
+
+}
