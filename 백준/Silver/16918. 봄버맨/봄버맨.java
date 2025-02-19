b93498c25609434266cf372e71107653c7539e17
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-19 10:38:56 +0900

    [Silver I] Title: 봄버맨, Time: 372 ms, Memory: 176272 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -3,105 +3,95 @@ import java.util.*;
 
 public class Main {
 
+	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	private static int[][] map;
 	private static int R, C, N;
-	private static int[][] board;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-	private static Queue<int[]> blasting = new ArrayDeque<>();
-	private static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		board = new int[R][C];
+
+		map = new int[R][C];
 
 		for (int r = 0; r < R; r++) {
 			String str = br.readLine();
-
 			for (int c = 0; c < C; c++) {
 				char tmp = str.charAt(c);
-				if (tmp == '.') {
-					board[r][c] = 0;
-				} else {
-					board[r][c] = 1;
-				}
 
+				if (tmp == '.')
+					map[r][c] = 0;
+				else
+					map[r][c] = 2;
 			}
-
 		}
-		int n = 1;
-		while (n <= N && N != 1) {
-			act1();
-			n++;
-			if (n >= N) {
-				break;
+
+		int time = 1;
+		while (time < N) {
+			time++;
+			ArrayList<int[]> adj = new ArrayList<>();
+			for (int r = 0; r < R; r++) {
+				for (int c = 0; c < C; c++) {
+					map[r][c]++;
+					if (map[r][c] >= 2)
+						adj.add(new int[] { r, c });
+				}
 			}
-			act2_1();
-			n++;
-			if (n >= N) {
+			if (time >= N)
 				break;
-			}
+			time++;
+			bfs(adj);
+
 		}
 
 		for (int r = 0; r < R; r++) {
 			for (int c = 0; c < C; c++) {
-				if(board[r][c] == 0) {
+				if (map[r][c] == 0)
 					sb.append(".");
-				}
-				else {
+				else
 					sb.append("O");
-				}
 			}
 			sb.append("\n");
 		}
 		
 		System.out.println(sb.toString());
-
 	}
 
-	private static void act1() {
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				board[r][c]++;
-			}
-		}
-		
-		
-	}
+	private static void bfs(ArrayList<int[]> adj) {
 
-	private static void act2_1() {
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				board[r][c]++;
-				if (board[r][c] >= 3) {
-					board[r][c] = 0;
-					blasting.add(new int[] { r, c });
+		boolean[][] visited = new boolean[R][C];
+		int size = adj.size();
+		for (int i = 0; i < size; i++) {
+			int[] curr = adj.get(i);
+
+			for (int idx = 0; idx < 4; idx++) {
+				int nr = curr[0] + delta[idx][0];
+				int nc = curr[1] + delta[idx][1];
+
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] != 0 && !visited[nr][nc]) {
+					visited[nr][nc] = true;
+					adj.add(new int[] { nr, nc });
 				}
 			}
 		}
-		while (!blasting.isEmpty()) {
-			int[] curr = blasting.poll();
-			act2_2(curr[0], curr[1]);
-		}
-		
-		
+
+		explode(adj);
 	}
 
-	private static void act2_2(int r, int c) {
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-				board[nr][nc] = 0;
-			}
+	private static void explode(ArrayList<int[]> adj) {
+
+		for (int i = 0; i < adj.size(); i++) {
+			int[] curr = adj.get(i);
+
+			map[curr[0]][curr[1]] = 0;
 		}
-		
-		
-		
-		
+
 	}
 
 }
