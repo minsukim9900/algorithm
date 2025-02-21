cdd92254a86e34423d64230f09c98fe31f373add
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-21 15:39:22 +0900

    [D4] Title: [Professional] 키 순서, Time: 467 ms, Memory: 98,964 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Solution {
 
 	private static int N, M, cnt;
-	private static int[][] adj, radj;
+	private static int[][] adj;
 
 	public static void main(String[] args) throws IOException {
 
@@ -20,25 +20,36 @@ public class Solution {
 			M = Integer.parseInt(br.readLine());
 
 			adj = new int[N + 1][N + 1];
-			radj = new int[N + 1][N + 1];
 			for (int i = 0; i < M; i++) {
 
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				radj[to][from] = adj[from][to] = 1;
+				adj[from][to] = 1;
 
 			}
 
-			int ans = 0;
+			for (int i = 1; i <= N; i++) {
+				adj[i][0] = -1;
+			}
 
 			for (int i = 1; i <= N; i++) {
-				cnt = 0;
-				dfs(i, adj, new boolean[N + 1]);
-				dfs(i, radj, new boolean[N + 1]);
-				if (cnt == N - 1) {
-					ans++;
+				if (adj[i][0] != -1)
+					continue;
+				gtDFS(i);
+			}
+
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					if (adj[i][j] == 1)
+						adj[0][j]++;
 				}
+			}
+
+			int ans = 0;
+			for (int i = 1; i <= N; i++) {
+				if (adj[0][i] + adj[i][0] == N - 1)
+					ans++;
 			}
 
 			sb.append("#" + t + " ").append(ans + "\n");
@@ -47,13 +58,28 @@ public class Solution {
 		System.out.println(sb.toString());
 	}
 
-	private static void dfs(int st, int[][] map, boolean[] visited) {
-		visited[st] = true;
+	private static void gtDFS(int curr) {
 		for (int i = 1; i <= N; i++) {
-			if (!visited[i] && map[st][i] != 0) {
-				dfs(i, map, visited);
-				cnt++;
+
+			if (adj[curr][i] == 0)
+				continue;
+
+			if (adj[i][0] == -1) {
+				gtDFS(i);
 			}
+			
+			if(adj[i][0] > 0) {
+				for (int j = 1; j <= N; j++) {
+					if (adj[i][j] == 1)
+						adj[curr][j] = 1;
+				}
+			}
+
+		}
+
+		adj[curr][0] = 0;
+		for (int k = 1; k <= N; k++) {
+			adj[curr][0] += adj[curr][k];
 		}
 
 	}
