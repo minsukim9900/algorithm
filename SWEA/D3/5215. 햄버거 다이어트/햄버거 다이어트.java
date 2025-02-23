9954fef13777234f69e2bced3fcff6a935b7813d
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-23 17:11:32 +0900

    [D3] Title: 햄버거 다이어트, Time: 112 ms, Memory: 36,708 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,77 +1,50 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
 
 	private static int N, L;
-	private static ArrayList<int[]> info;
-	private static int branch;
-	private static int[] resultScore;
-	private static int[] resultKal;
-	private static int max;
+	private static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());
-			max = Integer.MIN_VALUE;
-			info = new ArrayList<>();
 
-			for (int i = 0; i < N; i++) {
+			dp = new int[N + 1][L + 1];
+
+			int[][] items = new int[N + 1][2];
+			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine());
-				int score = Integer.parseInt(st.nextToken());
-				int kal = Integer.parseInt(st.nextToken());
-				info.add(new int[] { score, kal });
-			}
-			for (int i = 0; i <= N; i++) {
-				branch = i;
-				resultScore = new int[branch];
-				resultKal = new int[branch];
-				combination(0, 0);
-			}
-			
-			System.out.println("#"+t+" "+ max);
-
-		}
-
-	}
-
-	private static void combination(int num, int depth) {
-		
-		if (depth == branch) {
-			
-			int score = 0;
-			int kal = 0;
-			
-			for (int i = 0; i < branch; i++) {
-				score += resultScore[i];
-				kal += resultKal[i];
-			}
-			
-			if(kal > L) return;
-			
-			max = Math.max(max, score);
-			
-			
-		} else {
-
-			for (int i = num; i < N; i++) {
+				items[i][0] = Integer.parseInt(st.nextToken());
+				items[i][1] = Integer.parseInt(st.nextToken());
 				
-				int[] curr = info.get(i);
-				resultScore[depth] = curr[0];
-				resultKal[depth] = curr[1];
-				combination(i + 1, depth + 1);
-
 			}
+
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= L; j++) {
+					dp[i][j] = dp[i-1][j];
+					
+					if(items[i][1] <= j) {
+						dp[i][j] = Math.max(dp[i][j], items[i][0] + dp[i-1][j - items[i][1]]);
+					}
+					
+				}
+			}
+			
+			sb.append("#"+t+" ").append(dp[N][L]).append("\n");
+			
 		}
+		System.out.println(sb.toString());
 
 	}
 
