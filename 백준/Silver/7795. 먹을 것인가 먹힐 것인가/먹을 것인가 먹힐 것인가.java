b63e3e3099fd4ff35bd6444e2f0396eba7d1cff1
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-26 10:32:12 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 1216 ms, Memory: 42968 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int N, M;
+	private static int[] numsA, numsB;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+
+			st = new StringTokenizer(br.readLine());
+
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			numsA = new int[N];
+			st = new StringTokenizer(br.readLine());
+
+			for (int i = 0; i < N; i++) {
+				numsA[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(numsA);
+			
+			numsB = new int[M];
+			st = new StringTokenizer(br.readLine());
+
+			for (int i = 0; i < M; i++) {
+				numsB[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(numsB);
+			
+			System.out.println(search());
+		}
+
+	}
+
+	private static int search() {
+		
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			
+			for (int j = 0; j < M; j++) {
+				
+				if(numsA[i] > numsB[j]) cnt++;
+				else break;
+				
+				
+			}
+
+		}
+		
+		return cnt;
+
+	}
+
+}
