bd200b9a296a80942ffaebd9ccad779a0c9d2b49
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-26 22:40:28 +0900

    [Gold V] Title: 강의실 배정, Time: 444 ms, Memory: 68356 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class Main {
 
+	private static int N, size;
+	private static int[][] lec;
+	private static int[] heap;
 
 	public static void main(String[] args) throws IOException {
 
@@ -10,39 +13,90 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
 
-		int N = Integer.parseInt(br.readLine());
-
-		int[][] lec = new int[N][2];
+		N = Integer.parseInt(br.readLine());
+		lec = new int[N][2];
+		heap = new int[N + 1];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
+
 			lec[i][0] = Integer.parseInt(st.nextToken());
 			lec[i][1] = Integer.parseInt(st.nextToken());
+
 		}
 
 		Arrays.sort(lec, new Comparator<int[]>() {
 
-			@Override
 			public int compare(int[] o1, int[] o2) {
-
 				return o1[0] - o2[0];
 			}
-		});
-		
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.add(lec[0][1]);
+		});
+
+		push(lec[0][1]);
 
 		for (int i = 1; i < N; i++) {
 
-			if (pq.peek() <= lec[i][0]) {
-				pq.poll();
+			if (heap[1] <= lec[i][0]) {
+				pop();
 			}
-			pq.add(lec[i][1]);
+			push(lec[i][1]);
 
 		}
 		
-		System.out.println(pq.size());
+		System.out.println(size);
+
+	}
+
+	private static void swap(int i, int j) {
+		int tmp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = tmp;
+	}
+
+	private static void push(int data) {
+
+		heap[++size] = data;
+
+		int p = size / 2;
+		int ch = size;
+
+		while (ch != 1 && heap[p] > heap[ch]) {
+
+			swap(p, ch);
+
+			ch = p;
+			p = ch / 2;
+
+		}
+
+	}
+
+	private static int pop() {
+		int popNum = heap[1];
+
+		heap[1] = heap[size--];
+
+		int p = 1;
+		int ch = p * 2;
+		if (ch + 1 <= size && heap[ch] > heap[ch + 1]) {
+			ch++;
+		}
+
+		while (ch <= size && heap[p] > heap[ch]) {
+
+			swap(p, ch);
+
+			p = ch;
+			ch = p * 2;
+
+			if (ch + 1 <= size && heap[ch] > heap[ch + 1]) {
+				ch++;
+			}
+
+		}
+
+		return popNum;
 
 	}
 
