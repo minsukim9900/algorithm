c97cee8052a0c2a5873a1c5ccaca3898f80f01a7
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-27 10:49:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 471 ms, Memory: 28,504 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -3,83 +3,80 @@ import java.util.*;
 
 public class Solution {
 
-	private static int D, W, K, h;
-	private static int[][] film;
+	private static int D, W, K;
+	private static int[][] pilm;
+	private static int min;
 	private static int[] A, B;
-	private static int result = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-
 			st = new StringTokenizer(br.readLine());
+
 			D = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-
-			film = new int[D][W];
+			min = 987654321;
+			pilm = new int[D][W];
 
 			A = new int[W];
 			B = new int[W];
 			Arrays.fill(B, 1);
 
-			result = Integer.MAX_VALUE;
-
 			for (int r = 0; r < D; r++) {
 				st = new StringTokenizer(br.readLine());
 				for (int c = 0; c < W; c++) {
-					film[r][c] = Integer.parseInt(st.nextToken());
+					pilm[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
 
 			dfs(0, 0);
 
-			System.out.println("#" + t + " " + result);
+			sb.append("#" + t + " " + min + "\n");
 		}
+		System.out.println(sb.toString());
 
 	}
 
-	private static void dfs(int idx, int cnt) {
-		if (check()) {
-			result = Math.min(result, cnt);
-			return;
-		}
-		if (cnt > result) {
-			return;
-		}
-		if (idx == D) {
-			return;
-		} else {
+	private static void dfs(int r, int cnt) {
 
-			dfs(idx + 1, cnt);
-
-			int[] tmp = film[idx];
-			film[idx] =A;
-			dfs(idx + 1, cnt + 1);
-			film[idx] = tmp;
-			
-			film[idx] = B;
-			dfs(idx + 1, cnt + 1);
-			film[idx] = tmp;
-
+		if (isClear()) {
+			min = Math.min(min, cnt);
 		}
+
+		if (cnt > min || r >= D)
+			return;
+
+		dfs(r + 1, cnt);
+
+		int[] tmpA = pilm[r];
+		pilm[r] = A;
+		dfs(r + 1, cnt + 1);
+		pilm[r] = tmpA;
+
+		int[] tmpB = pilm[r];
+		pilm[r] = B;
+		dfs(r + 1, cnt + 1);
+		pilm[r] = tmpB;
 
 	}
 
-	private static boolean check() {
+	private static boolean isClear() {
 
 		for (int c = 0; c < W; c++) {
-			boolean flag = false;
+
 			int cnt = 1;
+			boolean flag = false;
+			
 			for (int r = 1; r < D; r++) {
 
-				if (film[r][c] == film[r - 1][c]) {
+				if (pilm[r][c] == pilm[r - 1][c]) {
 					cnt++;
 				} else {
 					cnt = 1;
@@ -89,6 +86,7 @@ public class Solution {
 					flag = true;
 					break;
 				}
+
 			}
 
 			if (!flag)
@@ -97,7 +95,6 @@ public class Solution {
 		}
 
 		return true;
-
 	}
 
 }
