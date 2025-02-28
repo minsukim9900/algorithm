f9d658ee2dcc5e1a6161f730988dfb8f753ccead
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-28 11:14:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 104 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,39 +3,45 @@ import java.util.*;
 
 public class Main {
 
+	private static int N, K;
+	private static int[][] items;
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-		int[][] info = new int[N + 1][2];
-		for (int i = 1; i <= N; i++) {
+		items = new int[N][2];
+		for (int i = 0; i < N; i++) {
+
 			st = new StringTokenizer(br.readLine());
-			info[i][0] = Integer.parseInt(st.nextToken()); // weight
-			info[i][1] = Integer.parseInt(st.nextToken()); // cost
+
+			items[i][0] = Integer.parseInt(st.nextToken());
+			items[i][1] = Integer.parseInt(st.nextToken());
+
 		}
 
-		int[][] dp = new int[N + 1][W + 1];
 
-		for (int i = 1; i <= N; i++) {
+		int[] dp = new int[K + 1];
 
-			for (int w = 0; w <= W; w++) {
+		for (int i = 0; i < N; i++) {
 
-				if (info[i][0] <= w) {
-					
-					dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-info[i][0]] + info[i][1]);
-					
-				} else {	
-					dp[i][w] = dp[i - 1][w];
-				}
+			int weight = items[i][0];
+			int cost = items[i][1];
 
+			for (int j = K; j >= weight; j--) {
+				dp[j] = Math.max(dp[j - weight] + cost, dp[j]);
 			}
+			
 		}
 		
-		System.out.println(dp[N][W]);
+		
+		System.out.println(dp[K]);
+
 	}
 }
