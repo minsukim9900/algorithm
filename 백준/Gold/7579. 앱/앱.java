a44d9eaa29dcbf44c8a792016695f33fb97e760e
Kim Minsu <mmsskksw9900@gmail.com>
2025-02-28 13:43:50 +0900

    [Gold III] Title: 앱, Time: 92 ms, Memory: 12068 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -25,50 +25,31 @@ public class Main {
 		}
 
 		st = new StringTokenizer(br.readLine());
+		int total = 0;
 		for (int i = 0; i < N; i++) {
 			items[i][1] = Integer.parseInt(st.nextToken());
+			total += items[i][1];
 		}
 
-		Arrays.sort(items, new Comparator<int[]>() {
+		int[] dp = new int[total + 1];
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[0] - o1[0];
+		for (int i = 0; i < N; i++) {
+
+			for (int j = total; j >= items[i][1]; j--) {
+				dp[j] = Math.max(dp[j], dp[j - items[i][1]] + items[i][0]);
 			}
-		});
-
-		int[] dp = new int[K + 1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0;
-
-		for (int i = 1; i <= Math.min(K, items[0][0]); i++) {
-			dp[i] = items[0][1];
-		}
-
-		int pre = items[0][0];
-
-		for (int i = 1; i < N; i++) {
-
-			int w = items[i][0];
-			if (w > K)
-				w = K;
-			int c = items[i][1];
-			pre += w;
-			if (pre > K)
-				pre = K;
-
-			for (int j = pre; j >= w; j--) {
-				dp[j] = Math.min(dp[j], dp[j - w] + c);
-			}
-
-			for (int j = 1; j <= w; j++) {
-				dp[j] = Math.min(dp[j], c);
-			}
-			
 
 		}
-
-		System.out.println(dp[K]);
+		
+		int ans = 0;
+		for (int i = 0; i <= total; i++) {
+			if(dp[i] >= K) {
+				ans = i;
+				break;
+			}
+		}
+		
+		System.out.println(ans);
 
 	}
 }
