d94b3fdefdb4ec419753bc27c87e42378e772b48
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-01 12:49:16 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 116 ms, Memory: 12432 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -3,48 +3,59 @@ import java.util.*;
 
 public class Main {
 
-    private static int N, M;
-    private static int[][] items;
+	private static int N, M;
+	private static int[][] items;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-        items = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            items[i][0] = Integer.parseInt(st.nextToken()); 
-            items[i][1] = Integer.parseInt(st.nextToken()); 
-            items[i][2] = Integer.parseInt(st.nextToken()); 
-        }
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        ArrayList<int[]> arr = new ArrayList<>();
+		items = new int[N][3];
 
-        for (int[] w : items) {
-            int weight = w[0];
-            int value = w[1];
-            int count = w[2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			items[i][0] = Integer.parseInt(st.nextToken());
+			items[i][1] = Integer.parseInt(st.nextToken());
+			items[i][2] = Integer.parseInt(st.nextToken());
+		}
 
-            for (int k = 1; count > 0; k <<= 1) {
-                int portion = Math.min(k, count);
-                arr.add(new int[] { weight * portion, value * portion });
-                count -= portion;
-            }
-        }
+		Map<Integer, int[]> map = new HashMap<>();
+		ArrayList<int[]> arr = new ArrayList<>();
 
-        int[] dp = new int[M + 1];
-        for (int[] curr : arr) {
-            int w = curr[0];
-            int c = curr[1];
+		for (int[] w : items) {
 
-            for (int j = M; j >= w; j--) {
-                dp[j] = Math.max(dp[j], dp[j - w] + c);
-            }
-        }
+			int v = w[0];
+			int c = w[1];
+			int k = w[2];
 
-        System.out.println(dp[M]);
-    }
+			for (int i = 1; k > 0; i *= 2) {
+				int cnt = Math.min(i, k);
+				arr.add(new int[] { v * cnt, c * cnt });
+				k -= cnt;
+			}
+
+		}
+
+		int[] dp = new int[M + 1];
+
+		for (int i = 0; i < arr.size(); i++) {
+
+			int[] curr = arr.get(i);
+			int w = curr[0];
+			int c = curr[1];
+
+			for (int j = M; j >= w; j--) {
+				dp[j] = Math.max(dp[j], dp[j - w] + c);
+			}
+
+		}
+
+		System.out.println(dp[M]);
+	}
 }
