6a6a3c6b4f33ec7f0291c787376b979ffa950838
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-02 20:07:14 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,51 +4,64 @@ import java.util.*;
 public class Main {
 
 	private static int N, M;
-	private static int[][] maze;
-	private static int[][] check;
-	private static int cnt = 0;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
+	private static int[][] map;
+	private static boolean[][] visited;
+	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		maze = new int[N][M];
-		check = new int[N][M];
+
+		map = new int[N][M];
+		visited = new boolean[N][M];
 
 		for (int r = 0; r < N; r++) {
 			String str = br.readLine();
 			for (int c = 0; c < M; c++) {
-				maze[r][c] = str.charAt(c) - '0';
+				map[r][c] = str.charAt(c) - '0';
 			}
 		}
-
-		bfs(0, 0, 1);
-		System.out.println(check[N - 1][M - 1]);
-
-	}
-
-	public static void bfs(int r, int c, int depth) {
-		Queue<int []> q = new ArrayDeque<>();
-		q.add(new int[] {r, c, depth});
-		check[r][c] = 1;
 		
-		while(!q.isEmpty()) {
-			int[] curr = q.poll();
-			for(int i = 0; i<4; i++) {
-				int nr = curr[0] + dr[i];
-				int nc = curr[1] + dc[i];
-				if(nr >= 0 && nr <N && nc >= 0 && nc<M) {
-					int num = maze[nr][nc];
-					if(num == 1 && check[nr][nc] == 0) {
-						check[nr][nc] = curr[2]+1;
-						q.add(new int[] {nr, nc, curr[2]+1});
-					}
-				}
-			}
-		}
+		System.out.println(bfs());
+
 	}
 
+	private static int bfs() {
+
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] { 0, 0, 1 });
+		visited[0][0] = true;
+
+		int cnt = 1;
+
+		while (!q.isEmpty()) {
+			int[] curr = q.poll();
+			
+			if(curr[0] == N -1 && curr[1] == M -1) {
+				cnt = curr[2];
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+
+				int nr = curr[0] + delta[i][0];
+				int nc = curr[1] + delta[i][1];
+
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] == 1 && !visited[nr][nc]) {
+					visited[nr][nc] = true;
+					q.offer(new int[] { nr, nc, curr[2] + 1 });
+				}
+
+			}
+		}
+		
+		return cnt;
+
+	}
 }
