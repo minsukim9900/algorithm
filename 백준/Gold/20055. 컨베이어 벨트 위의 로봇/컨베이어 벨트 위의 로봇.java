12549cbf6abe93119299b481d1e985802b15529b
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-15 16:38:58 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 304 ms, Memory: 69552 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	private static int N, K;
+	private static int[] info;
+	private static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		info = new int[N << 1];
+		visited = new boolean[N << 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < info.length; i++) {
+			info[i] = Integer.parseInt(st.nextToken());
+		}
+
+		simulate();
+
+	}
+
+	private static void simulate() {
+
+		Queue<Integer> robots = new ArrayDeque<>();
+
+		int idx = 0;
+		int t = 0;
+
+		while (true) {
+
+			int cnt = 0;
+
+			idx = ((idx - 1) + (N << 1)) % (N << 1);
+			
+			if (!robots.isEmpty()) {
+				
+				if (robots.peek() == ((idx + N - 1)) % (N << 1)) {
+					int tmp = robots.poll();
+					visited[tmp] = false;
+				}
+
+				robots = move(robots, idx);
+			}
+			
+			if (info[idx] > 0) {
+				robots.offer(idx);
+				visited[idx] = true;
+				info[idx]--;
+			}
+
+			for (int w : info) {
+				
+				if (w <= 0) {
+					cnt++;
+				}
+				
+			}
+
+			t++;
+			
+			if (cnt >= K) {
+				break;
+			}
+		}
+
+		System.out.println(t);
+
+	}
+
+	private static Queue<Integer> move(Queue<Integer> robots, int idx) {
+		int size = robots.size();
+
+		for (int i = 0; i < size; i++) {
+			int curr = robots.poll();
+
+			int nIdx = (curr + 1) % (N << 1);
+
+			if (info[nIdx] > 0 && !visited[nIdx]) {
+				
+				visited[nIdx] = true;
+				info[nIdx]--;
+				visited[curr] = false;
+				
+				if ((nIdx == ((idx + N - 1)) % (N << 1))) {
+					continue;
+				}
+
+				robots.offer(nIdx);
+			} else {
+				robots.offer(curr);
+			}
+
+		}
+
+
+		return robots;
+	}
+}
