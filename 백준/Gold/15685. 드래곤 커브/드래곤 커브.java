6aecfacfccf5800c2b4d8d070b9ab09fc917bae8
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-16 11:18:38 +0900

    [Gold III] Title: 드래곤 커브, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -2,30 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static int read() throws IOException {
+		int n = System.in.read() & 15, cur;
+		boolean isNegative = (n == 13);
+		if (isNegative) {
+			n = System.in.read() & 15;
+		}
+		while ((cur = System.in.read()) > 32) {
+			n = (n << 3) + (n << 1) + (cur & 15);
+		}
+		return isNegative ? ~n + 1: n;
+	}
 
 	private static boolean[][] map = new boolean[101][101];
 
 	private static int[][] delta = { { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 0 } };
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
 
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int c = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
-			int dir = Integer.parseInt(st.nextToken());
-			int gen = Integer.parseInt(st.nextToken());
+			int c = read();
+			int r = read();
+			int dir = read();
+			int gen = read();
+            
 			int[] info = makeGeneration(dir, gen);
-
 			visit(r, c, info);
 		}
 		int cnt = 0;
+        
 		for (int r = 0; r < 100; r++) {
 			for (int c = 0; c < 100; c++) {
 				if (map[r][c] && map[r + 1][c] && map[r][c + 1] && map[r + 1][c + 1])
@@ -38,8 +49,8 @@ public class Main {
 	}
 
 	private static void visit(int r, int c, int[] info) {
-
 		map[r][c] = true;
+        
 		for (int i = 0; i < info.length; i++) {
 			r += delta[info[i]][0];
 			c += delta[info[i]][1];
@@ -54,13 +65,10 @@ public class Main {
 		info[0] = dir;
 
 		for (int g = 0; g < gen; g++) {
-			
-			
 			for (int i = l - 1; i >= 0; i--) {
 				int nd = (info[i] + 1) & 3;
 				info[l++] = nd;
 			}
-
 		}
 
 		return info;
