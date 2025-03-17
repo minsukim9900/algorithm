651c31723cef9bc830a1a9d3643b325d969215cc
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-17 21:13:24 +0900

    [Silver I] Title: 오목, Time: 68 ms, Memory: 11916 KB -BaekjoonHub

---
+++ 백준/Silver/2072. 오목/오목.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	private static int result = 987654321;
+	private static int[][] map = new int[21][21];
+	private static int[][] board = new int[20][20];
+	private static int[][] delta = { { 1, 0 }, { 1, 1 }, { 1, -1 }, { 0, 1 } };
+
+	private static ArrayList<int[]> dolls = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+
+		int N = Integer.parseInt(br.readLine());
+		int cnt = 1;
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			board[r][c] = cnt++;
+
+			if ((i % 2) == 1) {
+				map[r][c] = 1;
+			} else {
+				map[r][c] = 2;
+			}
+
+			dolls.add(new int[] { r, c });
+		}
+
+		Collections.sort(dolls, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+
+				if (o1[0] == o2[0])
+					return o1[1] - o2[1];
+
+				return o1[0] - o2[0];
+			}
+
+		});
+
+		int[] info = null;
+
+		for (int[] curr : dolls) {
+
+			info = dfs(curr[0], curr[1], 0, map[curr[0]][curr[1]]);
+			if (info[2] == 5 && map[curr[0] - 1][curr[1]] != map[curr[0]][curr[1]]) {
+				search(info, 0);
+			}
+
+			info = dfs(curr[0], curr[1], 1, map[curr[0]][curr[1]]);
+			if (info[2] == 5 && map[curr[0] - 1][curr[1] - 1] != map[curr[0]][curr[1]]) {
+				search(info, 1);
+			}
+
+			info = dfs(curr[0], curr[1], 2, map[curr[0]][curr[1]]);
+			if (info[2] == 5 && map[curr[0] - 1][curr[1] + 1] != map[curr[0]][curr[1]]) {
+				search(info, 2);
+			}
+
+			info = dfs(curr[0], curr[1], 3, map[curr[0]][curr[1]]);
+			if (info[2] == 5 && map[curr[0]][curr[1] - 1] != map[curr[0]][curr[1]]) {
+				search(info, 3);
+			}
+		}
+
+
+		if (result == 987654321)
+			System.out.println(-1);
+		else
+			System.out.println(result);
+	}
+
+	private static void search(int[] info, int state) {
+		
+		int max = 0;
+		
+		if (state == 0) {
+			
+			for (int i = 0; i < 5; i++) {
+				max = Math.max(board[info[0] + i][info[1]], max);
+			}
+
+		} else if (state == 1) {
+			
+			for (int i = 0; i < 5; i++) {
+				max = Math.max(board[info[0] + i][info[1] + i], max);
+			}
+
+		} else if (state == 2) {
+			
+			for (int i = 0; i < 5; i++) {
+				max = Math.max(board[info[0] + i][info[1] - i], max);
+			}
+
+		} else {
+			
+			for (int i = 0; i < 5; i++) {
+				max = Math.max(board[info[0]][info[1] + i], max);
+			}
+			
+		}
+		
+		result = Math.min(max, result);
+
+	}
+
+	private static int[] dfs(int r, int c, int dir, int color) {
+
+		int nr = r;
+		int nc = c;
+		int nd = dir;
+		int cnt = 1;
+		int[] info = null;
+
+		while (true) {
+
+			nr += delta[nd][0];
+			nc += delta[nd][1];
+
+			if (!(nr >= 1 && nr < 20 && nc >= 1 && nc < 20)) {
+				break;
+			}
+
+			if (map[nr][nc] != color) {
+				break;
+			}
+
+			cnt++;
+		}
+
+		info = new int[] { r, c, cnt };
+
+		return info;
+
+	}
+
+}
