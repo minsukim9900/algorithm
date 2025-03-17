8e8243956d9fb39bad2e4ffe8ed1c9806dfb645a
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-17 11:19:02 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 80 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -5,23 +5,29 @@ public class Main {
 
 	private static int N, K, B;
 	private static int[] lights;
+    
+    private static int read() throws IOException {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) >= 48)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+    
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
 
-		st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
+		N = read();
+		K = read();
+		B = read();
 
 		lights = new int[(N >> 5) + 1];
 
 		for (int i = 0; i < B; i++) {
-			int idx = Integer.parseInt(br.readLine());
+			int idx = read();
 			lights[idx >> 5] |= 1 << (idx & 31);
 		}
 		
