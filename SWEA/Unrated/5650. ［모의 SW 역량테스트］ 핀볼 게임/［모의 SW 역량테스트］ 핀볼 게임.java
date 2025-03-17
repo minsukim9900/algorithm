c80f50fa941ce83b462c885abf4a199eea24d723
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-17 15:45:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 1,363 ms, Memory: 50,184 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -5,9 +5,17 @@ public class Solution {
 
 	private static int N, max;
 	private static int[][] board;
-	private static boolean isVisit;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 	private static ArrayList<int[]>[] hole;
+	private static int[][] block = new int[5][4];
+
+	static {
+		block[0] = new int[] { 1, 3, 0, 2 };
+		block[1] = new int[] { 3, 0, 1, 2 };
+		block[2] = new int[] { 2, 0, 3, 1 };
+		block[3] = new int[] { 1, 2, 3, 0 };
+		block[4] = new int[] { 1, 0, 3, 2 };
+	}
 
 	public static void main(String[] args) throws IOException {
 
@@ -21,7 +29,7 @@ public class Solution {
 			max = 0;
 			board = new int[N][N];
 			hole = new ArrayList[5];
-			
+
 			for (int i = 0; i < 5; i++) {
 				hole[i] = new ArrayList<>();
 			}
@@ -42,57 +50,63 @@ public class Solution {
 				}
 			}
 
+
 			for (int[] w : loc) {
 				for (int i = 0; i < 4; i++) {
-					isVisit = false;
-					dfs(w[0], w[1], i, w.clone(), 0);
+					dfs(w[0], w[1], i);
 				}
 			}
-			
+
 			sb.append("#" + t + " " + max + "\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static void dfs(int r, int c, int dir, int[] st, int cnt) {
-		
+	private static void dfs(int sr, int sc, int dir) {
 
-		if (r == st[0] && c == st[1] && isVisit) {
-			max = Math.max(max, cnt);
-			return;
+		int r = sr;
+		int c = sc;
+		int d = dir;
+		int cnt = 0;
+
+		while (true) {
+			r += delta[d][0];
+			c += delta[d][1];
+
+			if ((r == sr && sc == c) || (isRange(r, c) && board[r][c] == -1)) {
+				max = Math.max(max, cnt);
+				break;
+			}
+
+			if (!isRange(r, c)) {
+				d = visitWall(d);
+				cnt++;
+				continue;
+			}
+
+			if (board[r][c] >= 1 && board[r][c] <= 5) {
+				d = block[board[r][c] - 1][d];
+				cnt++;
+				continue;
+			}
+
+			if (board[r][c] >= 6 && board[r][c] <= 10) {
+				int[] info = potal(r, c);
+				r = info[0];
+				c = info[1];
+				continue;
+			}
 		}
-		
-		if(r == st[0] && c == st[1] && !isVisit) {
-			isVisit = true;
-		}
-		
+
+	}
+
+	private static boolean isRange(int r, int c) {
+
 		if (r < 0 || r >= N || c < 0 || c >= N) {
-			dfs(r - delta[dir][0], c - delta[dir][1], visitWall(dir), st, cnt + 1);
-			return;
+			return false;
 		}
 
-		if (board[r][c] == -1) {
-			max = Math.max(max, cnt);
-			return;
-		}
-
-
-		int nd = dir;
-		if (board[r][c] >= 1 && board[r][c] <= 5) {
-			nd = changeDir(board[r][c], nd);
-			dfs(r + delta[nd][0], c + delta[nd][1], nd, st, cnt + 1);
-			return;
-		}
-
-		if (board[r][c] >= 6 && board[r][c] <= 10) {
-			int[] next = potal(r, c);
-			dfs(next[0] + delta[dir][0], next[1] +delta[dir][1], dir, st, cnt);
-			return;
-		}
-		
-		dfs(r + delta[dir][0], c+ delta[dir][1] , dir, st, cnt);
-		return;
-
+		return true;
 	}
 
 	private static int visitWall(int dir) {
@@ -115,64 +129,6 @@ public class Solution {
 		}
 
 		return location;
-
-	}
-
-	private static int changeDir(int block, int dir) {
-
-		switch (block) {
-		case 1: {
-			if (dir == 1)
-				return 3;
-			else if (dir == 2)
-				return 0;
-			else if (dir == 3)
-				return 2;
-			else
-				return 1;
-		}
-		case 2: {
-			if (dir == 1)
-				return 0;
-			else if (dir == 0)
-				return 3;
-			else if (dir == 2)
-				return 1;
-			else
-				return 2;
-		}
-		case 3: {
-			if (dir == 0)
-				return 2;
-			else if (dir == 1)
-				return 0;
-			else if (dir == 2)
-				return 3;
-			else
-				return 1;
-		}
-		case 4: {
-			if (dir == 0)
-				return 1;
-			else if (dir == 1)
-				return 2;
-			else if (dir == 2)
-				return 3;
-			else
-				return 0;
-		}
-		case 5: {
-			if (dir == 0)
-				return 1;
-			else if (dir == 1)
-				return 0;
-			else if (dir == 2)
-				return 3;
-			return 2;
-		}
-		}
-
-		return 0;
 
 	}
 
