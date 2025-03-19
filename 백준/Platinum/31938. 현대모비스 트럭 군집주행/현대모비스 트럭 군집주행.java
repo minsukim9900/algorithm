f38c32c47a17733805cbe143eac0853f26a6c88f
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-19 23:24:43 +0900

    [Platinum V] Title: 현대모비스 트럭 군집주행, Time: 1860 ms, Memory: 187616 KB -BaekjoonHub

--- 백준/Platinum/31938. 현대모비스 트럭 군집주행/현대모비스 트럭 군집주행.java
+++ 백준/Platinum/31938. 현대모비스 트럭 군집주행/현대모비스 트럭 군집주행.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
 
+import javax.lang.model.type.UnionType;
+
 public class Main {
 	private static int N, M;
 	private static ArrayList<int[]>[] arr;
-	private static long[] dist;
-	private static int[] cost;
-	private static long[] result;
+	private static long[] dist, cost;
+	private static int[] p;
+	private static int[] childCnt;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,9 +19,14 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new ArrayList[N + 1];
+		childCnt = new int[N + 1];
 		dist = new long[N + 1];
-		cost = new int[N + 1];
-		result = new long[N + 1];
+		cost = new long[N + 1];
+		p = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			p[i] = i;
+		}
 
 		Arrays.fill(dist, Long.MAX_VALUE);
 
@@ -35,16 +42,44 @@ public class Main {
 			arr[from].add(new int[] { to, w });
 			arr[to].add(new int[] { from, w });
 		}
-		
+
 		dijkstra();
 		
-		long sum = 0;
+		ArrayList<Integer>[] adj = new ArrayList[N+1];
+		for(int i = 1; i<=N; i++) {
+			adj[i] = new ArrayList<>();
+		}
 		
-		for(long w : result) {
-			sum += w;
+		for(int i = 2; i<=N; i++) {
+			adj[p[i]].add(i);
+		}
+		
+		dfs(1, new boolean[N +1], adj);
+		
+		long sum = 0;
+
+		for (int i = 2; i <= N; i++) {
+			long cal = (long) (cost[i] * childCnt[i]) - ((cost[i] / 10) * (childCnt[i] - 1));
+			sum += cal;
 		}
 		System.out.println(sum);
 	}
+	
+	private static int dfs(int v, boolean[] visited, ArrayList<Integer>[] adj) {
+		int cnt = 1;
+		visited[v] = true;
+		
+		for(int w : adj[v]) {
+			if(visited[w]) continue;
+			cnt += dfs(w, visited, adj);
+		}
+		
+		childCnt[v] = cnt;
+		
+		return cnt;
+		
+	}
+
 
 	private static void dijkstra() {
 		boolean[] visited = new boolean[N + 1];
@@ -59,12 +94,11 @@ public class Main {
 			}
 		});
 
-		pq.add(new long[] { 1, 0 });
+		pq.add(new long[] { 1, 0, 0, 1 });
 
 		while (!pq.isEmpty()) {
 			long[] curr = pq.poll();
 			int idx = (int) curr[0];
-			
 			if (visited[idx])
 				continue;
 
@@ -73,13 +107,21 @@ public class Main {
 			for (int[] w : arr[idx]) {
 				if (dist[w[0]] > dist[idx] + w[1]) {
 					dist[w[0]] = dist[idx] + w[1];
-					result[w[0]] = (long) (dist[idx] * 0.9) + w[1];
-					pq.add(new long[] { w[0], dist[w[0]] });
-				} else if (dist[w[0]] == dist[idx] + w[1]) {
-					dist[w[0]] = dist[idx] + w[1];
-					result[w[0]] = Math.min(result[w[0]],(long) (dist[idx] * 0.9) + w[1]);
+					union(idx, w[0]);
+					cost[w[0]] = (long) w[1];
+					pq.offer(new long[] { w[0], dist[w[0]], w[1], idx });
+				} else if (dist[w[0]] == dist[idx] + w[1] && cost[w[0]] > (long) w[1]) {
+						union(idx, w[0]);
+						cost[w[0]] = (long) w[1];
 				}
 			}
 		}
+		
+
 	}
+	
+	private static void union(int x, int y) {
+		p[y] = x;
+	}
+
 }
