cd8af10748a770496b12c50d02795eabb8c0588e
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-21 00:23:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1096 ms, Memory: 205720 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,41 +1,82 @@
 import java.io.*;
 import java.util.*;
 
+import javax.lang.model.type.UnionType;
+
 public class Main {
-
-	private static int count[] = new int[20000001];
-	private static Set<Integer> num = new HashSet<>();
-	private static StringBuilder sb = new StringBuilder();
-
-	public static void cntCard(int tmp) {
-
-		if (num.contains(tmp)) {
-			sb.append(count[tmp]).append(" ");
-		} else {
-			sb.append(0).append(" ");
-		}
-	}
+	private static int N, M;
+	private static int[] arr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
 
 		for (int i = 0; i < N; i++) {
-			int tmp = 10000000 + Integer.parseInt(st.nextToken());
-			num.add(tmp);
-			count[tmp]++;
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int M = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		
+		M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < M; i++) {
-			int tmp = 10000000 + Integer.parseInt(st.nextToken());
-			cntCard(tmp);
+		for(int i = 0; i<M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int leftSide = findNumLeft(num);
+			int rightSide = findNumRight(num);
+			
+			int result = 0;
+			if(leftSide != -1) {
+				result = rightSide - leftSide + 1;
+			}
+			sb.append(result + " ");
 		}
+		
 		System.out.println(sb.toString());
 	}
+
+	private static int findNumRight(int num) {
+		int s = 0;
+		int e = N - 1;
+		int result = -1;
+
+		while (s <= e) {
+			int mid = (s + e) >> 1;
+			if (arr[mid] == num) {
+				result = mid;
+				s = mid + 1;
+			} else if (arr[mid] > num) {
+				e = mid - 1;
+			} else {
+				s = mid + 1;
+			}
+
+		}
+		return result;
+	}
+
+	private static int findNumLeft(int num) {
+		int s = 0;
+		int e = N - 1;
+		int result = -1;
+
+		while (s <= e) {
+			int mid = (s + e) >> 1;
+
+			if (arr[mid] == num) {
+				result = mid;
+				e = mid - 1;
+			} else if (arr[mid] > num) {
+				e = mid - 1;
+			} else {
+				s = mid + 1;
+			}
+		}
+		return result;
+	}
+
 }
