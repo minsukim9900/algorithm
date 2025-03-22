1f30308ab38e88b7cc491cb7e205c886c4a6fd57
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-22 23:34:13 +0900

    [Silver II] Title: 유기농 배추, Time: 96 ms, Memory: 13792 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,67 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	private static int[][] map;
+	private static boolean[][] visited;
+	private static int N, M, K;
+	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-	private static int[][] field;
-	private static boolean[][] check;
-	private static int N, M, napa;
-	private static int count;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+		StringTokenizer st = null;
+
 		int T = Integer.parseInt(br.readLine());
+
 		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			M = Integer.parseInt(st.nextToken());
 			N = Integer.parseInt(st.nextToken());
-			napa = Integer.parseInt(st.nextToken());
-			field = new int[N][M];
-			check = new boolean[N][M];
-			count = 0;
+			K = Integer.parseInt(st.nextToken());
+			visited = new boolean[N][M];
 
-			for (int i = 0; i < napa; i++) {
+			ArrayList<int[]> baechu = new ArrayList<>();
+			map = new int[N][M];
+			for (int i = 0; i < K; i++) {
 				st = new StringTokenizer(br.readLine());
 				int c = Integer.parseInt(st.nextToken());
 				int r = Integer.parseInt(st.nextToken());
-				field[r][c] = 1;
+
+				map[r][c] = 1;
+				baechu.add(new int[] { r, c });
 			}
-			
-			for(int r = 0; r<N; r++){
-				for(int c = 0; c<M; c++) {
-					if(!check[r][c] && field[r][c] == 1) {
-						dfs(r,c);
-						count++;
-					}
+			int cnt = 0;
+			for (int[] curr : baechu) {
+				if(!visited[curr[0]][curr[1]]) {
+					bfs(curr);
+					cnt++;
 				}
 			}
-			
-			sb.append(count).append("\n");
 
+			sb.append(cnt + "\n");
 		}
-		
 		System.out.println(sb.toString());
 
 	}
 
-	public static void dfs(int r, int c) {
-		check[r][c] = true;
+	private static void bfs(int[] curr) {
+		visited[curr[0]][curr[1]] = true;
 
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if(nr >=0 && nr<N && nc >= 0 && nc<M) {
-				int num = field[nr][nc];
-				if(!check[nr][nc] && num == 1) {
-					dfs(nr,nc);
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(curr);
+
+		while (!q.isEmpty()) {
+			int[] c = q.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nr = c[0] + delta[i][0];
+				int nc = c[1] + delta[i][1];
+
+				if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] == 1 && !visited[nr][nc]) {
+					visited[nr][nc] = true;
+					q.add(new int[] { nr, nc });
 				}
 			}
-			
+
 		}
-
 	}
-
 }
