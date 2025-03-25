4e69e847821e4075788133c970dadd1e8c5c55f9
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-25 16:49:39 +0900

    [Gold IV] Title: 우선순위 큐와 시뮬레이션, Time: 860 ms, Memory: 116760 KB -BaekjoonHub

---
+++ 백준/Gold/32718. 우선순위 큐와 시뮬레이션/우선순위 큐와 시뮬레이션.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int N, K, T;
+	private static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()) % K;
+		}
+
+		Arrays.sort(arr);
+
+		st = new StringTokenizer(br.readLine());
+
+		int x = 0;
+		for (int i = 0; i < T; i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			x = (x + tmp) % K;
+			sb.append(getMax(x) + " ");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static int getMax(int x) {
+		int t = K - x;
+		int idx = lowerBound(t);
+		int result1 = -1;
+		int result2 = -1;
+
+		if (idx > 0) {
+			result1 = arr[idx - 1] + x;
+		}
+
+		if (idx < N) {
+			result2 = arr[N - 1] + x - K;
+		}
+		
+		return Math.max(result1, result2);
+
+	}
+
+	private static int lowerBound(int t) {
+		int s = 0;
+		int e = N;
+
+		while (s < e) {
+			int mid = s + ((e - s) >> 1);
+
+			if (arr[mid] >= t) {
+				e = mid;
+			} else {
+				s = mid + 1;
+			}
+		}
+
+		return s;
+	}
+}
