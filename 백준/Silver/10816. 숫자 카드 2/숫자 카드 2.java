dfedcbdaee94ba51b657c82f9d8579bb84df4eee
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-25 11:32:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1088 ms, Memory: 232600 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-import javax.lang.model.type.UnionType;
-
 public class Main {
 	private static int N, M;
 	private static int[] arr;
@@ -11,72 +9,66 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
-
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
 
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
 		Arrays.sort(arr);
-		
+
 		M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			int leftSide = findNumLeft(num);
-			int rightSide = findNumRight(num);
-			
-			int result = 0;
-			if(leftSide != -1) {
-				result = rightSide - leftSide + 1;
-			}
-			sb.append(result + " ");
-		}
 		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			sb.append(upperBound(num) - lowerBound(num) + " ");
+		}
+		sb.append("\n");
 		System.out.println(sb.toString());
 	}
 
-	private static int findNumRight(int num) {
-		int s = 0;
-		int e = N - 1;
-		int result = -1;
+	private static int lowerBound(int v) {
+		int s = 0; // 처음 인덱스
+		int e = N - 1; // 마지막 인덱스
+		int result = 0; // 결과를 담을 곳
 
 		while (s <= e) {
-			int mid = (s + e) >> 1;
-			if (arr[mid] == num) {
-				result = mid;
-				s = mid + 1;
-			} else if (arr[mid] > num) {
-				e = mid - 1;
-			} else {
-				s = mid + 1;
-			}
+			int mid = s + ((e - s) >> 1);
 
+			if (arr[mid] == v) {
+				result = mid;
+				e = mid - 1;
+			} else if (arr[mid] < v) {
+				s = mid + 1;
+			} else {
+				e = mid - 1;
+			}
 		}
+
 		return result;
 	}
 
-	private static int findNumLeft(int num) {
-		int s = 0;
-		int e = N - 1;
-		int result = -1;
-
+	private static int upperBound(int v) {
+		int s = 0; // 처음 인덱스
+		int e = N - 1; // 마지막 인덱스
+		int result = 0; // 결과를 담을 곳
+		
 		while (s <= e) {
-			int mid = (s + e) >> 1;
+			int mid = s + ((e - s) >> 1);
 
-			if (arr[mid] == num) {
-				result = mid;
-				e = mid - 1;
-			} else if (arr[mid] > num) {
-				e = mid - 1;
-			} else {
+			if (arr[mid] == v) {
+				result = mid + 1;
 				s = mid + 1;
+			} else if (arr[mid] < v) {
+				s = mid + 1;
+			} else {
+				e = mid - 1;
 			}
 		}
+		
 		return result;
 	}
-
 }
