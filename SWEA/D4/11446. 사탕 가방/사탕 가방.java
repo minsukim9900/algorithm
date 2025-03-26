b5c5b69346555ebe4559eabcb6de4c3d8ab57f65
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-26 15:21:59 +0900

    [D4] Title: 사탕 가방, Time: 107 ms, Memory: 29,184 KB -BaekjoonHub

--- SWEA/D4/11446. 사탕 가방/사탕 가방.java
+++ SWEA/D4/11446. 사탕 가방/사탕 가방.java
@@ -1,67 +1,62 @@
 import java.io.*;
 import java.util.*;
 
-import javax.lang.model.type.UnionType;
-
 public class Solution {
 	private static int N;
 	private static long M;
-	private static long[] arr;
+	private static long[] candy;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
+
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Long.parseLong(st.nextToken());
-
-			arr = new long[N];
+			candy = new long[N];
+			
 			st = new StringTokenizer(br.readLine());
-
 			for (int i = 0; i < N; i++) {
-				arr[i] = Long.parseLong(st.nextToken());
+				candy[i] = Long.parseLong(st.nextToken());
 			}
-
-			long answer = cal();
-			sb.append("#" + t + " " + answer + "\n");
+			
+			sb.append("#" + t + " " + binarySearch() +"\n");
 		}
-		System.out.println(sb.toString());
+		System.out.print(sb.toString());
+
 	}
 
-	private static long cal() {
-		long answer = 0;
-		long left = 1;
-		long right = 1L << 60;
+	private static long binarySearch() {
+		long s = 1;
+		long e = 1L << 60;
+		long result = 0;
 
-		while (left <= right) {
-			long mid = (left + right) >> 1;
+		while (s <= e) {
+			long mid = s + (e - s) / 2;
+
 			if (check(mid)) {
-				answer = mid;
-				left = mid + 1;
+				result = mid;
+				s = mid + 1;
 			} else {
-				right = mid - 1;
+				e = mid - 1;
 			}
 
 		}
-
-		return answer;
+		return result;
 	}
 
-	private static boolean check(long v) {
-		// 몇 개의 사탕을 넣을 수 있는지
+	private static boolean check(long mid) {
 		long cnt = 0;
-
+		
 		for (int i = 0; i < N; i++) {
-			cnt += arr[i] / v;
-            
-            if(cnt >= M) return true;
+			cnt += candy[i] / mid;
+			
+			if(cnt >= M) return true;
 		}
-
 		return false;
 	}
-
 }
