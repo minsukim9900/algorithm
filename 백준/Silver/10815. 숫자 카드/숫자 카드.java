599a3a136dc77e849a9be539ae51a8f957827abe
Kim Minsu <mmsskksw9900@gmail.com>
2025-03-28 22:22:04 +0900

    [Silver V] Title: 숫자 카드, Time: 940 ms, Memory: 162924 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,33 +1,51 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static boolean[] nums;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        nums = new boolean[20000001];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            nums[Integer.parseInt(st.nextToken()) + 10000000] = true;
-        }
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            if (nums[tmp + 10000000]) {
-                sb.append(1).append(" ");
-            } else {
-                sb.append(0).append(" ");
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
 
-        System.out.println(sb.toString());
-    }
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(nums);
+		int M = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int target = Integer.parseInt(st.nextToken());
+			int ans = binarySearch(nums, target);
+			sb.append(ans + " ");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static int binarySearch(int[] nums, int target) {
+		int s = 0;
+		int e = nums.length - 1;
+		int result = 0;
+
+		while (s <= e) {
+			int mid = s + (e - s) / 2;
+			
+			if(nums[mid] == target) {
+				result = 1;
+				break;
+			}else if(nums[mid] < target) {
+				s = mid + 1;
+			}else if (nums[mid] > target) {
+				e = mid - 1;
+			}
+
+		}
+		return result;
+	}
 }
