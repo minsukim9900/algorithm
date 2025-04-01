0df271a3c627650f1dc7b5601f599cab0221a849
Kim Minsu <mmsskksw9900@gmail.com>
2025-04-02 01:42:57 +0900

    [Gold V] Title: 링크와 스타트, Time: 88 ms, Memory: 12020 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, ans;
+	private static int N, total, ans;
 	private static int[][] arr;
 
 	public static void main(String[] args) throws IOException {
@@ -13,71 +13,31 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N + 1][N + 1];
 		ans = 987654321;
+		total = 0;
 
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				arr[0][j] += arr[i][j];
+				arr[i][0] += arr[i][j];
+				total += arr[i][j];
 			}
 		}
 
-		for (int i = 1; i <= (N >> 1) + 1; i++) {
-			combi(1, 0, i, 0);
-		}
-
+		combi(1, 0, total);
 		System.out.println(ans);
 	}
 
-	private static void combi(int idx, int depth, int M, int pick) {
+	private static void combi(int idx, int cnt, int sum) {
 
-		if (depth == M) {
-			int num = divideTeam(pick);
-			ans = Math.min(ans, num);
+		if (idx == N + 1 || cnt == (N >> 1)) {
+			ans = Math.min(ans, Math.abs(sum));
+
 		} else {
-
-			for (int i = idx; i <= N - M + depth; i++) {
-				pick |= (1 << i);
-				combi(i + 1, depth + 1, M, pick);
-				pick ^= (1 << i);
-			}
+			combi(idx + 1, cnt + 1, sum - arr[0][idx] - arr[idx][0]);
+			combi(idx + 1, cnt, sum);
 		}
 
 	}
-
-	private static int divideTeam(int pick) {
-		StringBuilder sb = new StringBuilder();
-		int sc1 = 0;
-		int sc2 = 0;
-
-		for (int i = 1; i <= N; i++) {
-			if ((pick & (1 << i)) != 0) {
-				sc1 += cal(i, 1, pick);
-			} else {
-				sc2 += cal(i, 0, pick);
-			}
-		}
-
-		return Math.abs(sc2 - sc1);
-	}
-
-	private static int cal(int idx, int num, int pick) {
-		int sum = 0;
-
-		for (int i = 1; i <= N; i++) {
-			if (i == idx)
-				continue;
-
-			if (num == 1) {
-				if ((pick & (1 << i)) != 0) {
-					sum += arr[idx][i];
-				}
-			} else {
-				if ((pick & (1 << i)) == 0) {
-					sum += arr[idx][i];
-				}
-			}
-		}
-		return sum;
-	}
-
 }
