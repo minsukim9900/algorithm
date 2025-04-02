94577b416fbf3df3ae72e2b39596a1d5fc3e67a9
Kim Minsu <mmsskksw9900@gmail.com>
2025-04-02 16:03:18 +0900

    [Platinum V] Title: XOR삼형제 2, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Platinum/10736. XOR삼형제 2/XOR삼형제 2.java
+++ 백준/Platinum/10736. XOR삼형제 2/XOR삼형제 2.java
@@ -2,29 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int t = Integer.parseInt(br.readLine());
-        while(t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            if(n == 1) {
-                sb.append("1\n1\n");
-                continue;
-            }
-            int s = 1;
-            while(s <= n) {
-                s *= 2;
-            }
-            s /= 4;
-            int e = Math.min(3 * s - 1, n);
-            sb.append(e - s + 1).append("\n");
-            for (int i = s; i <= e; i++) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+
+			if (n == 1) {
+				sb.append("1\n1\n");
+				continue;
+			}
+
+			int m = (int) (Math.log(n) / Math.log(2));
+			int s = 1 << (m - 1);
+			int e = Math.min(3 * s - 1, n);
+			
+			sb.append(e - s + 1).append("\n");
+			for (int i = s; i <= e; i++) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb.toString());
+	}
 }
