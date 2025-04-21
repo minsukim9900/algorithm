ce7f7ff5f97c0a459e88e880d7c4fe86fd0e102d
Kim Minsu <mmsskksw9900@gmail.com>
2025-04-21 14:33:33 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 72 ms, Memory: 11900 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 	private static int N, M;
-	private static int[][] board = new int[10][10];
+	private static int[] board = new int[101];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,59 +16,43 @@ public class Main {
 
 		for (int i = 0; i < N + M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken()) - 1;
-			int to = Integer.parseInt(st.nextToken()) - 1;
-			int[] info = seperate(from);
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
 
-			board[info[0]][info[1]] = to;
+			board[from] = to;
 		}
-		
 		System.out.println(simulate());
 	}
 
 	private static int simulate() {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] { 0, 0, 0 });
-		boolean[][] visited = new boolean[10][10];
+		q.offer(new int[] { 1, 0 });
+		boolean[] visited = new boolean[101];
+		visited[1] = true;
 		int rs = 0;
 
 		while (!q.isEmpty()) {
 			int[] curr = q.poll();
-			if (curr[0] == 9 && curr[1] == 9) {
-				rs = curr[2];
+
+			if (curr[0] == 100) {
+				rs = curr[1];
 				break;
 			}
 
 			for (int i = 1; i <= 6; i++) {
-				int nr = curr[0];
-				int nc = curr[1] + i;
+				int next = curr[0] + i;
 
-				if (nc >= 10) {
-					nr = curr[0] + 1;
-					nc = nc - 10;
-				}
-				
-				if (nr < 10 && !visited[nr][nc]) {
-					if (board[nr][nc] != 0) {
-						int[] info = seperate(board[nr][nc]);
-						nr = info[0];
-						nc = info[1];
+				if (next <= 100 && !visited[next]) {
+					if (board[next] != 0) {
+						next = board[next];
 					}
-					visited[nr][nc] = true;
-					q.add(new int[] { nr, nc, curr[2] + 1 });
+					visited[next] = true;
+					q.add(new int[] { next, curr[1] + 1 });
 				}
-
 			}
+
 		}
 
 		return rs;
-	}
-
-	private static int[] seperate(int x) {
-
-		int sr = x / 10;
-		int sc = x % 10;
-
-		return new int[] { sr, sc };
 	}
 }
