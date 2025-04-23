9c142c6c254b9531f8acc51e3caf3d2f88d66ae9
Kim Minsu <mmsskksw9900@gmail.com>
2025-04-23 10:44:43 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27504 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -16,46 +16,28 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-
-		int[] comp = stackSimulate(arr);
-		
-		if(isPossible(arr, comp)) {
-			System.out.println(sb.toString());
-		}else {
-			System.out.println("NO");
-		}
-
+		stackSimulate(arr);
 	}
 	
-	private static boolean isPossible(int[] arr, int[] comp) {
-		for(int i = 0; i<N; i++) {
-			if(arr[i] != comp[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
 	
-	private static int[] stackSimulate(int[] arr) {
-		int[] comp = new int[N];
-		
+	private static void stackSimulate(int[] arr) {
 		int idx = 0;
+		
 		for (int i = 1; i <= N; i++) {
 			stack.push(i);
 			sb.append("+").append("\n");
 
 			while (!stack.isEmpty() && stack.peek() == arr[idx]) {
-				comp[idx++] = stack.pop();
+				stack.pop();
 				sb.append("-").append("\n");
+				idx++;
 			}
-			
-		}
-
-		while (!stack.isEmpty()) {
-			comp[idx++] = stack.pop();
-			sb.append("-").append("\n");
 		}
 		
-		return comp;
+		if(idx == N) {
+			System.out.println(sb.toString());
+		}else {
+			System.out.println("NO");
+		}
 	}
 }
