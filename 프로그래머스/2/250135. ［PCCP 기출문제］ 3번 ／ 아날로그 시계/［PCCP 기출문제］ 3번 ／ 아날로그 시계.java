4444b70e88b70e8055026c4dd47cfbe1d554f7c1
Kim Minsu <mmsskksw9900@gmail.com>
2025-04-25 10:58:25 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.05 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,29 +1,19 @@
-import java.util.HashSet;
-
 class Solution {
-    public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
-        // 1/42421초 단위를 1로 본다 (42421 = lcm(59, 719))
-        final long UNIT = 42421L;
-
-        long start = ((long)h1 * 3600 + m1 * 60 + s1) * UNIT;
-        long end   = ((long)h2 * 3600 + m2 * 60 + s2) * UNIT;
-
-        final long TH = 43200L * 59;
-        final long TM = 3600L  * 719;
-
-        HashSet<Long> events = new HashSet<>();
-
-        for (long i = 0; ; i++) {
-            long t = TH * i;
-            if (t > end) break;
-            if (t >= start) events.add(t);
-        }
-
-        for (long j = 0; ; j++) {
-            long t = TM * j;
-            if (t > end) break;
-            if (t >= start) events.add(t);
-        }
-        return events.size();
+    public int solution(int h1,int m1,int s1,int h2,int m2,int s2) {
+        long start = h1*3600L + m1*60 + s1;
+        long end   = h2*3600L + m2*60 + s2;
+        // 시침↔초침: 43200/719
+        long hi = (start*719 + 43200 - 1) / 43200;
+        long hj = (end  *719) / 43200;
+        long H = Math.max(0, hj - hi + 1);
+        // 분침↔초침: 3600/59
+        long mi = (start*59 + 3600 - 1) / 3600;
+        long mj = (end  *59) / 3600;
+        long M = Math.max(0, mj - mi + 1);
+        // 0초(=12시간 단위) 이벤트 중복 제거
+        int overlap = 0;
+        if (start <= 0 && end >= 0) overlap++;
+        if (start <= 43200 && end >= 43200) overlap++;
+        return (int)(H + M - overlap);
     }
 }
