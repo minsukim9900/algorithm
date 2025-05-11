3e0e30b157a842351a3897d9fe23cd6ff205323b
Kim Minsu <mmsskksw9900@gmail.com>
2025-05-11 15:02:14 +0900

    [level 2] Title: 비밀 코드 해독, Time: 7.98 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,56 +2,37 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int[] choice;
-    private static int answer = 0;
-    private static boolean[][] visited;
-    
+
     public int solution(int n, int[][] q, int[] ans) {
-        choice = new int[5];
+        int m = q.length;
+        int[] bitmasks = new int[m];
         
-        visited = new boolean[q.length][n + 1];
-        
-        for(int i = 0; i < q.length; i++) {
-            for(int j = 0; j < 5; j++) {
-                visited[i][q[i][j]] = true;
+        for(int i = 0; i < m; i++) {
+            for(int num : q[i]) {
+                bitmasks[i] |= 1 << (num - 1);
             }
         }
         
+        int comb = (1 << 5) - 1;
+        int limit = 1 << n;
+        int answer = 0;
         
-        cal(n, ans, 1, 0, new int[5]);
-        return answer;
-    }
-    
-    // 조합을 구하고 q의 갯수가 맞는지 검증
-    private static void cal(int n, int[] ans, int idx, int depth, int[] choice) {
-        if(depth == 5) {
-            if(isMatch(choice, ans)) {
-                answer++;
-            }
-            return;
-        }
-        
-        for(int i = idx; i <= n; i++) {
-            choice[depth] = i;
-            cal(n, ans, i + 1, depth + 1, choice);
-        }
-    }
-    
-    private static boolean isMatch(int[] choice, int[] ans) {
-        for(int i = 0; i < visited.length; i++) {
-            int cnt = 0;
+        while (comb < limit) {
+            boolean isPoss = true;
             
-            for(int j = 0; j < 5; j++) {
-                if(visited[i][choice[j]]) {
-                    cnt++;
+            for(int i = 0; i < m; i++) {
+                if(Integer.bitCount(comb & bitmasks[i]) != ans[i]) {
+                    isPoss = false;
+                    break;
                 }
             }
             
-            if(cnt != ans[i]) {
-                return false;
-            }
+            if(isPoss) answer++;
+            
+            int x = comb & -comb;
+            int y = comb + x;
+            comb = (((comb & ~y) / x) >> 1) | y;
         }
-        
-        return true;
+        return answer;
     }
 }
