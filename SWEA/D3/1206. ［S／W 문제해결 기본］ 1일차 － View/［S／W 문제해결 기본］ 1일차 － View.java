3848077b0dc26bc855ac3fb1e7ff85f7f8f48d00
Kim Minsu <mmsskksw9900@gmail.com>
2025-05-16 10:12:10 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 90 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -3,58 +3,43 @@ import java.util.*;
 
 public class Solution {
 
-	private static int N;
-	private static int[] building;
-	private static int sum;
-
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
 		for (int t = 1; t <= 10; t++) {
-
-			N = Integer.parseInt(br.readLine());
-			building = new int[N];
-			sum = 0;
+			int N = Integer.parseInt(br.readLine());
+			int[] heights = new int[N];
 
 			st = new StringTokenizer(br.readLine());
-
 			for (int i = 0; i < N; i++) {
-				building[i] = Integer.parseInt(st.nextToken());
+				heights[i] = Integer.parseInt(st.nextToken());
 			}
-
-			for (int i = 2; i < N - 2; i++) {
-				isSee(i);
+			
+			int ans = 0;
+			
+			for (int i = 2; i < N - 1; i++) {
+				ans += cntView(i, heights);
 			}
-
-			sb.append("#" + t).append(" ").append(sum+"\n");
-
+			
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 
 	}
 
-	private static boolean isSee(int idx) {
-
+	private static int cntView(int point, int[] heights) {
 		int max = 0;
-
-		for (int i = 2; i > 0; i--) {
-
-			if ((building[idx] - building[idx - i]) < 1)
-				return false;
-
-			if ((building[idx] - building[idx + i]) < 1)
-				return false;
-
-			max = Math.max(max, Math.max(building[idx - i], building[idx + i]));
+		
+		for(int i = point - 2; i < point + 3; i++) {
+			if(i == point) continue;
+			max = Math.max(max, heights[i]);
+			
+			if(heights[i] >= heights[point]) {
+				return 0;
+			}
 		}
-
-		sum += (building[idx] - max);
-
-		return true;
+		return Math.abs(heights[point] - max);
 	}
-
 }
