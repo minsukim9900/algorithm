990efd34ddbd56d30ab1652ca25437a6ed1624d5
Kim Minsu <mmsskksw9900@gmail.com>
2025-05-21 09:42:50 +0900

    [level 2] Title: 양궁대회, Time: 75.75 ms, Memory: 92.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static int N, max;
+    private static int[] infos;
+    private static int[] rs = {-1};
+    
+    public int[] solution(int n, int[] info) {
+        N = n;
+        infos = info;
+        
+        dfs(0, 0, new int[11]);
+        
+        return rs;
+    }
+    
+    private static void dfs(int depth, int n, int[] select) {
+        if(depth == 11 || n >= N) {
+            int tmp = diff(select);
+            if(tmp > 0 && n == N) {
+                if(max < tmp) {
+                    max = tmp;
+                    rs = select.clone();
+                }else if(max == tmp) {
+                    for(int i = 10; i >= 0; i--) {
+                        if(select[i] == 0 && rs[i] == 0) {
+                            continue;
+                        }
+                        
+                        if(select[i] > rs[i]) {
+                            rs = select.clone();
+                            break;
+                        }else {
+                            break;
+                        }
+                    }
+                }
+            }
+            return;
+        }
+        
+        // 해당 점수 과녁을 안쐈을 때
+        dfs(depth + 1, n, select);
+        
+        // 해당 점수 과녁을 1 ~ N개 쐈을 때
+        for(int i = 1; i <= N; i++) {
+            select[depth] = i;
+            dfs(depth + 1, n + i, select);
+            select[depth] = 0;
+        }
+        
+    }
+    
+    private static int diff(int[] select) {
+        int ryan = 0;
+        int pich = 0;
+        for(int i = 0; i <= 10; i++) {
+            if(select[i] == 0 && infos[i] == 0) {
+                continue;
+            }
+            
+            if(select[i] > infos[i]) {
+                ryan += (10 - i);
+            }else {
+                pich += (10 - i);
+            }
+        }
+        
+        return ryan - pich;
+    }
+}
