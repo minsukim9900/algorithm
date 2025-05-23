a5e637a76e7e76daa907aa50e46375fc0cfc7365
Kim Minsu <mmsskksw9900@gmail.com>
2025-05-23 22:04:34 +0900

    [Silver I] Title: 스타트와 링크, Time: 88 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -2,76 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	private static int N, answer;
+	private static int[][] board;
 
-	private static int N;
-	private static int[][] link;
-	private static int min = Integer.MAX_VALUE;
-	private static boolean[] visited;
-	private static int[] result;
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
-		link = new int[N][N];
-		visited = new boolean[N];
-		result = new int[N/2];
-
-		for (int r = 0; r < N; r++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			for (int c = 0; c < N; c++) {
-				link[r][c] = Integer.parseInt(st.nextToken());
-			}
-
-		}
-
-		dfs(0, 0);
-		System.out.println(min);
-	}
-
-	private static void dfs(int num, int depth) {
-
-		if (depth == N / 2) {
-			calS(result);
-		} else {
-
-			for (int i = num; i < N; i++) {
-				if (visited[i])
-					continue;
-				visited[i] = true;
-				result[depth] = i;
-				dfs(i + 1, depth + 1);
-				visited[i] = false;
-
-			}
-
-		}
-	}
-
-	private static void calS(int[] tmp) {
-
-		int start_team = 0;
-		int link_team = 0;
-
-		for (int i = 0; i < N - 1; i++) {
-			
-			for (int j = i+1; j < N; j++) {
-
-				if (visited[i] && visited[j]) {
-					start_team += link[i][j];
-					start_team += link[j][i];
-				}
-
-				if (!visited[i] && !visited[j]) {
-					link_team += link[i][j];
-					link_team += link[j][i];
-				}
-			}
-		}
+		answer = Integer.MAX_VALUE;
+		board = new int[N + 1][N + 1];
 		
-		int diff = Math.abs(start_team-link_team);
-		min = Math.min(min, diff);
+		int total = 0;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				board[0][j] += board[i][j];
+				board[i][0] += board[i][j];
+				total += board[i][j];
+			}
+		}
+		combi(1, 0, 0, total);
+		System.out.println(answer);
+	}
+
+	private static void combi(int idx, int depth, int sum, int total) {
+		if (depth == (N / 2)) {
+			answer = Math.min(answer, Math.abs(total - sum));
+			return;	
+		}
+
+		for (int i = idx; i <= N; i++) {
+			sum += (board[0][i] + board[i][0]);
+			combi(i + 1, depth +1, sum, total);
+			sum -= (board[0][i] + board[i][0]);
+		}
 	}
 
 }
