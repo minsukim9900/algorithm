79fc1587148492d22a5906fc76372a456dbd82e7
Kim Minsu <mmsskksw9900@gmail.com>
2025-06-01 17:41:52 +0900

    [level 2] Title: 문자열 압축, Time: 17.71 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static int answer, M;
+    public int solution(String s) {
+        M = s.length();
+        answer = s.length();
+        
+        for(int i = 1; i <= M; i++) {
+            dfs(0, i, s, "", "", 1);   
+        }
+        return answer;
+    }
+    
+    private static void dfs(int preIdx, int increase, String s, String str, String preStr, int count) {
+        if(preIdx >= M) {
+            if(count > 1) {
+                str += count;
+            }
+            str += preStr;
+            answer = Math.min(answer, str.length());
+            return;
+        }
+        int range = preIdx + increase;
+        if(range > M) {
+            range = M;
+        }
+        String tmp = s.substring(preIdx, range);
+        
+        if(preStr.equals(tmp)) {
+            dfs(range, increase, s, str, preStr, count + 1);
+        }else {
+            if(count > 1) {
+                str += count;
+            }
+            str += preStr;
+            dfs(range, increase, s, str, tmp, 1);
+        }
+    }
+}
