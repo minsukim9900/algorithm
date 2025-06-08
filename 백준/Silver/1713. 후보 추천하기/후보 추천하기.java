e376cf0a9d8a97593f04c2377192cc79f1c3cc55
Kim Minsu <mmsskksw9900@gmail.com>
2025-06-08 15:04:06 +0900

    [Silver I] Title: 후보 추천하기, Time: 88 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -2,81 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, student;
-	private static int[] frame;
-	private static int min, minSugge, minOrder;
-	private static boolean[] check;
+	private static int N, M;
+	private static int[] result;
+	private static int[][] numsInfo;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
-		student = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int idx = 0;
-		int[][] cntSt = new int[student + 1][2];
-		frame = new int[N];
-		check = new boolean[student+1];
+		M = Integer.parseInt(br.readLine());
+		result = new int[N];
+		numsInfo = new int[M + 1][2];
 
-		for (int i = 1; i <= student; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(!check[num]) cntSt[num][0] = i;// 몇번째 추천을 받았는가?
-			check[num] = true;
-			cntSt[num][1]++; // 몇 번 추천을 받았는가?
-			put(cntSt, num, idx++);
+		int cnt = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int curr = Integer.parseInt(st.nextToken());
+
+			if (numsInfo[curr][0] > 0) {
+				numsInfo[curr][0]++;
+				continue;
+			}
+
+			if (cnt == N) {
+				int idx = deleteSearchCandidate();
+				numsInfo[idx][0] = 0;
+				numsInfo[idx][1] = 0;
+			} else {
+				cnt++;
+			}
+			numsInfo[curr][0]++;
+			numsInfo[curr][1] = i;
 		}
-		Arrays.sort(frame);
-		for(int i : frame) {
-			if(i == 0) continue;
-			sb.append(i).append(" ");
+
+		for (int i = 1; i <= M; i++) {
+			if(numsInfo[i][0] > 0) {
+				sb.append(i).append(" ");
+			}
 		}
 		System.out.println(sb.toString());
 
 	}
 
-	public static void put(int[][] cntSt,int num, int idx) {
-		minSugge = cntSt[frame[0]][1];
-		minOrder = cntSt[frame[0]][0];
-		min = frame[0];
-		int frame_idx = 0;
-		for(int i = 0; i<frame.length; i++) {
-			if(frame[i] == num) return;
-		}
-		
-		if (idx < N) {
-			frame[idx] = num;
-		}
-		else {
-			// 추천이 가장 많은 사람이 앞에 오게
-			// 같다면 가장 나중에 추천 받은 사람이 앞에 오게s
-			// 꽉 차 있으면 추천수가 가장 낮고, 가장 먼저 들어온 사람이 out
-			// frame에 나가지면 받았던 추천수는 0으로 초기화
-			
-			
-			for(int i = 0; i<frame.length; i++) {
-				
-				if(minSugge > cntSt[frame[i]][1]) {
-					minSugge = cntSt[frame[i]][1];
-					minOrder = cntSt[frame[i]][0];
-					min = frame[i];
-					frame_idx = i;
-				}else if(minSugge == cntSt[frame[i]][1]) {
-					
-					if(minOrder > cntSt[frame[i]][0] ) {
-						minOrder = cntSt[frame[i]][0];
-						min = frame[i];
-						frame_idx = i;
-					}
-					
-				}
-				
+	private static int deleteSearchCandidate() {
+		int idx = 0;
+		int min = 1001;
+
+		for (int i = 1; i <= M; i++) {
+			if (numsInfo[i][0] == 0) {
+				continue;
 			}
-			cntSt[min][0] = 0;
-			cntSt[min][1] = 0;
-			check[min] = false;
-			frame[frame_idx] = num;
+
+			if (min > numsInfo[i][0]) {
+				min = numsInfo[i][0];
+				idx = i;
+			} else if (min == numsInfo[i][0]) {
+				if (numsInfo[idx][1] > numsInfo[i][1]) {
+					idx = i;
+				}
+			}
 		}
+
+		return idx;
 	}
 }
