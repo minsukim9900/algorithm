5762a5e2cc49b94143450278d0e5502ca8879d7e
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-13 15:33:40 +0900

    [D5] Title: 촛불 이벤트, Time: 240 ms, Memory: 62,848 KB -BaekjoonHub

--- SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
@@ -6,40 +6,41 @@ public class Solution {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
 			N = Long.parseLong(br.readLine());
-			sb.append("#" + t + " " + binarySearch() + "\n");
+			sb.append("#" + t + " ").append(binarySearch()).append("\n");
 		}
 		System.out.print(sb.toString());
-		
-		
-
 	}
 
 	private static long binarySearch() {
-		long s = 0;
-		long e = 10000000001L;
-		long ans = 0;
+		long answer = -1;
+		long s = 1;
+		long e = Integer.MAX_VALUE;
 
-		while (s != e) {
-
-			long mid = s + ((e - s) >> 1);
-			long value = (mid * (mid + 1)) >> 1;
-
-			if (N >= value) {
-				ans = mid;
-				s = mid + 1;
+		while (s <= e) {
+			long mid = (s + e) / 2;
+			long num = cal(mid);
+			
+			
+			if (num == N) {
+				answer = mid;
+				break;
+			} else if (num > N) {
+				e = mid - 1;
 			} else {
-				e = mid;
+				s = mid + 1;
 			}
 		}
-		
-		long v = (ans * (ans + 1)) >> 1;
-		return N == v ? ans : -1;
+		return answer;
+	}
+
+	private static long cal(long v) {
+		return (v * (v + 1)) / 2L;
 	}
 }
