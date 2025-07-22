1ac4f386456745fddd13c70bc6506c9f03f6b59c
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-22 10:50:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 88 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -2,17 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-	private static int N, K, maxL;
-	private static int[][] map;
-	private static boolean[][] visited;
+	private static int N, K, answer;
+	private static int[][] board;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
 
@@ -20,69 +17,76 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-
-			map = new int[N][N];
-			visited = new boolean[N][N];
-			maxL = 0;
-
+			board = new int[N][N];
 			int max = 0;
+			answer = 0;
 
 			for (int r = 0; r < N; r++) {
 				st = new StringTokenizer(br.readLine());
 				for (int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
-					max = Math.max(max, map[r][c]);
+					board[r][c] = Integer.parseInt(st.nextToken());
+					max = Math.max(max, board[r][c]);
 				}
 			}
 
-			for (int r = 0; r < N; r++) {
+			ArrayList<int[]> high = new ArrayList<>();
 
+			/*
+			 * high에 저장할 배열 내용은 가장 높은 정상부분의 정보를 저장한다.
+			 */
+			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					if (max == map[r][c]) {
-						dfs(r, c, map[r][c], 1, true);
+					if (board[r][c] == max) {
+						high.add(new int[] { r, c });
 					}
 				}
-
 			}
 
-			sb.append("#" + t + " " + maxL + "\n");
-
+			for (int[] curr : high) {
+				dfs(curr[0], curr[1], false, 1, new boolean[N][N]);
+			}
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
 		}
-
 		System.out.println(sb.toString());
-
 	}
 
-	private static void dfs(int r, int c, int h, int l, boolean skill) {
-		
-		if (l > maxL)
-			maxL = l;
-
+	private static void dfs(int r, int c, boolean isComplete, int distance, boolean[][] visited) {
 		visited[r][c] = true;
 
-		for (int i = 0; i < 4; i++) {
-
-			int nr = r + delta[i][0];
-			int nc = c + delta[i][1];
-
-			if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]) {
-
-				if (map[nr][nc] < h) {
-
-					dfs(nr, nc, map[nr][nc], l + 1, skill);
-
-				} else if (skill && map[nr][nc] - K < h) {
-
-					dfs(nr, nc, map[r][c] - 1, l + 1, false);
-
+		// 4방위 탐색
+		for (int idx = 0; idx < 4; idx++) {
+			int nr = r + delta[idx][0];
+			int nc = c + delta[idx][1];
+			// 범위에 들어왔는지 && 방문한 적이 없는지
+			if (isRange(nr, nc) && !visited[nr][nc]) {
+				int savePoint = board[nr][nc];
+				// 다음 위치가 현재 위치보다 낮은 지형일 때
+				if (board[nr][nc] < board[r][c]) {
+					visited[nr][nc] = true;
+					dfs(nr, nc, isComplete, distance + 1, visited);
+				} else {
+					// 다음 위치가 현재 위치보다 같거나 높은 지형일 때
+					// 깍은 적이 없고 깎았을 때 내리막길이 될 경우에만
+					if (!isComplete && board[nr][nc] - K < board[r][c]) {
+						isComplete = true;
+						board[nr][nc] = board[r][c] - 1;
+						visited[nr][nc] = true;
+						dfs(nr, nc, isComplete, distance + 1, visited);
+						// 다시 깎았는지 여부 확인 복구
+						isComplete = false;
+					} else {
+						// 이미 한 번 깍은 상태라면
+						answer = Math.max(answer, distance);
+					}
 				}
-
+				// 깎은 지형 다시 복구
+				visited[nr][nc] = false;
+				board[nr][nc] = savePoint;
 			}
-
 		}
-
-		visited[r][c] = false;
-
 	}
 
+	private static boolean isRange(int r, int c) {
+		return (r >= 0 && r < N && c >= 0 && c < N);
+	}
 }
