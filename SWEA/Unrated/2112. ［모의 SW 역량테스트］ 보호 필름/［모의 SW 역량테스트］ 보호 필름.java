c336ab8dd224702bafd0cec89a4fcd42801a145f
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-23 09:42:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 324 ms, Memory: 30,712 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -5,7 +5,7 @@ public class Solution {
 	private static int D, W, K;
 	private static int[][] board;
 	private static int answer;
-	private static int[] A, B;
+	private static int[] treat;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,10 +19,7 @@ public class Solution {
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			answer = D;
-			A = new int[W];
-			B = new int[W];
-			Arrays.fill(A, 0);
-			Arrays.fill(B, 1);
+			treat = new int[D];
 
 			board = new int[D][W];
 			for (int r = 0; r < D; r++) {
@@ -37,9 +34,6 @@ public class Solution {
 		System.out.println(sb.toString());
 	}
 
-	/**
-	 * @param cnt: 약품 투입 갯수
-	 */
 	private static void simulate(int depth, int cnt) {
 		if (cnt >= answer) {
 			return;
@@ -52,46 +46,45 @@ public class Solution {
 			return;
 		}
 
-		// 약품 주입 X
+		treat[depth] = -1;
 		simulate(depth + 1, cnt);
 
-		// 약품 A 주입
-		int[] tmp = board[depth].clone();
-		board[depth] = A;
+		treat[depth] = 0;
 		simulate(depth + 1, cnt + 1);
 
-		// 약품 B 주입
-		board[depth] = B;
+		treat[depth] = 1;
 		simulate(depth + 1, cnt + 1);
-		board[depth] = tmp;
 	}
 
 	private static boolean check() {
 		for (int c = 0; c < W; c++) {
 			int cnt = 1;
-			int state = board[0][c];
+			int state = cellValue(0, c);
 
 			for (int r = 1; r < D; r++) {
-				// 만약 이전 상태와 같다면 cnt 증가
-				if (state == board[r][c]) {
+				if (state == cellValue(r, c)) {
 					cnt++;
-				} else { // 아니라면 cnt 1로 초기화, 상태 바꾸기
-					state = board[r][c];
+				} else {
+					state = cellValue(r, c);
 					cnt = 1;
 				}
 
-				// cnt 갯수가 K개 이상이라면 해당 열은 통과이기 때문에 break;
 				if (cnt >= K) {
 					break;
 				}
 			}
 
-			// 해당 열이 통과하지 못한다면 기준 미만으로 false 반환
 			if (cnt < K) {
 				return false;
 			}
 		}
-		// 모든 열이 통과되었기 때문에 true 반환
 		return true;
+	}
+
+	private static int cellValue(int r, int c) {
+		if (treat[r] >= 0) {
+			return treat[r];
+		}
+		return board[r][c];
 	}
 }
