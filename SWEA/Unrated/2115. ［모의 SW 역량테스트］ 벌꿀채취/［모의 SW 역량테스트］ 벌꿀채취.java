4a9840f1c6c03fa831d9cf83cda3543b85ebffe6
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-25 20:31:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 93 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -27,55 +27,39 @@ public class Solution {
 				}
 			}
 
+			int[] info = new int[3];
 			for (int r = 0; r < dist.length; r++) {
 				for (int c = 0; c < dist[r].length; c++) {
 					dist[r][c] = selectHoneyCal(r, c);
+					if (info[2] < dist[r][c]) {
+						info[2] = dist[r][c];
+						info[0] = r;
+						info[1] = c;
+					}
 				}
 			}
-			sb.append("#").append(t).append(" ").append(cal(dist)).append("\n");
+
+			sb.append("#").append(t).append(" ").append(cal(info, dist)).append("\n");
 		}
-		
+
 		System.out.println(sb.toString());
 	}
 
-	private static int cal(int[][] dist) {
-	    int N = dist.length;
-	    int W = N - M + 1;
-	    int answer = 0;
+	private static int cal(int[] info, int[][] dist) {
+		
+		for(int c = Math.max(0, info[1] - M + 1);
+				c <= Math.min(dist[info[0]].length - 1, info[1] + M -1); c++) {
+			dist[info[0]][c] = 0;
+		}
+		
+		int max = 0;
+		for (int r = 0; r < dist.length; r++) {
+			for (int c = 0; c < dist[r].length; c++) {
+				max = Math.max(max, dist[r][c]);
+			}
+		}
 
-	    // 1) 같은 행 내에서 겹치지 않는 두 구간
-	    for (int r = 0; r < N; r++) {
-	        // suffixMax[c] = c열부터 끝까지의 최대 이익
-	        int[] suffixMax = new int[W + 1];
-	        suffixMax[W] = 0;
-	        for (int c = W - 1; c >= 0; c--) {
-	            suffixMax[c] = Math.max(suffixMax[c+1], dist[r][c]);
-	        }
-	        // c열 구간 + (c+M)열 이후 구간 조합
-	        for (int c = 0; c + M < W; c++) {
-	            answer = Math.max(answer, dist[r][c] + suffixMax[c + M]);
-	        }
-	    }
-
-	    // 2) 서로 다른 행 조합
-	    // bestBelow[r] = 행 r 이상에서의 최대 이익
-	    int[] bestBelow = new int[N + 1];
-	    bestBelow[N] = 0;
-	    for (int r = N - 1; r >= 0; r--) {
-	        int rowMax = 0;
-	        for (int c = 0; c < W; c++) {
-	            rowMax = Math.max(rowMax, dist[r][c]);
-	        }
-	        bestBelow[r] = Math.max(bestBelow[r+1], rowMax);
-	    }
-	    // (r,c) + bestBelow[r+1]
-	    for (int r = 0; r < N; r++) {
-	        for (int c = 0; c < W; c++) {
-	            answer = Math.max(answer, dist[r][c] + bestBelow[r+1]);
-	        }
-	    }
-
-	    return answer;
+		return max + info[2];
 	}
 
 	/**
@@ -85,12 +69,12 @@ public class Solution {
 	private static int selectHoneyCal(int r, int c) {
 		int max = 0;
 		int[] arr = new int[M];
-		
+
 		for (int i = 0; i < M; i++) {
 			arr[i] = board[r][c + i];
 		}
 		int limit = 1 << M;
-		
+
 		for (int mask = 0; mask < limit; mask++) {
 			int sum = 0;
 			int squreSum = 0;
@@ -105,7 +89,7 @@ public class Solution {
 				max = Math.max(max, squreSum);
 			}
 		}
-		
+
 		return max;
 	}
 
