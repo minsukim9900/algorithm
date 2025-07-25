e318628f10536e4d99c3b811cf412a2fca8a9f63
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-25 17:22:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 104 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -2,127 +2,92 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-	private static int N, M, C, max, rs;
-	private static int[][] map;
+	private static int N, M, C;
+	private static int[][] board;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
-
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
-			rs = 0;
-			
-			map = new int[N][N];
+
+			board = new int[N][N];
+			int[][] dist = new int[N][N - M + 1];
 
 			for (int r = 0; r < N; r++) {
 				st = new StringTokenizer(br.readLine());
-
 				for (int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+					board[r][c] = Integer.parseInt(st.nextToken());
 				}
-
 			}
 
-			// 첫 번째 사람이 고르는 벌통
-			for (int r = 0; r < N; r++) {
-
-				for (int c = 0; c < N - M + 1; c++) {
-					int[] honey1 = new int[M];
-
-					for (int i = 0; i < M; i++) {
-						honey1[i] = map[r][c + i];
-					}
-
-					int sum1 = select(honey1);
-
-					// 두 번쨰 사람이 고르는 벌통
-					for (int k = r; k < N; k++) {
-
-						for (int l = 0; l < N - M + 1; l++) {
-							if (k == r && (l <= c + M - 1))
-								continue;
-
-							int[] honey2 = new int[M];
-
-							for (int j = 0; j < M; j++) {
-								honey2[j] = map[k][l + j];
-							}
-							
-							int sum2 = select(honey2);
-							
-							rs = Math.max(rs, sum1 + sum2);
-
-						}
-
-					}
-
+			for (int r = 0; r < dist.length; r++) {
+				for (int c = 0; c < dist[r].length; c++) {
+					dist[r][c] = selectHoneyCal(r, c);
 				}
-
 			}
-			
-			sb.append("#" + t + " " + rs + "\n");
+			sb.append("#").append(t).append(" ").append(cal(dist)).append("\n");
 		}
 		System.out.println(sb.toString());
-
 	}
 
-	private static int select(int[] honey) {
+	private static int cal(int[][] dist) {
+		int answer = 0;
 
-		int sum = 0;
-
-		for (int i : honey) {
-			sum += i;
-		}
-
-		// 선택한 벌통이 기준치보다 크다면 조합으로 대소 비교를 해야 한다.
-		if (sum > C) {
-
-			max = 0;
-			for (int i = 1; i < M; i++) {
-				combi(0, 0, i, 0, 0, honey);
+		for (int r = 0; r < dist.length; r++) {
+			for (int c = 0; c < dist[r].length; c++) {
+				int sum = dist[r][c];
+				for (int nr = r; nr < dist.length; nr++) {
+					for (int nc = 0; nc < dist[r].length; nc++) {
+						if (r == nr && (nc <= c + M)) {
+							continue;
+						}
+						answer = Math.max(answer,  sum + dist[nr][nc]);
+					}
+				}
 			}
-			return max;
+		}
+		return answer;
+	}
 
+	/**
+	 * 선택한 벌통 중에서 가능한 조합 중 가장 큰 값을 리턴
+	 */
+	private static int max;
+
+	private static int selectHoneyCal(int r, int c) {
+		max = 0;
+		int[] arr = new int[M];
+
+		for (int i = 0; i < M; i++) {
+			arr[i] = board[r][c + i];
+		}
+		perm(0, arr, new boolean[arr.length], 0, 0);
+		return max;
+	}
+
+	private static void perm(int depth, int[] arr, boolean[] visited, int sum, int total) {
+		if (depth == arr.length) {
+			max = Math.max(max, total);
 		} else {
-			int result = 0;
-			for (int w : honey) {
-				result += (w * w);
+			for (int i = 0; i < arr.length; i++) {
+				if (visited[i])
+					continue;
+				int num = 0;
+				
+				if (sum + arr[i] <= C) {
+					num = arr[i];
+				}
+				visited[i] = true;
+				perm(depth + 1, arr, visited, sum + num, total + (num * num));
+				visited[i] = false;
 			}
-			
-			return result;
 		}
-
 	}
-
-	private static void combi(int idx, int depth, int end, int result, int sum, int[] honey) {
-		
-		if(sum > C ) return;
-		
-		if (depth == end) {
-
-			max = Math.max(max, result);
-
-		} else {
-
-			for (int i = idx; i < honey.length; i++) {
-				sum += honey[i];
-				result += (honey[i] * honey[i]);
-				combi(i + 1, depth + 1, end, result, sum, honey);
-				result -= (honey[i] * honey[i]);
-				sum -= honey[i];
-			}
-
-		}
-
-	}
-
 }
