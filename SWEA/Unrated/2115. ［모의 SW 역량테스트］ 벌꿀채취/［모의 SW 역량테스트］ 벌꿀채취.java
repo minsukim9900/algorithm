8324bf8f0ab996887298c5b025cfb971ebbd5ecf
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-25 20:02:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 105 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -34,60 +34,79 @@ public class Solution {
 			}
 			sb.append("#").append(t).append(" ").append(cal(dist)).append("\n");
 		}
+		
 		System.out.println(sb.toString());
 	}
 
 	private static int cal(int[][] dist) {
-		int answer = 0;
+	    int N = dist.length;
+	    int W = N - M + 1;
+	    int answer = 0;
 
-		for (int r = 0; r < dist.length; r++) {
-			for (int c = 0; c < dist[r].length; c++) {
-				int sum = dist[r][c];
-				for (int nr = r; nr < dist.length; nr++) {
-					for (int nc = 0; nc < dist[r].length; nc++) {
-						if (r == nr && (nc <= c + M)) {
-							continue;
-						}
-						answer = Math.max(answer,  sum + dist[nr][nc]);
-					}
-				}
-			}
-		}
-		return answer;
+	    // 1) 같은 행 내에서 겹치지 않는 두 구간
+	    for (int r = 0; r < N; r++) {
+	        // suffixMax[c] = c열부터 끝까지의 최대 이익
+	        int[] suffixMax = new int[W + 1];
+	        suffixMax[W] = 0;
+	        for (int c = W - 1; c >= 0; c--) {
+	            suffixMax[c] = Math.max(suffixMax[c+1], dist[r][c]);
+	        }
+	        // c열 구간 + (c+M)열 이후 구간 조합
+	        for (int c = 0; c + M < W; c++) {
+	            answer = Math.max(answer, dist[r][c] + suffixMax[c + M]);
+	        }
+	    }
+
+	    // 2) 서로 다른 행 조합
+	    // bestBelow[r] = 행 r 이상에서의 최대 이익
+	    int[] bestBelow = new int[N + 1];
+	    bestBelow[N] = 0;
+	    for (int r = N - 1; r >= 0; r--) {
+	        int rowMax = 0;
+	        for (int c = 0; c < W; c++) {
+	            rowMax = Math.max(rowMax, dist[r][c]);
+	        }
+	        bestBelow[r] = Math.max(bestBelow[r+1], rowMax);
+	    }
+	    // (r,c) + bestBelow[r+1]
+	    for (int r = 0; r < N; r++) {
+	        for (int c = 0; c < W; c++) {
+	            answer = Math.max(answer, dist[r][c] + bestBelow[r+1]);
+	        }
+	    }
+
+	    return answer;
 	}
 
 	/**
 	 * 선택한 벌통 중에서 가능한 조합 중 가장 큰 값을 리턴
 	 */
-	private static int max;
 
 	private static int selectHoneyCal(int r, int c) {
-		max = 0;
+		int max = 0;
 		int[] arr = new int[M];
-
+		
 		for (int i = 0; i < M; i++) {
 			arr[i] = board[r][c + i];
 		}
-		perm(0, arr, new boolean[arr.length], 0, 0);
+		int limit = 1 << M;
+		
+		for (int mask = 0; mask < limit; mask++) {
+			int sum = 0;
+			int squreSum = 0;
+			for (int i = 0; i < M; i++) {
+				if ((mask & (1 << i)) != 0) {
+					sum += arr[i];
+					squreSum += (arr[i] * arr[i]);
+				}
+			}
+
+			if (sum <= C) {
+				max = Math.max(max, squreSum);
+			}
+		}
+		
 		return max;
 	}
 
-	private static void perm(int depth, int[] arr, boolean[] visited, int sum, int total) {
-		if (depth == arr.length) {
-			max = Math.max(max, total);
-		} else {
-			for (int i = 0; i < arr.length; i++) {
-				if (visited[i])
-					continue;
-				int num = 0;
-				
-				if (sum + arr[i] <= C) {
-					num = arr[i];
-				}
-				visited[i] = true;
-				perm(depth + 1, arr, visited, sum + num, total + (num * num));
-				visited[i] = false;
-			}
-		}
-	}
 }
