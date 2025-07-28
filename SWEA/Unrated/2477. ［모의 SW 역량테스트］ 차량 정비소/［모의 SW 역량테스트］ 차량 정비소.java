1c3dd0a174086ee68a6d5a8b4e90e96443f9eb87
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-28 20:52:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 차량 정비소, Time: 178 ms, Memory: 37,180 KB -BaekjoonHub

--- SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
+++ SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
@@ -5,6 +5,11 @@ public class Solution {
 	private static int N, M, K, A, B;
 	private static int[] infoA, infoB;
 	private static boolean[] visitedA, visitedB;
+	private static Queue<int[]> guest = new ArrayDeque<>();
+	private static PriorityQueue<int[]> workA;
+	private static PriorityQueue<int[]> workB;
+	private static Queue<int[]> waitB;
+	private static int count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,119 +25,132 @@ public class Solution {
 			A = Integer.parseInt(st.nextToken());
 			B = Integer.parseInt(st.nextToken());
 
-			infoA = new int[N + 1];
-			visitedA = new boolean[N + 1];
+			init();
 
 			st = new StringTokenizer(br.readLine());
 			for (int i = 1; i <= N; i++) {
 				infoA[i] = Integer.parseInt(st.nextToken());
 			}
 
-			infoB = new int[M + 1];
-			visitedB = new boolean[M + 1];
-
 			st = new StringTokenizer(br.readLine());
 			for (int i = 1; i <= M; i++) {
 				infoB[i] = Integer.parseInt(st.nextToken());
 			}
 
-			Queue<int[]> guest = new ArrayDeque<>();
 			st = new StringTokenizer(br.readLine());
 			for (int i = 1; i <= K; i++) {
 				int time = Integer.parseInt(st.nextToken());
 				guest.add(new int[] { i, time });
 			}
-			sb.append("#").append(t).append(" ").append(simulate(guest)).append("\n");
+			sb.append("#").append(t).append(" ").append(simulate()).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-
-	private static int simulate(Queue<int[]> guest) {
+	
+	private static int simulate() {
 		int time = guest.peek()[1];
 		int result = 0;
 
-		PriorityQueue<int[]> workA = new PriorityQueue<>(new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if (o1[1] == o2[1]) {
-					return o1[2] - o2[2];
-				}
-				return o1[1] - o2[1];
-			}
-		});
-
-		Queue<int[]> waitB = new ArrayDeque<>();
-
-		PriorityQueue<int[]> workB = new PriorityQueue<>(new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1] - o2[1];
-			}
-		});
-
-		int count = 0;
 		while (count < K) {
 			// 접수 창구가 끝났다면 차량 정비 대기 줄에 진입
-			while (!workA.isEmpty() && workA.peek()[1] <= time) {
-				int[] curr = workA.poll();
-				
-				// 정비 창구 접수 종료했기 때문에 false 처리
-				visitedA[curr[2]] = false;
-				waitB.add(curr);
-			}
+			moveToRepairWaitingLine(time);
 
 			// 손님이 도착 한 시간에 접수 창구가 비어있다면 접수 창구에 넣기
 			// 접수 창구가 비어있지 않다면 while 문 종료
-			while (!guest.isEmpty() && guest.peek()[1] <= time) {
-				boolean isPoss = false;
+			assignArrivalsToReceptions(time);
 
-				for (int i = 1; i <= N; i++) {
-					if (visitedA[i])
-						continue;
-
-					int[] curr = guest.poll();
-					visitedA[i] = true;
-					isPoss = true;
-					workA.add(new int[] { curr[0], time + infoA[i], i });
-					break;
-				}
-				if (!isPoss) {
-					break;
-				}
-			}
-
-			while (!workB.isEmpty() && workB.peek()[1] <= time) {
-				int[] curr = workB.poll();
-				visitedB[curr[3]] = false;
-				count++;
-				if (curr[2] == A && curr[3] == B) {
-					result += curr[0];
-				}
-			}
+			// 정비 창구에서 할 일을 끝낸 사람은
+			// 설문조사 기록 후 빼기
+			result += processRepairCompletions(time);
 
 			// 차량 정비 대기하는 사람들을 순서대로 차량 정비 시작
-			while (!waitB.isEmpty()) {
-				boolean isPoss = false;
-
-				for (int i = 1; i <= M; i++) {
-					if (visitedB[i])
-						continue;
-					int[] curr = waitB.poll();
-					isPoss = true;
-					visitedB[i] = true;
-					workB.add(new int[] { curr[0], time + infoB[i], curr[2], i });
-					break;
-				}
-
-				if (!isPoss) {
-					break;
-				}
-			}
-
+			startRepairsForWaitingGuests(time);
 			time++;
 		}
 		return result == 0 ? -1 : result;
+	}
+
+	private static void moveToRepairWaitingLine(int time) {
+		while (!workA.isEmpty() && workA.peek()[1] <= time) {
+			int[] curr = workA.poll();
+			// 정비 창구 접수 종료했기 때문에 false 처리
+			visitedA[curr[2]] = false;
+			waitB.add(curr);
+		}
+	}
+
+	private static void assignArrivalsToReceptions(int time) {
+		while (!guest.isEmpty() && guest.peek()[1] <= time) {
+			boolean isPoss = false;
+
+			for (int i = 1; i <= N; i++) {
+				// 만약 현재 index 창구가 접수 중이라면 건너띄기
+				if (visitedA[i])
+					continue;
+
+				// 비어있는 접수창구로 가고
+				// workA 우선순위큐에 넣기
+				int[] curr = guest.poll();
+				visitedA[i] = true;
+				isPoss = true;
+				workA.add(new int[] { curr[0], time + infoA[i], i });
+				break;
+			}
+
+			// 만약 모든 접수 창구가 이용중이라면 대기하기
+			if (!isPoss) {
+				break;
+			}
+		}
+	}
+
+	private static int processRepairCompletions(int time) {
+		int result = 0;
+
+		while (!workB.isEmpty() && workB.peek()[1] <= time) {
+			int[] curr = workB.poll();
+			visitedB[curr[3]] = false;
+			count++;
+			if (curr[2] == A && curr[3] == B) {
+				result += curr[0];
+			}
+		}
+		return result;
+	}
+
+	private static void startRepairsForWaitingGuests(int time) {
+		while (!waitB.isEmpty()) {
+			boolean isPoss = false;
+
+			for (int i = 1; i <= M; i++) {
+				// 해당 정비 창구가 이용중이라면 건너띄기
+				if (visitedB[i])
+					continue;
+
+				// 해당 정비 창구에
+				int[] curr = waitB.poll();
+				isPoss = true;
+				visitedB[i] = true;
+				workB.add(new int[] { curr[0], time + infoB[i], curr[2], i });
+				break;
+			}
+
+			// 모든 정비 창구가 이용중이라면 대기
+			if (!isPoss) {
+				break;
+			}
+		}
+	}
+
+	private static void init() {
+		infoA = new int[N + 1];
+		visitedA = new boolean[N + 1];
+
+		workA = new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[2] - b[2] : a[1] - b[1]);
+		workB = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+		waitB = new ArrayDeque<>();
+		count = 0;
+		infoB = new int[M + 1];
+		visitedB = new boolean[M + 1];
 	}
 }
