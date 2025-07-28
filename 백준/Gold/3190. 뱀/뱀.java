4f09f3ecedd4983c559a43f3874dc633d89a3747
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-28 11:30:47 +0900

    [Gold IV] Title: 뱀, Time: 72 ms, Memory: 11920 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -2,92 +2,86 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	private static int N, K, L;
 	private static int[][] board;
-	private static int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
+	private static int[][] delta = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
 		K = Integer.parseInt(br.readLine());
-
 		board = new int[N][N];
+
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-
-			board[x][y] = 1;
+			int r = Integer.parseInt(st.nextToken()) - 1;
+			int c = Integer.parseInt(st.nextToken()) - 1;
+			board[r][c] = 1;
 		}
 
 		L = Integer.parseInt(br.readLine());
-		Queue<int[]> order = new ArrayDeque<>();
-		
+		int[][] orders = new int[L][2];
 		for (int i = 0; i < L; i++) {
 			st = new StringTokenizer(br.readLine());
-			int time = Integer.parseInt(st.nextToken());
-			String dir = st.nextToken();
-			
-			if (dir.equals("D")) {
-				order.add(new int[] { time, 1 });
-			} else {
-				order.add(new int[] { time, -1 });
+			int x = Integer.parseInt(st.nextToken());
+			int dir = 1;
+			String str = st.nextToken();
+
+			if (str.equals("L")) {
+				dir = -1;
 			}
-			
+
+			orders[i][0] = x;
+			orders[i][1] = dir;
 		}
-		
-		
-		System.out.println(game(order));
-		
+		System.out.println(simulate(orders));
 	}
-	
-	private static int game(Queue<int[]> order) {
-		
+
+	private static int simulate(int[][] orders) {
+		int time = 0;
 		Queue<int[]> snake = new ArrayDeque<>();
-		
+		board[0][0] = -1;
+		snake.add(new int[] { 0, 0 });
+
+		int idx = 0;
 		int r = 0;
 		int c = 0;
-		board[r][c] = 2;
-		
-		snake.add(new int[] {0,0});
-		int time = 0;
-		int idx = 0;
-		while(true) {
-			
-			r+=dir[idx][0];
-			c+=dir[idx][1];
-			
-			if(!(r >= 0 && r < N && c>= 0 && c<N) || board[r][c] == 2) {
+		int dir = 0;
+		while (true) {
+
+			if (idx < orders.length && time == orders[idx][0]) {
+				dir = (dir + orders[idx][1] + 4) % 4;
+				idx++;
+			}
+
+			r += delta[dir][0];
+			c += delta[dir][1];
+
+			if (!isRange(r, c)) {
 				time++;
-				return time;
-			}
-			if(r >= 0 && r < N && c>= 0 && c<N) {
-				if(board[r][c] == 0) {
-					int[] tail = snake.poll();
-					int nr = tail[0];
-					int nc = tail[1];
-					board[nr][nc] = 0;
+				break;
+			} else {
+				if (board[r][c] < 0) {
+					time++;
+					break;
+				} else {
+					snake.add(new int[] { r, c });
+					if (board[r][c] == 0) {
+						int[] tail = snake.poll();
+						board[tail[0]][tail[1]] = 0;
+					}
+					board[r][c] = -1;
 				}
-				board[r][c] = 2;
-				snake.add(new int[] {r, c});
 			}
-			
 			time++;
-			
-			if(!order.isEmpty() && time == order.peek()[0]) {
-				idx += order.poll()[1];
-				if(idx == 4) {
-					idx = 0;
-				}else if(idx == -1) {
-					idx = 3;
-				}
-			}
-			
 		}
+		return time;
+	}
+
+	private static boolean isRange(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
 	}
 }
