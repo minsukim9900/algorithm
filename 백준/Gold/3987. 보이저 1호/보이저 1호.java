887a36213cf50dac2881b08ce9036fbff90e04b3
Kim Minsu <mmsskksw9900@gmail.com>
2025-07-31 13:54:20 +0900

    [Gold V] Title: 보이저 1호, Time: 200 ms, Memory: 44364 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.java
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.java
@@ -2,101 +2,92 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int N, M;
-	private static char[][] board;
-	private static int[][] delta = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+    private static int N, M;
+    private static char[][] board;
+    // 0: U, 1: R, 2: D, 3: L
+    private static int[][] delta = {
+        { -1, 0 },  // U
+        {  0, 1 },  // R
+        {  1, 0 },  // D
+        {  0,-1 }   // L
+    };
+    private static char[] dirChar = { 'U', 'R', 'D', 'L' };
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new char[N][M];
+        for (int r = 0; r < N; r++) {
+            board[r] = br.readLine().toCharArray();
+        }
+        st = new StringTokenizer(br.readLine());
+        int sr = Integer.parseInt(st.nextToken()) - 1;
+        int sc = Integer.parseInt(st.nextToken()) - 1;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        int bestDir = 0, maxTime = -1;
+        // 첫 무한 방향을 기록할 변수. 없다면 -1
+        int infiniteDir = -1;
 
-		board = new char[N][M];
+        for (int d = 0; d < 4; d++) {
+            int t = simulate(sr, sc, d);
+            if (t == -1) {
+                // 무한 전파 최초 발견
+                infiniteDir = d;
+                break;
+            } else if (t > maxTime) {
+                maxTime = t;
+                bestDir = d;
+            }
+        }
 
-		for (int r = 0; r < N; r++) {
-			String str = br.readLine();
-			for (int c = 0; c < M; c++) {
-				board[r][c] = str.charAt(c);
-			}
-		}
+        if (infiniteDir != -1) {
+            System.out.println(dirChar[infiniteDir]);
+            System.out.println("Voyager");
+        } else {
+            System.out.println(dirChar[bestDir]);
+            System.out.println(maxTime);
+        }
+    }
 
-		st = new StringTokenizer(br.readLine());
-		int sr = Integer.parseInt(st.nextToken()) - 1;
-		int sc = Integer.parseInt(st.nextToken()) - 1;
-		int[] result = simulate(sr, sc);
+    /**
+     * (r, c)에서 initialDir 방향으로 전파를 시작했을 때,
+     * 무한루프 발생 시 -1을 리턴, 아니면 소요된 시간(이동한 칸 수)을 리턴.
+     */
+    private static int simulate(int r, int c, int initialDir) {
+        boolean[][][] visited = new boolean[N][M][4];
+        int cr = r, cc = c, dir = initialDir, time = 0;
 
-		switch (result[0]) {
-		case 0:
-			System.out.println("U");
-			break;
-		case 1:
-			System.out.println("R");
-			break;
-		case 2:
-			System.out.println("D");
-			break;
-		case 3:
-			System.out.println("L");
-			break;
-		}
-		if (result[1] == -1) {
-			System.out.println("Voyager");
-			return;
-		}
-		System.out.println(result[1]);
-	}
+        while (true) {
+            // 한 칸 이동
+            int nr = cr + delta[dir][0];
+            int nc = cc + delta[dir][1];
+            time++;
 
-	private static int[] simulate(int r, int c) {
-		int dir = 0;
-		int max = 0;
+            // 영역 밖이거나 블랙홀(C)이면 종료
+            if (nr < 0 || nr >= N || nc < 0 || nc >= M || board[nr][nc] == 'C') {
+                break;
+            }
 
-		Queue<int[]> q = new ArrayDeque<>();
+            // 이미 같은 위치·방향으로 왔었다면 무한 전파
+            if (visited[nr][nc][dir]) {
+                return -1;
+            }
+            visited[nr][nc][dir] = true;
 
-		for (int i = 0; i < 4; i++) {
-			q.add(new int[] { r, c, i, 1 });
+            // 거울에 맞으면 방향 반사
+            char cell = board[nr][nc];
+            if (cell == '/') {
+                dir ^= 1;       // U↔R, D↔L
+            } else if (cell == '\\') {
+                dir = 3 - dir;  // U↔L, R↔D
+            }
 
-			while (!q.isEmpty()) {
-				int[] curr = q.poll();
+            cr = nr;
+            cc = nc;
+        }
 
-				int nr = curr[0] + delta[curr[2]][0];
-				int nc = curr[1] + delta[curr[2]][1];
-
-				if (curr[3] > 500 * 500 * 2) {
-					return new int[] { i, -1 };
-				}
-
-				if (max < curr[3]) {
-					dir = i;
-					max = curr[3];
-				}
-
-				if (isRange(nr, nc) && board[nr][nc] != 'C') {
-					if (board[nr][nc] == '/') {
-						int num = 1;
-						if (curr[2] % 2 == 0) {
-							num = -1;
-						}
-						curr[2] = (curr[2] + ((-1) * num) + 4) % 4;
-					} else if (board[nr][nc] == '\\') {
-						int num = 1;
-						if (curr[2] % 2 == 0) {
-							num = -1;
-						}
-						curr[2] = (curr[2] + (1 * num) + 4) % 4;
-					}
-					q.add(new int[] { nr, nc, curr[2], curr[3] + 1 });
-				}
-			}
-		}
-		return new int[] { dir, max };
-	}
-
-	private static boolean isRange(int r, int c) {
-		return r >= 0 && r < N && c >= 0 && c < M;
-	}
-
+        return time;
+    }
 }
