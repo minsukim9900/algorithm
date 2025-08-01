ce618f85d2f0e1d934909d0c82f97fba1015422a
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-01 16:30:10 +0900

    [Gold III] Title: 경사로, Time: 84 ms, Memory: 12780 KB -BaekjoonHub

---
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, L;
+    static int[][] map;
+
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (canPass(map[i])) result++;
+        }
+
+        for (int j = 0; j < N; j++) {
+            int[] col = new int[N];
+            for (int i = 0; i < N; i++) col[i] = map[i][j];
+            if (canPass(col)) result++;
+        }
+
+        System.out.println(result);
+    }
+
+    private static boolean canPass(int[] line) {
+        boolean[] used = new boolean[N]; // 경사로 설치한 칸 표시
+
+        for (int i = 0; i < N - 1; i++) {
+            int curr = line[i];
+            int next = line[i + 1];
+
+            if (curr == next) continue;
+
+            if (next - curr == 1) {
+                for (int k = i; k > i - L; k--) {
+                    if (k < 0 || line[k] != curr || used[k]) return false;
+                    used[k] = true;
+                }
+            }
+            else if (curr - next == 1) {
+                for (int k = i + 1; k <= i + L; k++) {
+                    if (k >= N || line[k] != next || used[k]) return false;
+                    used[k] = true;
+                }
+                i += L - 1;
+            } else {
+                return false; // 높이 차 >=2
+            }
+        }
+        return true;
+    }
+}
