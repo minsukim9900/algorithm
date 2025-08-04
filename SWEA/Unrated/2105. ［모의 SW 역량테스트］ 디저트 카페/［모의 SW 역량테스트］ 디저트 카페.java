d7b210a2c0bb1de4366d6ff279fd2eb17c6dce91
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-04 19:21:28 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 289 ms, Memory: 35,684 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -2,128 +2,87 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
 	private static int N, max;
+	private static int[][] board;
 	private static int[][] delta = { { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
-	private static int[][] map;
 	private static boolean[][] visited;
+	private static boolean[] dir;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
-
 		for (int t = 1; t <= T; t++) {
-
+			N = Integer.parseInt(br.readLine());
+			board = new int[N][N];
 			max = 0;
 
-			N = Integer.parseInt(br.readLine());
-
-			map = new int[N][N];
-
 			for (int r = 0; r < N; r++) {
-
 				st = new StringTokenizer(br.readLine());
-
 				for (int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+					board[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
 
-			visited = new boolean[N][N];
-			boolean[] visitNum = new boolean[101];
 			for (int r = 0; r < N; r++) {
-
 				for (int c = 0; c < N; c++) {
-					
-					if(r == 0 && c== 0) continue;
-					if(r == N-1 && c == 0) continue;
-					if(r == 0 && c == N-1) continue;
-					if(r == N-1 && c == N-1) continue;
-					
-					visited[r][c] = true;
-					int[] curr = new int[] { r, c };
-					visitNum[map[r][c]] = true;
-					dfs(curr, curr, visitNum, new boolean[4], -1, 0);
-					visited[r][c] = false;
-					visitNum[map[r][c]] = false;
+					visited = new boolean[N][N];
+					dir = new boolean[4];
+					dfs(r, c, r, c, 0, 0, new boolean[101]);
 				}
 			}
-			
-			if(max == 0) max = -1;
-			
-			sb.append("#" + t + " " + max + "\n");
+			sb.append("#").append(t).append(" ").append(max = max == 0 ? -1 : max).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static void dfs(int[] curr, int[] p, boolean[] visitNum, boolean[] visitDir, int preDir, int cnt) {
-		
-		// 방향을 두번 이상 바꾸었으며, 현재 위치가 처음 위치랑 같을 때
-		if (cntVisitDir(visitDir) >= 2 && curr[0] == p[0] && curr[1] == p[1]) {
+	private static void dfs(int r, int c, int sr, int sc, int count, int preDir, boolean[] tasted) {
+		if (count > 0 && r == sr && c == sc) {
+			dir[preDir] = true;
+			if (check(sr, sc)) {
+				max = Math.max(max, count);
+			}
+			dir[preDir] = false;
+			return;
+		}
 
+		for (int i = 0; i < 4; i++) {
+			if (dir[i])
+				continue;
 
-			max = Math.max(max, cnt + 2);
-			
-		} else {
-			
-			// 방향을 두 번 이상 바꾸면, 현재 위치 방문처리 한 것을 초기화 해준다.
-			if (cntVisitDir(visitDir) == 2) {
-				
-				visited[p[0]][p[1]] = false;
+			if (preDir != i) {
+				dir[preDir] = true;
 			}
 
-			for (int i = 0; i < 4; i++) {
+			int nr = r + delta[i][0];
+			int nc = c + delta[i][1];
 
-				int nr = curr[0] + delta[i][0];
-				int nc = curr[1] + delta[i][1];
-
-				if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visitDir[i] && !visited[nr][nc]) {
-					if (nr == p[0] && nc == p[1]) {
-
-						dfs(new int[] { nr, nc }, p, visitNum, visitDir, i, cnt - 1);
-
-					} else {
-
-						if (!visitNum[map[nr][nc]]) {
-
-							if (preDir != -1 && preDir != i) {
-								visitDir[preDir] = true;
-							}
-							visited[nr][nc] = true;
-							visitNum[map[nr][nc]] = true;
-
-							dfs(new int[] { nr, nc }, p, visitNum, visitDir, i, cnt + 1);
-
-							if (preDir != -1) {
-								visitDir[preDir] = false;
-							}
-							visited[nr][nc] = false;
-							visitNum[map[nr][nc]] = false;
-
-						}
-					}
-
-				}
-
+			if (isRange(nr, nc) && !visited[nr][nc] && !tasted[board[nr][nc]]) {
+				visited[nr][nc] = true;
+				tasted[board[nr][nc]] = true;
+				dfs(nr, nc, sr, sc, count + 1, i, tasted);
+				visited[nr][nc] = false;
+				tasted[board[nr][nc]] = false;
 			}
 
+			if (dir[preDir]) {
+				dir[preDir] = false;
+			}
 		}
 
 	}
 
-	private static int cntVisitDir(boolean[] visitDir) {
-
-		int cnt = 0;
-		for (boolean b : visitDir) {
-			if (b)
-				cnt++;
+	private static boolean check(int sr, int sc) {
+		for (boolean v : dir) {
+			if (!v)
+				return false;
 		}
-
-		return cnt;
+		return true;
 	}
 
+	private static boolean isRange(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
 }
