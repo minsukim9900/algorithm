82c2c4e133514cd8d982fbdc94b90142cb8b4412
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-05 15:44:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 104 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -6,161 +6,136 @@ public class Solution {
 	private static int[][] delta = { { 0, 0 }, { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 	private static int[][][] board;
 	private static int[][][] map;
-	private static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
-
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
 			M = Integer.parseInt(st.nextToken());
 			A = Integer.parseInt(st.nextToken());
+
 			board = new int[10][10][2];
-			visited = new boolean[A];
 			map = new int[10][10][2];
 
-			List<Integer> aOrder = new ArrayList<>();
+			int[][] times = new int[M][2];
+
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < M; i++) {
-				aOrder.add(Integer.parseInt(st.nextToken()));
+				times[i][0] = Integer.parseInt(st.nextToken());
 			}
 
-			List<Integer> bOrder = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < M; i++) {
-				bOrder.add(Integer.parseInt(st.nextToken()));
+				times[i][1] = Integer.parseInt(st.nextToken());
 			}
 
-			for (int i = 0; i < A; i++) {
+			for (int index = 1; index <= A; index++) {
 				st = new StringTokenizer(br.readLine());
+
 				int x = Integer.parseInt(st.nextToken()) - 1;
 				int y = Integer.parseInt(st.nextToken()) - 1;
 				int c = Integer.parseInt(st.nextToken());
 				int p = Integer.parseInt(st.nextToken());
-				bfs(y, x, c, p, i + 1);
+				bfs(y, x, c, p, index);
 			}
-			sb.append("#").append(t).append(" ").append(simulate(aOrder, bOrder)).append("\n");
+			sb.append("#").append(t).append(" ").append(simulate(times)).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static int simulate(List<Integer> aOrder, List<Integer> bOrder) {
-		List<int[]> arr = new ArrayList<>();
-		arr.add(new int[] { 0, 0 });
-		arr.add(new int[] { 9, 9 });
-		int result = cal(arr);
+	private static int simulate(int[][] times) {
+		int ar = 0;
+		int ac = 0;
+		int br = 9;
+		int bc = 9;
+		int result = cal(new int[][] { { ar, ac }, { br, bc } });
+		for (int[] time : times) {
+			ar += delta[time[0]][0];
+			ac += delta[time[0]][1];
 
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { 0, 0 });
-		q.add(new int[] { 9, 9 });
+			br += delta[time[1]][0];
+			bc += delta[time[1]][1];
 
-		for (int i = 0; i < M; i++) {
-			int size = q.size();
-			arr = new ArrayList<>();
-			for (int j = 0; j < size; j++) {
-				int[] curr = q.poll();
-				int nr = 0;
-				int nc = 0;
-
-				if (j == 0) {
-					nr = curr[0] + delta[aOrder.get(i)][0];
-					nc = curr[1] + delta[aOrder.get(i)][1];
-				} else {
-					nr = curr[0] + delta[bOrder.get(i)][0];
-					nc = curr[1] + delta[bOrder.get(i)][1];
-				}
-				q.add(new int[] { nr, nc });
-				arr.add(new int[] { nr, nc });
-			}
-			result += cal(arr);
+			result += cal(new int[][] { { ar, ac }, { br, bc } });
 		}
 		return result;
 	}
 
-	private static int cal(List<int[]> arr) {
-		int sum = 0;
-		int[] a = arr.get(0);
-		int[] b = arr.get(1);
-		int aValue = map[a[0]][a[1]][0];
-		int bValue = map[b[0]][b[1]][0];
+	private static int cal(int[][] info) {
+		int[] a = info[0];
+		int[] b = info[1];
 
-		if (aValue > 0 && aValue == bValue) {
+		// 같은 BC 구간을 쓰고 있다면
+		if (map[a[0]][a[1]][0] == map[b[0]][b[1]][0]) {
 			int tmpA = 0;
 			int tmpB = 0;
 
-			if (map[a[0]][a[1]][1] > 0) {
-				tmpA = board[a[0]][a[1]][1] + board[b[0]][b[1]][0];
+			// A가 다른 BC와 겹치는 구간에 있다면
+			if (board[a[0]][a[1]][1] > 0) {
+				tmpA = board[a[0]][a[1]][0] + board[a[0]][a[1]][1];
 			}
 
-			if (map[b[0]][b[1]][1] > 0) {
-				tmpB = board[b[0]][b[1]][1] + board[a[0]][a[1]][0];
+			// B가 다른 BC와 겹치는 구간에 있다면
+			if (board[b[0]][b[1]][1] > 0) {
+				tmpB = board[b[0]][b[1]][0] + board[b[0]][b[1]][1];
 			}
-			return Math.max(tmpA, Math.max(tmpB, board[a[0]][a[1]][0]));
+			return Math.max(tmpA, Math.max(board[a[0]][a[1]][0], tmpB));
 		}
+
 		return board[a[0]][a[1]][0] + board[b[0]][b[1]][0];
 	}
 
-	private static void output() {
-		for (int i = 0; i < 2; i++) {
-			for (int r = 0; r < 10; r++) {
-				for (int c = 0; c < 10; c++) {
-					System.out.print(board[r][c][i] + "\t");
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}
-	}
+	/**
+	 * board[][][0] => 보여지는 충전양 board[][][1] => 겹치는 구간의 두 번째로 큰 충전양 map[][][0] =>
+	 * 보여지는 충전하는 장소의 index map[][][1] => 겹치는 구간의 두 번째로 큰 충전 장소 index
+	 */
 
-	private static void bfs(int y, int x, int c, int p, int idx) {
+	private static void bfs(int r, int c, int range, int power, int index) {
 		boolean[][] visited = new boolean[10][10];
-		Queue<int[]> q = new ArrayDeque<>();
+		visited[r][c] = true;
 
-		if (board[y][x][0] > p) {
-			if (board[y][x][1] < p) {
-				board[y][x][1] = p;
-				map[y][x][1] = idx;
+		// 이미 충전할 위치에 충전양이 존재하고 현재 power보다 크다면
+		if (board[r][c][0] > power) {
+			// board[r][c][1] 겹치는 구간에 power 값을 최신화 시켜준다.
+			// 겹치는 구간에 이미 값이 존재할 수 있기 때문에 대소비교해서 넣어준다.
+			if (board[r][c][1] < power) {
+				board[r][c][1] = power;
+				map[r][c][1] = index;
 			}
-		} else {
-			board[y][x][0] = p;
-			map[y][x][0] = idx;
+		} else { // 만약 현재 power가 기존에 있던 power보다 크다면
+			board[r][c][1] = board[r][c][0];
+			board[r][c][0] = power;
+			map[r][c][1] = map[r][c][0];
+			map[r][c][0] = index;
 		}
 
-		visited[y][x] = true;
-		q.add(new int[] { y, x, c });
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[] { r, c, range });
 
 		while (!q.isEmpty()) {
 			int[] curr = q.poll();
 
 			for (int i = 1; i <= 4; i++) {
-
 				int nr = curr[0] + delta[i][0];
 				int nc = curr[1] + delta[i][1];
 
 				if (isRange(nr, nc) && !visited[nr][nc] && curr[2] > 0) {
-
-					if (board[nr][nc][0] > 0) {
-						int tmp = board[nr][nc][0];
-
-						if (tmp > p) {
-							if (board[nr][nc][1] < p) {
-								board[nr][nc][1] = p;
-								map[nr][nc][1] = idx;
-							}
-						} else {
-							board[nr][nc][0] = p;
-							board[nr][nc][1] = tmp;
-							map[nr][nc][1] = map[nr][nc][0];
-							map[nr][nc][0] = idx;
+					if (board[nr][nc][0] > power) {
+						// board[nr][nc][1] 겹치는 구간에 power 값을 최신화 시켜준다.
+						// 겹치는 구간에 이미 값이 존재할 수 있기 때문에 대소비교해서 넣어준다.
+						if (board[nr][nc][1] < power) {
+							board[nr][nc][1] = power;
+							map[nr][nc][1] = index;
 						}
-
-					} else {
-						board[nr][nc][0] = p;
-						map[nr][nc][0] = idx;
+					} else { // 만약 현재 power가 기존에 있던 power보다 크다면
+						board[nr][nc][1] = board[nr][nc][0];
+						board[nr][nc][0] = power;
+						map[nr][nc][1] = map[nr][nc][0];
+						map[nr][nc][0] = index;
 					}
 					visited[nr][nc] = true;
 					q.add(new int[] { nr, nc, curr[2] - 1 });
