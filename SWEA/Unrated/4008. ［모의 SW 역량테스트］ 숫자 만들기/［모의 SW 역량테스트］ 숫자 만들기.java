64b443582f974aa97d17eee3d90d221687fd0e5e
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-07 11:59:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 113 ms, Memory: 27,644 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,89 +1,79 @@
 import java.io.*;
+import java.security.CryptoPrimitive;
 import java.util.*;
 
 public class Solution {
-
-	private static int N, min, max;
-	private static ArrayList<Integer> sign;
-	private static int[] arr;
+	private static int N;
+	private static int[] nums;
+	private static int[] cnt;
+	private static int max, min;
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 
 		int T = Integer.parseInt(br.readLine());
-
 		for (int t = 1; t <= T; t++) {
-
 			N = Integer.parseInt(br.readLine());
-
-			sign = new ArrayList<>();
-			arr = new int[N];
+			nums = new int[N];
+			cnt = new int[4];
+			max = Integer.MIN_VALUE;
+			min = Integer.MAX_VALUE;
 
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < 4; i++) {
-
-				int tmp = Integer.parseInt(st.nextToken());
-
-				for (int j = 0; j < tmp; j++) {
-					sign.add(i);
-				}
-
+				cnt[i] = Integer.parseInt(st.nextToken());
 			}
 
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
 
-			min = Integer.MAX_VALUE;
-			max = Integer.MIN_VALUE;
-
-			selectSign(0, arr[0], new boolean[N - 1]);
-
-			sb.append("#" + t + " " + (max - min) + "\n");
+			dfs(0, new int[N - 1]);
+			sb.append("#").append(t).append(" ").append(max - min).append("\n");
 		}
 		System.out.println(sb.toString());
-
 	}
 
-	private static int cal(int a, int b, int s) {
-		if (s == 0) {
-			return a + b;
-		} else if (s == 1) {
-			return a - b;
-
-		} else if (s == 2) {
-			return a * b;
-		} else {
-			return a / b;
-		}
-	}
-
-	private static void selectSign(int depth, int num, boolean[] visited) {
-
+	private static void dfs(int depth, int[] operator) {
 		if (depth == N - 1) {
-			min = Math.min(num, min);
-			max = Math.max(num, max);
-
+			int result = cal(operator);
+			min = Math.min(min, result);
+			max = Math.max(max, result);
 		} else {
-			
-			int pre = -1;
-			for (int i = 0; i < N - 1; i++) {
-				
-				if (visited[i] || pre == sign.get(i)) {
+			for (int i = 0; i < 4; i++) {
+				if (cnt[i] == 0)
 					continue;
-				}
-
-				pre = sign.get(i);
-				visited[i] = true;
-				selectSign(depth + 1, cal(num, arr[depth + 1], sign.get(i)), visited);
-				visited[i] = false;
+				operator[depth] = i;
+				cnt[i]--;
+				dfs(depth + 1, operator);
+				cnt[i]++;
 			}
-
 		}
+	}
 
+	private static int cal(int[] operator) {
+		int sum = nums[0];
+
+		for (int i = 1; i < N; i++) {
+			int curr = nums[i];
+			switch (operator[i - 1]) {
+			case 0:
+				sum += curr;
+				break;
+			case 1:
+				sum -= curr;
+				break;
+			case 2:
+				sum *= curr;
+				break;
+			case 3:
+				sum /= curr;
+				break;
+			}
+		}
+		return sum;
 	}
 }
