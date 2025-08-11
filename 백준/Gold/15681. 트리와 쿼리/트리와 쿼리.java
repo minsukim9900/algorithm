10dec4948f10525f6fd43afbabbc195444ae9705
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-12 00:27:17 +0900

    [Gold V] Title: 트리와 쿼리, Time: 528 ms, Memory: 80428 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -3,8 +3,12 @@ import java.util.*;
 
 public class Main {
 	private static int N, R, Q;
+	private static int[] subTreeCount;
+	private static int[] parent;
+	private static int[] level;
+	private static boolean[] visited;
+
 	private static List<Integer>[] arr;
-	private static int[] p;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,9 +16,15 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 
 		st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
+
+		subTreeCount = new int[N + 1];
+		parent = new int[N + 1];
+		level = new int[N + 1];
+		visited = new boolean[N + 1];
 
 		arr = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
@@ -25,33 +35,35 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			link(from, to);
+			arr[from].add(to);
+			arr[to].add(from);
 		}
-
-		p = new int[N + 1];
-		dfs(R, new boolean[N + 1]);
+		dfs(R, 0);
+//		System.out.println(Arrays.toString(subTreeCount));
+//		System.out.println(Arrays.toString(parent));
+//		System.out.println(Arrays.toString(level));
 
 		for (int i = 0; i < Q; i++) {
-			sb.append(p[Integer.parseInt(br.readLine())]).append("\n");
+			sb.append(subTreeCount[Integer.parseInt(br.readLine())]);
+			sb.append("\n");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static int dfs(int num, boolean[] visited) {
-		visited[num] = true;
+	private static void dfs(int curr, int l) {
+		visited[curr] = true;
 		int cnt = 1;
+		level[curr] = l;
 
-		for (int node : arr[num]) {
+		for (int node : arr[curr]) {
 			if (visited[node])
 				continue;
-			cnt += dfs(node, visited);
-		}
-		p[num] = cnt;
-		return cnt;
-	}
 
-	private static void link(int from, int to) {
-		arr[from].add(to);
-		arr[to].add(from);
+			parent[node] = curr;
+			dfs(node, l + 1);
+			cnt += subTreeCount[node];
+		}
+
+		subTreeCount[curr] = cnt;
 	}
 }
