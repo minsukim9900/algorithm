337c0a973a74fb8f20e2d68859ae21a8d2bf4e07
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-12 15:33:02 +0900

    [Gold V] Title: 감소하는 수, Time: 72 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int N, count;
+	private static long answer;
+	private static boolean isEnd;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		N = Integer.parseInt(br.readLine());
+		answer = -1L;
+		for (int i = 1; i < 11; i++) {
+			dfs(0, 10, i, 0);
+		}
+		System.out.println(answer);
+	}
+
+	private static void dfs(int depth, int pre, int level, long sum) {
+		if (isEnd)
+			return;
+		if (depth == level) {
+			if (count == N) {
+				answer = sum;
+				isEnd = true;
+			}
+			count++;
+			return;
+		}
+		for (int i = 0; i < 10; i++) {
+			if (pre <= i)
+				continue;
+			dfs(depth + 1, i, level, sum * 10 + i);
+		}
+	}
+}
