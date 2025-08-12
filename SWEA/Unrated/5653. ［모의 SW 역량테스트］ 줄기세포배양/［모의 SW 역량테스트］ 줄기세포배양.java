f39ef450281ed68c6d0244f0e5af6f0d1476412a
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-12 10:08:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 366 ms, Memory: 112,600 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -2,132 +2,86 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-	private static int R, C, K;
-	private static int[][] map;
-	private static ArrayList<int[]> cell;
+	private static int N, M, K;
+	private static int[][] board;
+	private static final int MAX_LENGTH = 1000;
+	private static boolean[][] visited;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			
-			R = N + K;
-			C = M + K;
-			map = new int[R][C];
-			cell = new ArrayList<>();
+			board = new int[MAX_LENGTH][MAX_LENGTH];
+			visited = new boolean[MAX_LENGTH][MAX_LENGTH];
 
-			int tmp = K/2;
-			
+			Queue<int[]> cell = new ArrayDeque<>();
+
 			for (int r = 0; r < N; r++) {
-
 				st = new StringTokenizer(br.readLine());
-				int nr = r + tmp;
+
 				for (int c = 0; c < M; c++) {
-					
-					int nc = c + tmp;
-					map[nr][nc] = Integer.parseInt(st.nextToken());
+					int state = Integer.parseInt(st.nextToken());
+					board[(MAX_LENGTH / 2) + r][(MAX_LENGTH / 2) + c] = state;
 
-					if (map[nr][nc] > 0) {
-						cell.add(new int[] { nr, nc, map[nr][nc], map[nr][nc], map[nr][nc] });
+					if (state > 0) {
+						cell.add(new int[] { (MAX_LENGTH / 2) + r, (MAX_LENGTH / 2) + c, state, state, state });
+						visited[(MAX_LENGTH / 2) + r][(MAX_LENGTH / 2) + c] = true;
 					}
-
-				}
-
-			}
-
-			sortArr();
-			culture();
-
-			int cnt = 0;
-			for (int r = 0; r < R; r++) {
-				for (int c = 0; c < C; c++) {
-					if (map[r][c] >= 1)
-						cnt++;
 				}
 			}
-
-			sb.append("#" + t + " ").append(cnt).append("\n");
-
+			sb.append("#").append(t).append(" ").append(simulate(cell)).append("\n");
 		}
-
 		System.out.println(sb.toString());
-
 	}
 
-	private static void culture() {
+	private static int simulate(Queue<int[]> cell) {
+		int count = 0;
 
-		Queue<int[]> q = new ArrayDeque<>();
+		while (K-- > 0) {
+			int size = cell.size();
+			List<int[]> growCell = new ArrayList<>();
 
-		for (int i = 0; i < cell.size(); i++) {
-			q.offer(cell.get(i));
-		}
+			for (int i = 0; i < size; i++) {
+				int[] curr = cell.poll();
 
-		for (int t = 0; t < K; t++) {
-
-			int size = q.size();
-
-			for (int k = 0; k < size; k++) {
-
-				int[] curr = q.poll();
-
-				if (curr[2] != 0) {
-					curr[2]--;
-					q.offer(curr);
-
+				curr[2]--;
+				if (curr[2] < 0) {
+					growCell.add(curr);
 				} else {
+					cell.add(curr);
+				}
+			}
 
-					curr[3]--;
-					
-					if (curr[3] > 0) {
-						q.offer(curr);
-					} else {
-						map[curr[0]][curr[1]] = -1;
-					}
+			Collections.sort(growCell, (a, b) -> a[4] == b[4] ? a[1] - b[1] : b[4] - a[4]);
 
-					for (int i = 0; i < 4; i++) {
+			for (int[] c : growCell) {
+				for (int i = 0; i < 4; i++) {
+					int nr = c[0] + delta[i][0];
+					int nc = c[1] + delta[i][1];
 
-						int nr = curr[0] + delta[i][0];
-						int nc = curr[1] + delta[i][1];
-
-						if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] == 0) {
-
-							map[nr][nc] = curr[4];
-							q.offer(new int[] { nr, nc, map[nr][nc], map[nr][nc], map[nr][nc] });
-						}
+					if (nr >= 0 && nr < MAX_LENGTH && nc >= 0 && nc < MAX_LENGTH && !visited[nr][nc]) {
+						board[nr][nc] = c[4];
+						visited[nr][nc] = true;
+						cell.add(new int[] { nr, nc, board[nr][nc], board[nr][nc], board[nr][nc] });
 					}
 
 				}
-
-
+				c[3]--;
+				if (c[3] > 0) {
+					cell.add(c);
+				}
 			}
-
+			count = cell.size();
 		}
-
+		return count;
 	}
-
-	private static void sortArr() {
-
-		Collections.sort(cell, new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[4] - o1[4];
-			}
-
-		});
-
-	}
-
 }
