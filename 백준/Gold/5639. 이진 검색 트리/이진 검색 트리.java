fb678073a9e55335c6f45d73edf70c0f81d254b0
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-13 11:37:51 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 328 ms, Memory: 16288 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static class Node {
+		int value;
+		Node head, left, right;
+
+		public Node(int value, Main.Node head, Main.Node left, Main.Node right) {
+			this.value = value;
+			this.head = head;
+			this.left = left;
+			this.right = right;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [value=" + value + ", head=" + (head == null ? 0 : head.value) + ", left="
+					+ (left == null ? 0 : left.value) + ", right=" + (right == null ? 0 : right.value) + "]";
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		Node node = null;
+		while (true) {
+			String tmp = br.readLine();
+			if (tmp == null) {
+				break;
+			}
+			int value = Integer.parseInt(tmp);
+			Node curr = new Node(value, null, null, null);
+
+			if (node == null) {
+				node = curr;
+				continue;
+			}
+
+			insertValue(node, curr);
+		}
+		postOrder(node);
+		System.out.println(sb.toString());
+
+	}
+
+	private static void insertValue(Node node, Node curr) {
+		while (true) {
+			if (node.value > curr.value) { // 현재 노드보다 curr value가 작을 때
+				if (node.left == null) {
+					node.left = curr;
+					curr.head = node;
+					break;
+				}
+				node = node.left;
+			} else { // 현재 노드보다 curr.value가 클 때
+				if (node.right == null) {
+					node.right = curr;
+					curr.head = node;
+					break;
+				}
+				node = node.right;
+			}
+		}
+	}
+
+	private static StringBuilder sb = new StringBuilder();
+
+	private static void postOrder(Node node) {
+		if (node.left != null) {
+			postOrder(node.left);
+		}
+
+		if (node.right != null) {
+			postOrder(node.right);
+		}
+
+		sb.append(node.value).append("\n");
+	}
+}
