68426b8717e22bbeabbb7a827e588a58d7d910e7
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-16 13:55:33 +0900

    [Gold IV] Title: 호텔, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -6,12 +6,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-
 		st = new StringTokenizer(br.readLine());
 		int C = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
-		
-		
+
 		/**
 		 * dist 배열 채우기, N개 중 1번 째 입력 값으로 dist 채우기
 		 * 
@@ -20,7 +18,7 @@ public class Main {
 		int cost = Integer.parseInt(st.nextToken());
 		int p = Integer.parseInt(st.nextToken());
 		int[] dp = new int[C + 1];
-		
+
 		for (int c = 0; c <= C; c++) {
 			dp[c] = (c / p) * cost;
 
@@ -28,14 +26,10 @@ public class Main {
 				dp[c] += cost;
 			}
 		}
-		
-		
+
 		/**
-		 * N-1개 dp 활용
-		 * dp 배열 인덱스는 호텔 x명
-		 * 인덱스에 들어갈 값: x명을 유치하기 위한 최소 비용
-		 * 비교 값: 이전의 dp[x	], dp[x - p] + cost 중 최솟값
-		 * preIdx가 필요한 이유 j - p를 하게 되면 0보다 더 작아질 수 있다.
+		 * N-1개 dp 활용 dp 배열 인덱스는 호텔 x명 인덱스에 들어갈 값: x명을 유치하기 위한 최소 비용 비교 값: 이전의 dp[x ],
+		 * dp[x - p] + cost 중 최솟값 preIdx가 필요한 이유 j - p를 하게 되면 0보다 더 작아질 수 있다.
 		 */
 		for (int i = 1; i <= N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -43,16 +37,10 @@ public class Main {
 			p = Integer.parseInt(st.nextToken());
 
 			for (int c = 1; c <= C; c++) {
-				int currCost = (c / p) * cost;
-
-				if (c % p != 0) {
-					currCost += cost;
-				}
 				int preIdx = Math.max(0, c - p);
 				dp[c] = Math.min(dp[c], dp[preIdx] + cost);
 			}
 		}
-
 		System.out.println(dp[C]);
 	}
 }
