a6b96844cbe62f8168af937fc837f39b052301ef
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-16 13:49:21 +0900

    [Gold IV] Title: 호텔, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -3,49 +3,56 @@ import java.util.*;
 
 public class Main {
 
-	private static int C, N;
-	private static int[][] info;
-
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+		StringTokenizer st = null;
 
 		st = new StringTokenizer(br.readLine());
-		C = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		
+		/**
+		 * dist 배열 채우기, N개 중 1번 째 입력 값으로 dist 채우기
+		 * 
+		 */
+		st = new StringTokenizer(br.readLine());
+		int cost = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		int[] dp = new int[C + 1];
+		
+		for (int c = 0; c <= C; c++) {
+			dp[c] = (c / p) * cost;
 
-		info = new int[N][2];
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			info[i][0] = Integer.parseInt(st.nextToken());
-			info[i][1] = Integer.parseInt(st.nextToken());
-		}
-
-		int[] dp = new int[C + 101];
-
-		Arrays.fill(dp, 987654321);
-		dp[0] = 0;
-
-		for (int i = 0; i < N; i++) {
-
-			for (int j = info[i][1]; j < dp.length; j++) {
-
-				dp[j] = Math.min(dp[j], dp[j - info[i][1]] + info[i][0]);
-				
+			if (c % p != 0) {
+				dp[c] += cost;
 			}
-
 		}
 		
+		
+		/**
+		 * N-1개 dp 활용
+		 * dp 배열 인덱스는 호텔 x명
+		 * 인덱스에 들어갈 값: x명을 유치하기 위한 최소 비용
+		 * 비교 값: 이전의 dp[x	], dp[x - p] + cost 중 최솟값
+		 * preIdx가 필요한 이유 j - p를 하게 되면 0보다 더 작아질 수 있다.
+		 */
+		for (int i = 1; i <= N - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			cost = Integer.parseInt(st.nextToken());
+			p = Integer.parseInt(st.nextToken());
 
-		int result = Integer.MAX_VALUE;
-		for (int i = C; i < dp.length; i++) {
-			result = Math.min(dp[i], result);
+			for (int c = 1; c <= C; c++) {
+				int currCost = (c / p) * cost;
+
+				if (c % p != 0) {
+					currCost += cost;
+				}
+				int preIdx = Math.max(0, c - p);
+				dp[c] = Math.min(dp[c], dp[preIdx] + cost);
+			}
 		}
 
-		System.out.println(result);
-
+		System.out.println(dp[C]);
 	}
 }
