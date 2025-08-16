c0f7625cf121e87b7307e2d0bacfda950464dd17
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-16 14:52:08 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 576 ms, Memory: 55140 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -30,31 +30,28 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 
-			arr[from].add(new int[] { from, to, w });
+			arr[from].add(new int[] { to, w });
 		}
 
 		st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
+
 		sb.append(findTrack(start, end)).append("\n");
-		sb.append(countCity(end)).append("\n");
-		
-		while(!answer.isEmpty()) {
+		sb.append(countCity(end, start)).append("\n");
+		while (!answer.isEmpty()) {
 			sb.append(answer.pop()).append(" ");
 		}
 		System.out.println(sb.toString());
 	}
 
-	private static int countCity(int city) {
+	private static int countCity(int city, int start) {
 		answer.add(city);
-		if (p[city] == 0) {
+		if (city == start) {
 			return 1;
 		}
-
 		int cnt = 1;
-
-		cnt += countCity(p[city]);
-
+		cnt += countCity(p[city], start);
 		return cnt;
 	}
 
@@ -62,26 +59,22 @@ public class Main {
 		dist[start] = 0;
 		p[start] = 0;
 
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
-		pq.addAll(arr[start]);
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+		pq.add(new int[] { start, 0 });
 
 		while (!pq.isEmpty()) {
 			int[] curr = pq.poll();
-			int from = curr[0];
-			int to = curr[1];
-			int w = curr[2];
-
-			if (dist[to] > dist[from] + w) {
-				dist[to] = dist[from] + w;
-				p[to] = from;
-				pq.addAll(arr[to]);
+			
+			if(curr[1] > dist[curr[0]]) continue;
+			
+			for (int[] next : arr[curr[0]]) {
+				if (dist[next[0]] > dist[curr[0]] + next[1]) {
+					p[next[0]] = curr[0];
+					dist[next[0]] = dist[curr[0]] + next[1];
+					pq.add(new int[] { next[0], dist[next[0]] });
+				}
 			}
 		}
-
 		return dist[end];
-	}
-
-	private static void union(int x, int y) {
-		p[y] = x;
 	}
 }
