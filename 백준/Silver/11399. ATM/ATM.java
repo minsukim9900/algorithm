d173de448ac23c2820daead7b16adf7da05fc42b
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-19 17:07:58 +0900

    [Silver IV] Title: ATM, Time: 76 ms, Memory: 11884 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,38 +1,29 @@
+import java.util.*;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		ArrayList<Integer> arr1= new ArrayList<>();
-		ArrayList<Integer> arr2 = new ArrayList<>();
-		
 		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
-			arr1.add(Integer.parseInt(st.nextToken()));
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		arr1.sort(null);
-		
-		int temp = 0;
-		for(int i = 0; i<arr1.size(); i++) {
-			arr2.add(arr1.get(i)+temp);
-			temp += arr1.get(i);
+
+		Arrays.sort(nums);
+
+		long sum = 0;
+		long answer = 0;
+		for (int i = 0; i < N; i++) {
+			sum += nums[i];
+			answer += sum;
 		}
-		
-		int sum = 0;
-		for(int i = 0; i<arr2.size(); i++) {
-			sum+=arr2.get(i);
-		}
-		
-		System.out.println(sum);
-		
-		
+		System.out.println(answer);
 	}
 }
