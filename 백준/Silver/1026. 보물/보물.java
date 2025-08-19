5a8431480c5b18c0fa553296e2b9dfea9fab0a60
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-19 16:52:46 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,39 +1,33 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
+import java.io.*;
 
 public class Main {
-    private static int[] nums;
-    private static int[] B;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        nums = new int[N];
-        B = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(nums);
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++){
-            B[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(B);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
 
-        int i = 0;
-        int j = N-1;
-        int sum = 0;
-        while(i != N &&j != -1){
-            sum += nums[i] * B[j];
-            i++;
-            j--;
-        }
-        System.out.println(sum);
-    }
+		int[] numA = new int[N];
+		int[] numB = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numA[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numA);
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numB[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numB);
+
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += (numA[i] * numB[N - i - 1]);
+		}
+		System.out.println(sum);
+	}
 }
