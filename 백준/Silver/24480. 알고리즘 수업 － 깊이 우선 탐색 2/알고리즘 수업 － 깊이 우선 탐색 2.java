70afa8a02386c0ff694fb5c218c4a57f10b2c89d
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-20 17:57:37 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 896 ms, Memory: 94288 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -2,55 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	private static int N, M, R;
-	private static List<Integer>[] adj;
-	private static int[] visited;
-	private static int cnt = 0;
+	private static int N, M, R, cnt;
+	private static List<Integer>[] arr;
+	private static int[] count;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		
-		visited = new int[N+1];
-		adj = new ArrayList[N+1];
-		for(int i = 1; i<=N; i++) {
-			adj[i] = new ArrayList<>();
+		count = new int[N + 1];
+		arr = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = new ArrayList<>();
 		}
-		
-		for (int m = 0; m < M; m++) {
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			adj[from].add(to);
-			adj[to].add(from);
+			arr[from].add(to);
+			arr[to].add(from);
 		}
-		
-		for(int i = 1; i<=N; i++) {
-			Collections.sort(adj[i],Collections.reverseOrder());
+
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(arr[i], Comparator.reverseOrder());
 		}
-		
-		dfs(R);
-		
-		for(int i = 1; i<=N; i++) {
-			sb.append(visited[i]).append("\n");
+		dfs(R, new boolean[N + 1]);
+		for(int i = 1; i <= N; i++) {
+			sb.append(count[i]).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
-	
-	public static void dfs(int v) {
-		visited[v] = ++cnt;
-		
-		for(int w : adj[v]) {
-			if(visited[w] == 0) {
-				dfs(w);
-			}
-		}
-	}
 
+	private static boolean[] dfs(int num, boolean[] visited) {
+		visited[num] = true;
+		count[num] = ++cnt;
+		for (int w : arr[num]) {
+			if (visited[w])
+				continue;
+
+			dfs(w, visited);
+		}
+		return visited;
+	}
 }
