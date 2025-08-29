84157bb6d787cf0724a91ee7801b5129f91c3ef4
Kim Minsu <mmsskksw9900@gmail.com>
2025-08-29 22:35:16 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 296 ms, Memory: 49544 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
@@ -16,7 +16,7 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		chess = new char[N + 1][M + 1];
-		int[][][] prefix = new int[2][N + 1][M + 1];
+		int[][] prefix = new int[N + 1][M + 1];
 
 		for (int r = 1; r <= N; r++) {
 			String str = br.readLine();
@@ -26,36 +26,14 @@ public class Main {
 		}
 
 		for (int r = 1; r <= N; r++) {
-			char comp = 'B';
-			if (r % 2 == 1) {
-				comp = 'W';
-			}
 			int sum = 0;
 			for (int c = 1; c <= M; c++) {
 				int num = 0;
-				if (chess[r][c] != comp) {
+				if (chess[r][c] != (((r + c) % 2 == 0) ? 'W' : 'B')) {
 					num++;
 				}
 				sum += num;
-				prefix[0][r][c] = prefix[0][r - 1][c] + sum;
-				comp = changeState(comp);
-			}
-		}
-
-		for (int r = 1; r <= N; r++) {
-			char comp = 'W';
-			if (r % 2 == 1) {
-				comp = 'B';
-			}
-			int sum = 0;
-			for (int c = 1; c <= M; c++) {
-				int num = 0;
-				if (chess[r][c] != comp) {
-					num++;
-				}
-				sum += num;
-				prefix[1][r][c] = prefix[1][r - 1][c] + sum;
-				comp = changeState(comp);
+				prefix[r][c] = prefix[r - 1][c] + sum;
 			}
 		}
 
@@ -65,17 +43,12 @@ public class Main {
 			for (int ec = K; ec <= M; ec++) {
 				int sr = er - K + 1;
 				int sc = ec - K + 1;
-				int cal1 = prefix[0][er][ec] - (prefix[0][er][sc - 1] + prefix[0][sr - 1][ec])
-						+ prefix[0][sr - 1][sc - 1];
-				int cal2 = prefix[1][er][ec] - (prefix[1][er][sc - 1] + prefix[1][sr - 1][ec])
-						+ prefix[1][sr - 1][sc - 1];
+				int cal1 = prefix[er][ec] - (prefix[er][sc - 1] + prefix[sr - 1][ec])
+						+ prefix[sr - 1][sc - 1];
+				int cal2 = K * K - cal1;
 				answer = Math.min(answer, Math.min(cal1, cal2));
 			}
 		}
 		System.out.println(answer);
-	}
-
-	private static char changeState(char c) {
-		return c == 'W' ? 'B' : 'W';
 	}
 }
