a5375afa415a75bad0a33a2398bd5c982f2e678c
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-01 15:41:47 +0900

    [Platinum III] Title: 달리기, Time: 312 ms, Memory: 57676 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.java
+++ 백준/Platinum/16930. 달리기/달리기.java
@@ -8,9 +8,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 	private static int N, M, K;
-	private static int[][] board;
+	private static char[][] board;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-	private static final int INF = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,17 +21,9 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		board = new int[N][M];
+		board = new char[N][M];
 		for (int r = 0; r < N; r++) {
-			char[] ch = br.readLine().toCharArray();
-			for (int c = 0; c < M; c++) {
-				int num = 0;
-
-				if (ch[c] == '#') {
-					num = -1;
-				}
-				board[r][c] = num;
-			}
+			board[r] = br.readLine().toCharArray();
 		}
 
 		st = new StringTokenizer(br.readLine());
@@ -40,19 +31,18 @@ public class Main {
 		int sc = Integer.parseInt(st.nextToken()) - 1;
 		int er = Integer.parseInt(st.nextToken()) - 1;
 		int ec = Integer.parseInt(st.nextToken()) - 1;
-
 		System.out.println(bfs(sr, sc, er, ec));
 	}
 
 	private static int bfs(int sr, int sc, int er, int ec) {
 		int[][] dist = new int[N][M];
-		for (int r = 0; r < N; r++) {
-			Arrays.fill(dist[r], INF);
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
 		}
 
+		dist[sr][sc] = 0;
 		Queue<int[]> q = new ArrayDeque<>();
 		q.add(new int[] { sr, sc });
-		dist[sr][sc] = 0;
 
 		while (!q.isEmpty()) {
 			int[] curr = q.poll();
@@ -69,18 +59,17 @@ public class Main {
 					nr += delta[i][0];
 					nc += delta[i][1];
 
-					if (!isRange(nr, nc) || board[nr][nc] == -1)
+					if (!isRange(nr, nc) || board[nr][nc] == '#')
 						break;
 
 					if (dist[nr][nc] < dist[curr[0]][curr[1]] + 1) {
 						break;
 					}
 
-					if (dist[nr][nc] == dist[curr[0]][curr[1]] + 1)
-						continue;
-
-					dist[nr][nc] = dist[curr[0]][curr[1]] + 1;
-					q.add(new int[] { nr, nc });
+					if (dist[nr][nc] > dist[curr[0]][curr[1]] + 1) {
+						dist[nr][nc] = dist[curr[0]][curr[1]] + 1;
+						q.add(new int[] { nr, nc });
+					}
 				}
 			}
 		}
