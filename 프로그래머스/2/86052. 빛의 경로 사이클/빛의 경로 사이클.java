e62dae39af5dc714039b080a4d517b3c071ff68f
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-03 01:36:04 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 46.09 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.java
@@ -1,43 +1,50 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static final int[][] delta = {{-1,0},{0,1},{1,0},{0,-1}};
-    public ArrayList<Integer> solution(String[] grid) {
-        int N = grid.length, M = grid[0].length();
-        char[][] board = new char[N][];
-        for (int r = 0; r < N; r++) board[r] = grid[r].toCharArray();
-
-        boolean[][][] visited = new boolean[4][N][M];
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                for (int d = 0; d < 4; d++) {
-                    if (visited[d][r][c]) continue;
-
-                    int cr = r, cc = c, cd = d, len = 0;
-
-                    // 이번 출발에서 새로 방문한 것들만 길이에 포함
-                    while (!visited[cd][cr][cc]) {
+    public List<Integer> solution(String[] grid) {
+        int n = grid.length;
+        int m = grid[0].length();
+        boolean[][][] visited = new boolean[4][n][m];
+        int[][] delta = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
+        char[][] board = new char[n][];
+        for(int r = 0; r < n; r++) {
+            board[r] = grid[r].toCharArray();
+        }
+        
+        List<Integer> arr = new ArrayList<>();
+        for(int r = 0; r < n; r++) {
+            for(int c = 0; c < m; c++) {
+                for(int i = 0; i < 4; i++) {
+                    if(visited[i][r][c]) continue;
+                    int cr = r;
+                    int cc = c;
+                    int cd = i;
+                    int count = 0;
+                    
+                    
+                    while(!visited[cd][cr][cc]) {
                         visited[cd][cr][cc] = true;
-                        len++;
-
-                        // 현재 칸에서 회전 결정
-                        char ch = board[cr][cc];
-                        if (ch == 'L') cd = (cd + 3) % 4;        // 왼쪽 회전
-                        else if (ch == 'R') cd = (cd + 1) % 4;   // 오른쪽 회전
-
-                        // 한 칸 전진 (토러스)
-                        cr = (cr + delta[cd][0] + N) % N;
-                        cc = (cc + delta[cd][1] + M) % M;
+                        count++;
+                        
+                        if(board[cr][cc] == 'L') {
+                            cd = (cd - 1 + 4) % 4;
+                        } else if(board[cr][cc] == 'R') {
+                            cd = (cd + 1) % 4;
+                        } 
+                        
+                        cr = (cr + delta[cd][0] + n) % n;
+                        cc = (cc + delta[cd][1] + m) % m;
                     }
-
-                    if (len > 0) ans.add(len);
+                    
+                    if(count > 0) {
+                        arr.add(count);
+                    }
                 }
             }
         }
-
-        Collections.sort(ans);
-        return ans;
+        Collections.sort(arr);
+        return arr;
     }
 }
