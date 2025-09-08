7c013f664588ce5b7b2f19c6bacf8c7529d0b4e7
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-08 11:30:49 +0900

    [Gold V] Title: 빌런 호석, Time: 172 ms, Memory: 13708 KB -BaekjoonHub

---
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;;
+
+public class Main {
+	private static int N, K, P, X;
+	private static int[] nums = { 123, 10, 55, 31, 78, 93, 125, 11, 127, 95 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		Map<Integer, Integer>[] info = new HashMap[10];
+		for (int i = 0; i < 10; i++) {
+			info[i] = new HashMap<>();
+			info[i].put(i, 0);
+		}
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			int x = nums[i];
+			for (int j = i + 1; j < nums.length; j++) {
+				int y = nums[j];
+				int cnt = Integer.bitCount(x ^ y);
+
+				info[i].put(j, cnt);
+				info[j].put(i, cnt);
+			}
+		}
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+
+		int answer = 0;
+		for (int i = 1; i <= N; i++) {
+			if (i == X)
+				continue;
+
+			int comA = X;
+			int comB = i;
+			int cnt = 0;
+			for (int j = 0; j < K; j++) {
+				int x = comA % 10;
+				int y = comB % 10;
+
+				cnt += info[x].get(y);
+				comA /= 10;
+				comB /= 10;
+			}
+			if (cnt <= P) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+}
