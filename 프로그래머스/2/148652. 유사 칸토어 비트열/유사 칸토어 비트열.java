60ca46d49830d4364bfd3015707f5156edad0996
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-10 22:59:54 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 184.78 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,36 +1,17 @@
-import java.util.*;
-
 class Solution {
-    private static long[] p5, p4;
-    public long solution(int n, long l, long r) {
-        init(n);
-        return prefix(r, n) - prefix(l - 1, n);
+    public int solution(int n, long l, long r) {
+        int answer = 0;
+        for(long i = l - 1; i < r; i++) {
+            answer += cal(i);
+        }
+        return answer;
     }
     
-    private static long prefix(long point, int size) {
-        if(size <= 0) {
-           return 0;
+    private static long cal(long x) {
+        if(x % 5 == 2) {
+            return 0;
         }
-        
-        long page = point / p5[size - 1];
-        long rem = point % p5[size - 1];
-        long prePageOneCount = p4[size - 1] * (page >= 3 ? page - 1 : page);
-        
-        long sum = prePageOneCount;
-        if(page == 2) {
-            return sum;
-        }else {
-            return sum += prefix(rem, size - 1);
-        }
-    }
-    
-    private static void init(int n) {
-        p5 = new long[n + 1];
-        p4 = new long[n + 1];
-        p5[0] = 1; p4[0] = 1;
-        for(int i = 1; i <= n; i++) {
-            p4[i] = p4[i - 1] * 4;
-            p5[i] = p5[i - 1] * 5;
-        }
+        if(x == 0) return 1;
+        return cal(x / 5);
     }
 }
