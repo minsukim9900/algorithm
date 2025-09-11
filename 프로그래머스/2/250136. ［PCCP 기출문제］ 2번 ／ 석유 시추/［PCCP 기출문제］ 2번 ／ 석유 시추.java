c603226d08f2feae9e79f922a1799587ab686391
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-12 08:52:24 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 67.60 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,61 +2,57 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int N, M;
-    private static int[] amount;
+    private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private static boolean[][] visited;
-    private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1},{0,1}};
+    private static int[] count;
+    private static int N, M;
+    private static int answer;
     public int solution(int[][] land) {
         N = land.length;
         M = land[0].length;
+        count = new int[M];
         visited = new boolean[N][M];
-        amount = new int[M];
+        answer = 0;
         
         for(int r = 0; r < N; r++) {
             for(int c = 0; c < M; c++) {
-                if(!visited[r][c] && land[r][c] == 1) {
-                    searchOil(r, c, land);
+                if(land[r][c] == 1 && !visited[r][c]) {
+                    bfs(r, c, land);
                 }
             }
         }
-        
-        
-        int answer = 0;
-        for(int w : amount) {
-            answer = Math.max(answer, w);
-        }
-        
         return answer;
     }
     
-    private static void searchOil(int r, int c, int[][] land) {
-        int maxC = c;
-        int minC = c;
-        int cnt = 0;
+    private static void bfs(int r, int c, int[][] land) {
+        visited[r][c] = true;
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[] {r, c});
-        visited[r][c] = true;
-           
+        int minC = M;
+        int maxC = 0;
+        int cnt= 0;
+        
         while(!q.isEmpty()) {
             int[] curr = q.poll();
+            
             cnt++;
+            minC = Math.min(curr[1], minC);
+            maxC = Math.max(curr[1], maxC);
             
             for(int i = 0; i < 4; i++) {
                 int nr = curr[0] + delta[i][0];
                 int nc = curr[1] + delta[i][1];
                 
-                if(nr >= 0 && nr < N && nc >= 0 && nc < M && 
-                   !visited[nr][nc] && land[nr][nc] == 1) {
+                if(nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && land[nr][nc] == 1) {
                     visited[nr][nc] = true;
-                    maxC = Math.max(maxC, nc);
-                    minC = Math.min(minC, nc);
                     q.add(new int[] {nr, nc});
                 }
-            }   
+            }
         }
         
         for(int i = minC; i <= maxC; i++) {
-            amount[i] += cnt;
+            count[i] += cnt;
+            answer = Math.max(answer, count[i]);
         }
     }
 }
