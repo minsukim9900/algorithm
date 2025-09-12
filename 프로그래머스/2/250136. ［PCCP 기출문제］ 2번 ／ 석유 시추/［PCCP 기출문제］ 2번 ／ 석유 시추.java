c2a3a710717086701874647c620bba61e30666d6
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-12 09:08:34 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 53.06 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -3,34 +3,38 @@ import java.util.*;
 
 class Solution {
     private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    private static boolean[][] visited;
-    private static int[] count;
+    private static int[] diff;
     private static int N, M;
-    private static int answer;
     public int solution(int[][] land) {
         N = land.length;
         M = land[0].length;
-        count = new int[M];
-        visited = new boolean[N][M];
-        answer = 0;
+        diff = new int[M];
         
         for(int r = 0; r < N; r++) {
             for(int c = 0; c < M; c++) {
-                if(land[r][c] == 1 && !visited[r][c]) {
+                if(land[r][c] == 1) {
                     bfs(r, c, land);
                 }
             }
         }
+        int answer = 0;
+        int run = 0;
+        
+        for(int c = 0; c < M; c++) {
+            run += diff[c];
+            answer = Math.max(answer, run);
+        }
+        
         return answer;
     }
     
     private static void bfs(int r, int c, int[][] land) {
-        visited[r][c] = true;
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[] {r, c});
         int minC = M;
         int maxC = 0;
         int cnt= 0;
+        land[r][c] = 0;
         
         while(!q.isEmpty()) {
             int[] curr = q.poll();
@@ -43,16 +47,14 @@ class Solution {
                 int nr = curr[0] + delta[i][0];
                 int nc = curr[1] + delta[i][1];
                 
-                if(nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && land[nr][nc] == 1) {
-                    visited[nr][nc] = true;
+                if(nr >= 0 && nr < N && nc >= 0 && nc < M && land[nr][nc] == 1) {
+                    land[nr][nc] = 0;
                     q.add(new int[] {nr, nc});
                 }
             }
         }
         
-        for(int i = minC; i <= maxC; i++) {
-            count[i] += cnt;
-            answer = Math.max(answer, count[i]);
-        }
+        diff[minC] += cnt;
+        if(maxC + 1 < M) diff[maxC + 1] -= cnt;
     }
 }
