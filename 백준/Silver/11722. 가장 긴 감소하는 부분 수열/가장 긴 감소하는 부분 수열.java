ec3c37b365baa252aec121ec384aa4a4e38daa34
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-18 12:32:11 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 76 ms, Memory: 11896 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;;
+
+public class Main {
+	private static int[] nums, dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = -Integer.parseInt(st.nextToken());
+		}
+
+		dp = new int[N];
+		int idx = 1;
+		dp[0] = nums[0];
+
+		for (int i = 1; i < N; i++) {
+			if (nums[i] > dp[idx - 1]) {
+				dp[idx++] = nums[i];
+			} else {
+				int sidx = binarySerach(0, idx, nums[i]);
+				dp[sidx] = nums[i];
+			}
+		}
+		System.out.println(idx);
+	}
+
+	private static int binarySerach(int s, int e, int target) {
+
+		while (s <= e) {
+			int mid = (s + e) / 2;
+
+			if (dp[mid] < target) {
+				s = mid + 1;
+			} else {
+				e = mid - 1;
+			}
+		}
+		return s;
+	}
+}
