2c963993a33a625fc7fef08dc8364a414ea508f7
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-23 10:34:41 +0900

    [Gold IV] Title: 알파벳, Time: 144 ms, Memory: 12300 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 	private static int R, C, answer;
-	private static int[][] board;
+	private static int[][] board, seen;
 	private static int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 	public static void main(String[] args) throws Exception {
@@ -21,25 +21,29 @@ public class Main {
 				board[r][c] = tmp[c] - 'A';
 			}
 		}
-		int visited = 0;
-		visited = (1 << board[0][0]);
+		
+		seen = new int[R][C];
+		for(int[] r : seen) {
+			Arrays.fill(r, -1);
+		}
+		int visited = (1 << board[0][0]);
 		dfs(0, 0, 1, visited);
 		System.out.println(answer);
 	}
 
 	private static void dfs(int r, int c, int count, int visited) {
+		if(seen[r][c] == visited) return;
+		seen[r][c] = visited;
 		
 		answer = Math.max(answer, count);
+		if (answer == 26)
+			return;
+
 		for (int i = 0; i < 4; i++) {
 			int nr = r + delta[i][0];
 			int nc = c + delta[i][1];
-			if (isRange(nr, nc)) {
-				if ((visited & (1 << board[nr][nc])) != 0)
-					continue;
-
-				visited |= (1 << board[nr][nc]);
-				dfs(nr, nc, count + 1, visited);
-				visited ^= (1 << board[nr][nc]);
+			if (isRange(nr, nc) && (visited & (1 << board[nr][nc])) == 0) {
+				dfs(nr, nc, count + 1, visited | (1 << board[nr][nc]));
 			}
 		}
 	}
