b5d6a10b559541d5fa74ecf6bbcd66b372a8cd3a
Kim Minsu <mmsskksw9900@gmail.com>
2025-09-24 21:51:52 +0900

    [level 3] Title: 양과 늑대, Time: 12.05 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static int N, answer;
+    private static boolean[] used;
+    private static int[] nums;
+    private static List<Integer>[] adj;
+    
+    public int solution(int[] info, int[][] edges) {
+        N = info.length;
+        answer = 0;
+        nums = info;
+        used = new boolean[1 << N];
+        adj = new ArrayList[N];
+        
+        for(int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
+        for(int[] e : edges) {
+            adj[e[0]].add(e[1]);
+        }
+        used[1] = true;
+        dfs(1, 1, 0);
+        return answer;
+    }
+    
+    private static void dfs(int mask, int sheep, int wolf) {
+        if(sheep <= wolf) return;
+        answer = Math.max(answer, sheep);
+        
+        for(int i = 0; i < N; i++) {
+            if((mask & (1 << i)) == 0) continue;
+            
+            for(int v : adj[i]) {
+                if((mask & (1 << v)) != 0) continue;
+                int nmask = mask | (1 << v);
+                int ns = sheep + (nums[v] == 0 ? 1 : 0);
+                int nw = wolf + (nums[v] == 1 ? 1 : 0);
+                
+                if(!used[nmask])
+                    dfs(nmask, ns, nw);
+            }
+        }
+    }
+}
