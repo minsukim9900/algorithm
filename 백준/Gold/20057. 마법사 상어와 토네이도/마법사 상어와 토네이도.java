4aa5114052c07b818444955ff2a20aadd28672f4
Kim Minsu <mmsskksw9900@gmail.com>
2025-10-08 16:33:01 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 372 ms, Memory: 35120 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-	private static Map<Integer, int[][]> map;
+	private static int[][][] map;
 	private static int N;
 	private static int[][] board;
 	private static int[][] delta = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
@@ -13,7 +13,6 @@ public class Main {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		map = new HashMap<>();
 		init();
 		N = Integer.parseInt(br.readLine());
 		board = new int[N][N];
@@ -29,53 +28,43 @@ public class Main {
 	}
 
 	private static int simulate() {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { N / 2, N / 2, 0, 0, 2 });
+		int r = N / 2, c = N / 2;
+		int dir = 0;
+		int step = 0;
+		int segmentTwice = 2;
 		int answer = 0;
 
-		while (!q.isEmpty()) {
-			int[] curr = q.poll();
-			int r = curr[0];
-			int c = curr[1];
-			int dir = curr[2];
-			int count = curr[3];
-			int max = curr[4];
-
+		while (true) {
+			r += delta[dir][0];
+			c += delta[dir][1];
 			// 토네이도 이동
-			int nr = r + delta[dir][0];
-			int nc = c + delta[dir][1];
 
-			if (!isRange(nr, nc))
+			if (!isRange(r, c))
 				break;
 
-			answer += cal(nr, nc, dir);
-
-			if ((max / 2) == (count + 1)) {
-				q.add(new int[] { nr, nc, (dir + 1) % 4, count + 1, max });
-				continue;
+			answer += cal(r, c, dir);
+			step++;
+			if (step == segmentTwice / 2) {
+				dir = (dir + 1) % 4;
+			} else if (step == segmentTwice) {
+				dir = (dir + 1) % 4;
+				step = 0;
+				segmentTwice += 2;
 			}
-
-			if (max == count + 1) {
-				q.add(new int[] { nr, nc, (dir + 1) % 4, 0, max + 2 });
-				continue;
-			}
-
-			q.add(new int[] { nr, nc, dir, count + 1, max });
 		}
 		return answer;
 	}
 
 	private static int cal(int r, int c, int dir) {
-		int[][] info = map.get(dir);
+		int[][] info = map[dir];
 
 		int total = board[r][c];
-		
-		if(total == 0) {
+
+		if (total == 0) {
 			return 0;
 		}
 		int sum = 0;
 		int result = 0;
-
 
 		for (int i = 0; i < info.length - 1; i++) {
 			int nr = r + info[i][0];
@@ -106,27 +95,19 @@ public class Main {
 		return result;
 	}
 
-	private static void output() {
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				System.out.print(board[r][c] + " ");
-			}
-			System.out.println();
-		}
-	}
-
 	private static void init() {
-		map.put(0, new int[][] { { -1, 0, 7 }, { 1, 0, 7 }, { -2, 0, 2 }, { 2, 0, 2 }, { -1, -1, 10 }, { 1, -1, 10 },
-				{ 0, -2, 5 }, { -1, 1, 1 }, { 1, 1, 1 }, { 0, -1 } });
+		map = new int[4][][];
+		map[0] = new int[][] { { -1, 0, 7 }, { 1, 0, 7 }, { -2, 0, 2 }, { 2, 0, 2 }, { -1, -1, 10 }, { 1, -1, 10 },
+				{ 0, -2, 5 }, { -1, 1, 1 }, { 1, 1, 1 }, { 0, -1 } };
 
-		map.put(1, new int[][] { { 0, -1, 7 }, { 0, 1, 7 }, { 0, -2, 2 }, { 0, 2, 2 }, { 1, -1, 10 }, { 1, 1, 10 },
-				{ 2, 0, 5 }, { -1, -1, 1 }, { -1, 1, 1 }, { 1, 0 } });
+		map[1] = new int[][] { { 0, -1, 7 }, { 0, 1, 7 }, { 0, -2, 2 }, { 0, 2, 2 }, { 1, -1, 10 }, { 1, 1, 10 },
+				{ 2, 0, 5 }, { -1, -1, 1 }, { -1, 1, 1 }, { 1, 0 } };
 
-		map.put(2, new int[][] { { -1, 0, 7 }, { 1, 0, 7 }, { -2, 0, 2 }, { 2, 0, 2 }, { -1, 1, 10 }, { 1, 1, 10 },
-				{ 0, 2, 5 }, { -1, -1, 1 }, { 1, -1, 1 }, { 0, 1 } });
+		map[2] =  new int[][] { { -1, 0, 7 }, { 1, 0, 7 }, { -2, 0, 2 }, { 2, 0, 2 }, { -1, 1, 10 }, { 1, 1, 10 },
+				{ 0, 2, 5 }, { -1, -1, 1 }, { 1, -1, 1 }, { 0, 1 } };
 
-		map.put(3, new int[][] { { 0, -1, 7 }, { 0, 1, 7 }, { 0, -2, 2 }, { 0, 2, 2 }, { -1, -1, 10 }, { -1, 1, 10 },
-				{ -2, 0, 5 }, { 1, -1, 1 }, { 1, 1, 1 }, { -1, 0 } });
+		map[3] = new int[][] { { 0, -1, 7 }, { 0, 1, 7 }, { 0, -2, 2 }, { 0, 2, 2 }, { -1, -1, 10 }, { -1, 1, 10 },
+				{ -2, 0, 5 }, { 1, -1, 1 }, { 1, 1, 1 }, { -1, 0 } };
 	}
 
 	private static boolean isRange(int r, int c) {
