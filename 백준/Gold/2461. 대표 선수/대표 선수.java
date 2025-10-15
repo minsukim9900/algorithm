d8da42c61142d0a248a0834f3b46df89ec0e84a1
Kim Minsu <mmsskksw9900@gmail.com>
2025-10-15 10:49:13 +0900

    [Gold II] Title: 대표 선수, Time: 1196 ms, Memory: 180180 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -15,48 +15,35 @@ public class Main {
 		PriorityQueue<Integer>[] pq = new PriorityQueue[N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			pq[i] = new PriorityQueue<>((a, b) -> a - b);
+			pq[i] = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
 			for (int j = 0; j < M; j++) {
 				pq[i].add(Integer.parseInt(st.nextToken()));
 			}
 		}
 
-		PriorityQueue<int[]> cal = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
+		PriorityQueue<int[]> cal = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
 		int max = -1;
-		int min = Integer.MAX_VALUE;
-		int maxIdx = 0;
-		int minIdx = 0;
 
 		for (int i = 0; i < N; i++) {
 			int num = pq[i].poll();
-
-			if (max < num) {
-				max = num;
-				maxIdx = i;
-			}
-
-			if (min > num) {
-				min = num;
-				minIdx = i;
-			}
 			cal.add(new int[] { num, i });
-		}
-		cal.poll();
-		int ans = max - min;
-
-		while (true) {
-			if (pq[minIdx].isEmpty())
-				break;
-
-			int num = pq[minIdx].poll();
 			max = Math.max(max, num);
-			cal.add(new int[] { num, minIdx });
-			int[] minInfo = cal.poll();
-			min = minInfo[0];
-			minIdx = minInfo[1];
-			ans = Math.min(ans, max - min);
 		}
-		System.out.println(ans);
+		
+		int ans = Integer.MAX_VALUE;
+		
+		while(true) {
+			int[] curr = cal.poll();
+			int min = curr[0];
+			int minIdx = curr[1];
+			ans = Math.min(ans, max - min);
+			
+			if(pq[minIdx].isEmpty()) break;
+			
+			int next = pq[minIdx].poll();
+			max = Math.max(max, next);
+			cal.add(new int[] {next, minIdx});
+		}
+        System.out.println(ans);
 	}
 }
