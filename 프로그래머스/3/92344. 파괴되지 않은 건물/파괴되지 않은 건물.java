01b660a3c15cc6b51b01cf9d2633d4d9c341b398
Kim Minsu <mmsskksw9900@gmail.com>
2025-10-16 11:07:59 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 61.03 ms, Memory: 210 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static int N, M;
+    private static int[][] prefix;
+    
+    public int solution(int[][] board, int[][] skill) {
+        N = board.length;
+        M = board[0].length;
+        
+        prefix = new int[N + 1][M + 1];
+        
+        for(int[] s : skill) {
+            int state = s[0] == 1 ? -1 : 1;
+            int sr = s[1];
+            int sc = s[2];
+            int er = s[3];
+            int ec = s[4];
+            int degree = s[5];
+            
+            prefix[sr][sc] += (state * degree);
+            prefix[er + 1][sc] += ((state * -1) * degree);
+            prefix[sr][ec + 1] += ((state * -1) * degree);
+            prefix[er + 1][ec + 1] += (state * degree);
+        }
+        
+        
+        for(int r = 1; r < N; r++) {
+            for(int c =0; c < M; c++) {
+                prefix[r][c] += prefix[r - 1][c];
+            }
+        }
+        
+        for(int c = 1; c < M; c++) {
+            for(int r =0; r < N; r++) {
+                prefix[r][c] += prefix[r][c - 1];
+            }
+        }
+        
+        int answer = 0;
+        for(int r = 0; r < N; r++) {
+            for(int c = 0; c < M; c++) {
+                board[r][c] += prefix[r][c];
+                if(board[r][c] > 0 ) answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
