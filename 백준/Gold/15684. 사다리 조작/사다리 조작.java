789e9a998ce80085fa1b0a29f4793f280ff5641b
Kim Minsu <mmsskksw9900@gmail.com>
2025-10-24 18:51:59 +0900

    [Gold III] Title: 사다리 조작, Time: 840 ms, Memory: 13860 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -29,15 +29,13 @@ public class Main {
 			info[x][y + 1] = y;
 		}
 
-		for (int i = 0; i <= 3; i++) {
-			if (!isEnd) {
-				addLine(0, i);
-			}
+		for (int i = 0; i <= 3 && !isEnd; i++) {
+			addLine(0, i, 1, 1);
 		}
 		System.out.println(answer);
 	}
 
-	private static void addLine(int depth, int K) {
+	private static void addLine(int depth, int K, int ir, int ic) {
 		if (isEnd)
 			return;
 
@@ -49,15 +47,17 @@ public class Main {
 			return;
 		}
 
-		for (int r = 1; r <= H; r++) {
-			for (int c = 1; c < N; c++) {
+		for (int r = ir; r <= H; r++) {
+
+			int sc = (r == ir) ? ic : 1;
+			for (int c = sc; c < N; c++) {
 				if (info[r][c] > 0 || info[r][c + 1] > 0)
 					continue;
 
 				info[r][c] = c + 1;
 				info[r][c + 1] = c;
 
-				addLine(depth + 1, K);
+				addLine(depth + 1, K, ir, ic + 2);
 
 				info[r][c] = 0;
 				info[r][c + 1] = 0;
@@ -66,7 +66,6 @@ public class Main {
 	}
 
 	private static boolean isPoss() {
-		
 		for (int i = 1; i <= N; i++) {
 			int c = i;
 			for (int r = 1; r <= H; r++) {
