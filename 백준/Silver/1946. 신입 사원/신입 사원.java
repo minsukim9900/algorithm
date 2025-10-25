67d4b6fa3f8025fb299a67369cb9863bda77525f
Kim Minsu <mmsskksw9900@gmail.com>
2025-10-25 19:53:09 +0900

    [Silver I] Title: 신입 사원, Time: 748 ms, Memory: 297896 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -12,21 +12,18 @@ public class Main {
 
 		for (int t = 0; t < T; t++) {
 			int N = Integer.parseInt(br.readLine());
-			int[][] info = new int[N][2];
+			int[] rank = new int[N + 1];
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				info[i][0] = Integer.parseInt(st.nextToken());
-				info[i][1] = Integer.parseInt(st.nextToken());
+				rank[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
 			}
 
-			Arrays.sort(info, (a, b) -> Integer.compare(a[0], b[0]));
 			int count = 1;
-			int min = info[0][1];
-
-			for (int i = 1; i < N; i++) {
-				if (min > info[i][1]) {
-					min = info[i][1];
+			int min = rank[1];
+			for (int i = 2; i <= N; i++) {
+				if (min > rank[i]) {
+					min = rank[i];
 					count++;
 				}
 			}
