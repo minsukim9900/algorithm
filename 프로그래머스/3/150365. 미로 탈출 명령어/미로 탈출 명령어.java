aaf6a0a3604ce39e88daeaa3a8ec42ef259161a2
Kim Minsu <mmsskksw9900@gmail.com>
2025-11-06 08:57:22 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.41 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -2,43 +2,57 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    private static int N, M, K;
     private static boolean isPoss = false;
     private static String answer = "impossible";
     private static int[][] delta = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+    private static char[] ch = {'d', 'l', 'r', 'u'};
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        int distance = Math.abs(x - r) + Math.abs(y - c);
-        if(distance > k || ((k - distance) % 2) == 1)  {
+        int distance = Math.abs(r - x) + Math.abs(c - y);
+        if(distance > k || (k - distance) % 2 == 1) {
             return answer;
         }
         
-        int cr = x;
-        int cc = y;
-        char[] ch = {'d', 'l', 'r', 'u'};
-        StringBuilder sb = new StringBuilder();
+        N = n;
+        M = m;
+        K = k;
+        dfs(x, y, r, c, 0, new StringBuilder());
+        return answer;
+    }
+    
+    private static void dfs(int r, int c, int er, int ec, int depth, StringBuilder sb) {
+        if(depth < K && r == er && c == ec && (K - depth) % 2 == 1) {
+            return;
+        }
         
-        for(int step = 0; step < k; step++) {
+        if(isPoss) return;
+        
+        if(depth == K) {
+            if(r == er && c == ec) {
+                isPoss = true;
+                answer = sb.toString();
+            }
+            return;
+        } else {
             for(int i = 0; i < 4; i++) {
-                int nr = cr + delta[i][0];
-                int nc = cc + delta[i][1];
-                if(!isRange(nr, nc, n, m)) continue;
+                int nr = r + delta[i][0];
+                int nc = c + delta[i][1];
                 
-                int remain = k - step - 1;
-                int md = Math.abs(nr - r) + Math.abs(nc - c);
-                
-                if(md <= remain && ((remain - md) % 2) == 0) {
-                    sb.append(ch[i]);
-                    cr = nr;
-                    cc = nc;
-                    break;
+                if(isRange(nr, nc)) {
+                    int dis = Math.abs(er - nr) + Math.abs(ec - nc);
+                    int remain = K - (depth + 1);
+                    if(dis <= remain && (remain - dis) % 2 == 0) {
+                        dfs(nr, nc, er, ec, depth + 1, sb.append(ch[i]));
+                        break;
+                    }
                 }
             }
         }
-        return sb.toString();
     }
     
     
-    private static boolean isRange(int r, int c, int n, int m) {
-        return r >= 1 && r <= n && c >= 1 && c <= m;
+    private static boolean isRange(int r, int c) {
+        return r >= 1 && r <= N && c >= 1 && c <= M;
     }
 }
