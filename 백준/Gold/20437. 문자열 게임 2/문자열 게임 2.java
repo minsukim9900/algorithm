779b50a491fb4142d1ddeb52082b69754a89fd3f
Kim Minsu <mmsskksw9900@gmail.com>
2025-11-27 16:29:51 +0900

    [Gold V] Title: 문자열 게임 2, Time: 256 ms, Memory: 30900 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int K, min, max;
+	private static int[] count;
+	private static List<Integer>[] arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			init();
+			String str = br.readLine();
+			K = Integer.parseInt(br.readLine());
+
+			for (int i = 0; i < str.length(); i++) {
+				int index = str.charAt(i) - 'a';
+				count[index]++;
+				arr[index].add(i);
+			}
+
+			if (isPoss()) {
+				cal();
+				sb.append(min + " " + max);
+			} else {
+				sb.append(-1);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static void cal() {
+		for (int i = 0; i < 26; i++) {
+			if (arr[i].size() < K)
+				continue;
+			int left = 0;
+			int right = K - 1;
+
+			while (right < arr[i].size()) {
+				min = Math.min(min, arr[i].get(right) - arr[i].get(left) + 1);
+				max = Math.max(max, arr[i].get(right) - arr[i].get(left) + 1);
+				left++;
+				right++;
+			}
+		}
+	}
+
+	private static void init() {
+		min = Integer.MAX_VALUE;
+		max = 0;
+		count = new int['z' - 'a' + 1];
+		arr = new ArrayList['z' - 'a' + 1];
+		for (int i = 0; i < 'z' - 'a' + 1; i++) {
+			arr[i] = new ArrayList<>();
+		}
+	}
+
+	private static boolean isPoss() {
+		for (int w : count) {
+			if (K <= w)
+				return true;
+		}
+		return false;
+	}
+}
