55ac4f5edf834a3a84ed043fc7f6b4b820a9cfe7
Kim Minsu <mmsskksw9900@gmail.com>
2025-12-04 00:32:48 +0900

    [Silver II] Title: 촌수계산, Time: 64 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -2,58 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	private static int N, x, y, M;
-	private static StringBuilder sb = new StringBuilder();
+	private static int N;
+	private static int start, end;
 	private static List<Integer>[] adj;
-	private static boolean[] visited;
-	private static int result = 0;
+	private static final int INF = 100_000_000;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
 		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		x = Integer.parseInt(st.nextToken());
-		y = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(br.readLine());
-		visited = new boolean[N+1];
-		
-		adj = new ArrayList[N+1];
+		st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+
+		adj = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
 			adj[i] = new ArrayList<>();
 		}
 
+		int M = Integer.parseInt(br.readLine());
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			adj[p].add(c);
-			adj[c].add(p);
-		}
-		
-		dfs(x,0);
-		if(result == 0) {
-			System.out.println(-1);
-		}else {
-			System.out.println(result);
-		}
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
+			adj[x].add(y);
+			adj[y].add(x);
+		}
+		System.out.println(bfs());
 	}
 
-	private static void dfs(int num, int depth) {
-		if (num == y) {
-			result = depth;
-		} else {
-			if (visited[num])
-				return;
-			else {
-				visited[num] = true;
-				for (int i = 0; i < adj[num].size(); i++) {
-					dfs(adj[num].get(i), depth+1);
-				}
+	private static int bfs() {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(start);
+		int[] dist = new int[N + 1];
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+
+		while (!q.isEmpty()) {
+			int node = q.poll();
+
+			if (node == end) {
+				return dist[node];
 			}
 
+			for (int next : adj[node]) {
+				if (dist[next] > dist[node] + 1) {
+					dist[next] = dist[node] + 1;
+					q.add(next);
+				}
+			}
 		}
+		return -1;
 	}
 }
