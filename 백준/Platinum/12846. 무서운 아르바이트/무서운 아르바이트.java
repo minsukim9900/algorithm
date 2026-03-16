28cab193c356d7d17c44af86fa28474071639928
Kim Minsu <mmsskksw9900@gmail.com>
2026-03-17 06:51:27 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 280 ms, Memory: 27736 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int N;
+	private static int[] indexTree, nums;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		indexTree = new int[N * 4];
+		nums = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		init(1, 0, N - 1);
+
+		System.out.println(calMoney(0, N - 1));
+
+	}
+
+	private static long calMoney(int queryLeft, int queryRight) {
+		long day = queryRight - queryLeft + 1;
+		int targetIndex = getIndexQuery(1, 0, N - 1, queryLeft, queryRight);
+		int money = nums[targetIndex];
+
+		long result = day * money;
+
+		long leftResult = 0L;
+		long rightResult = 0L;
+
+		if (queryLeft <= targetIndex - 1) {
+			leftResult = calMoney(queryLeft, targetIndex - 1);
+		}
+
+		if (targetIndex + 1 <= queryRight) {
+			rightResult = calMoney(targetIndex + 1, queryRight);
+		}
+
+		return Math.max(result, Math.max(leftResult, rightResult));
+	}
+
+	private static void init(int node, int nodeLeft, int nodeRight) {
+		if (nodeLeft == nodeRight) {
+			indexTree[node] = nodeLeft;
+			return;
+		}
+
+		int mid = (nodeLeft + nodeRight) >> 1;
+		int next = node << 1;
+
+		init(next, nodeLeft, mid);
+		init(next + 1, mid + 1, nodeRight);
+
+		indexTree[node] = nums[indexTree[next]] <= nums[indexTree[next + 1]] ? indexTree[next] : indexTree[next + 1];
+	}
+
+	private static int getIndexQuery(int node, int nodeLeft, int nodeRight, int queryLeft, int queryRight) {
+		if (queryRight < nodeLeft || nodeRight < queryLeft) {
+			return -1;
+		}
+
+		if (queryLeft <= nodeLeft && nodeRight <= queryRight) {
+			return indexTree[node];
+		}
+
+		int mid = (nodeLeft + nodeRight) >> 1;
+		int next = node << 1;
+
+		int leftIndex = getIndexQuery(next, nodeLeft, mid, queryLeft, queryRight);
+		int rightIndex = getIndexQuery(next + 1, mid + 1, nodeRight, queryLeft, queryRight);
+
+		if (leftIndex == -1) {
+			return rightIndex;
+		}
+
+		if (rightIndex == -1) {
+			return leftIndex;
+		}
+
+		return nums[leftIndex] <= nums[rightIndex] ? leftIndex : rightIndex;
+	}
+}
