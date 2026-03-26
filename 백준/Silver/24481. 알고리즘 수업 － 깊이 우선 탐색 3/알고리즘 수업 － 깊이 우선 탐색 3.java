cde2ee5c8e131d85ed90d7ff3f11f8d309195525
Kim Minsu <mmsskksw9900@gmail.com>
2026-03-27 00:26:57 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 956 ms, Memory: 97124 KB -BaekjoonHub

--- 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -2,60 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	private static int N, M, R;
 	private static List<Integer>[] adj;
-	private static int[] visited;
-	private static int cnt = 0;
+	private static int[] result;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 
-		visited = new int[N + 1];
 		adj = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
+		for (int i = 1; i < N + 1; i++) {
 			adj[i] = new ArrayList<>();
 		}
 
-		for (int m = 0; m < M; m++) {
+		result = new int[N + 1];
+		Arrays.fill(result, -1);
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
+
 			adj[from].add(to);
 			adj[to].add(from);
 		}
 
-		for (int i = 1; i <= N; i++) {
-			Collections.sort(adj[i]);
+		for (int i = 1; i < N + 1; i++) {
+			adj[i].sort((a, b) -> Integer.compare(a, b));
 		}
 
-		dfs(R, 0);
+		dfs(R, new boolean[N + 1], 0);
 
-		for (int i = 1; i <= N; i++) {
-			if (i != R && visited[i] == 0) {
-				sb.append(-1).append("\n");
-			} else
-
-			{
-				sb.append(visited[i]).append("\n");
-			}
+		for (int i = 1; i < N + 1; i++) {
+			sb.append(result[i]).append("\n");
 		}
+
 		System.out.println(sb.toString());
 	}
 
-	public static void dfs(int v, int depth) {
-		visited[v] = depth;
+	private static void dfs(int node, boolean[] visited, int depth) {
+		result[node] = depth;
+		visited[node] = true;
 
-		for (int w : adj[v]) {
-			if ( w != R && visited[w] == 0) {
-				dfs(w, depth + 1);
+		for (int next : adj[node]) {
+			if (visited[next]) {
+				continue;
 			}
+
+			dfs(next, visited, depth + 1);
 		}
 	}
-
 }
