dd78612dad34a2afa9e0930658a0ae66550541a3
Kim Minsu <mmsskksw9900@gmail.com>
2026-03-30 19:12:11 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 776 ms, Memory: 74928 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	private static int N, M;
+	private static long[] nums, sumTree, lazyTree;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		nums = new long[N];
+		sumTree = new long[N * 4];
+		lazyTree = new long[N * 4];
+        
+        st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Long.parseLong(st.nextToken());
+		}
+
+		init(1, 0, N - 1);
+        
+        M = Integer.parseInt(br.readLine());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int order = Integer.parseInt(st.nextToken());
+
+            if (order == 1) {
+                int queryLeft = Integer.parseInt(st.nextToken()) - 1;
+	            int queryRight = Integer.parseInt(st.nextToken()) - 1;
+	            long target = Long.parseLong(st.nextToken());
+
+	            updateQuery(1, 0, N - 1, queryLeft, queryRight, target);
+            } else {
+                int index = Integer.parseInt(st.nextToken()) - 1;
+	            sb.append(sumQuery(1, 0, N - 1, index, index)).append("\n");
+            }
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static void init(int node, int nodeLeft, int nodeRight) {
+		if (nodeLeft == nodeRight) {
+			sumTree[node] = nums[nodeLeft];
+			return;
+		}
+
+		int mid = (nodeLeft + nodeRight) >> 1;
+		int next = node << 1;
+
+		init(next, nodeLeft, mid);
+		init(next + 1, mid + 1, nodeRight);
+		sumTree[node] = sumTree[next] + sumTree[next + 1];
+	}
+
+	private static void lazyPropagate(int node, int nodeLeft, int nodeRight) {
+		if (lazyTree[node] == 0) {
+			return;
+		}
+
+		int next = node << 1;
+
+		if (nodeLeft != nodeRight) {
+			lazyTree[next] += lazyTree[node];
+			lazyTree[next + 1] += lazyTree[node];
+		}
+
+		sumTree[node] += lazyTree[node] * (nodeRight - nodeLeft + 1);
+		lazyTree[node] = 0;
+	}
+
+	private static void updateQuery(int node, int nodeLeft, int nodeRight, int queryLeft, int queryRight, long target) {
+		lazyPropagate(node, nodeLeft, nodeRight);
+
+		if (queryRight < nodeLeft || nodeRight < queryLeft) {
+			return;
+		}
+
+		if (queryLeft <= nodeLeft && nodeRight <= queryRight) {
+			lazyTree[node] += target;
+			lazyPropagate(node, nodeLeft, nodeRight);
+			return;
+		}
+
+		int mid = (nodeLeft + nodeRight) >> 1;
+		int next = node << 1;
+
+		updateQuery(next, nodeLeft, mid, queryLeft, queryRight, target);
+		updateQuery(next + 1, mid + 1, nodeRight, queryLeft, queryRight, target);
+
+		sumTree[node] = sumTree[next] + sumTree[next + 1];
+	}
+
+	private static long sumQuery(int node, int nodeLeft, int nodeRight, int queryLeft, int queryRight) {
+		lazyPropagate(node, nodeLeft, nodeRight);
+
+		if (queryRight < nodeLeft || nodeRight < queryLeft) {
+			return 0;
+		}
+
+		if (queryLeft <= nodeLeft && nodeRight <= queryRight) {
+			return sumTree[node];
+		}
+
+		int mid = (nodeLeft + nodeRight) >> 1;
+		int next = node << 1;
+
+		long leftSum = sumQuery(next, nodeLeft, mid, queryLeft, queryRight);
+		long rightSum = sumQuery(next + 1, mid + 1, nodeRight, queryLeft, queryRight);
+
+		return leftSum + rightSum;
+	}
+}
