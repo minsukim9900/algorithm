1dacb8dd268449f81088239b7b23f3c2715e4234
Kim Minsu <mmsskksw9900@gmail.com>
2026-04-08 10:17:45 +0900

    [Gold V] Title: 두 배 더하기, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/12931. 두 배 더하기/두 배 더하기.java
+++ 백준/Gold/12931. 두 배 더하기/두 배 더하기.java
@@ -10,37 +10,17 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[] nums = new int[N];
 		st = new StringTokenizer(br.readLine());
-
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-			sum += nums[i];
-		}
-
-		System.out.println(cal(nums, sum));
-	}
-
-	private static int cal(int[] nums, int sum) {
 		int count = 0;
 
-		while (sum != 0) {
-			sum = 0;
-			for (int i = 0; i < nums.length; i++) {
-				if (nums[i] == 0)
-					continue;
+		int maxLength = 0;
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
 
-				if (nums[i] % 2 == 1) {
-					count++;
-					nums[i]--;
-				}
-
-				nums[i] /= 2;
-				sum += nums[i];
-			}
-			count++;
+			count += Integer.bitCount(num);
+			maxLength = Math.max(maxLength, Integer.toBinaryString(num).length() - 1);
 		}
-		return count == 0 ? count : count - 1;
+		
+		System.out.println(count + maxLength);
 	}
 }
