a9bf1933b19883dc488897e883c8b9e07611083e
Kim Minsu <mmsskksw9900@gmail.com>
2026-04-16 17:45:54 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 13;
+    double b = 0.165;
+
+    cout << fixed;
+
+    cout << a << " * " << b << " = " << (a * b);
+    return 0;
+}
