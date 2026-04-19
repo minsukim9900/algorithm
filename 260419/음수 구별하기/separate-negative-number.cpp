cf61c136a0f085c980c68c08a4929d65b4e051c0
Kim Minsu <mmsskksw9900@gmail.com>
2026-04-19 11:11:54 +0900

    Title: 음수 구별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260419/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    cout << N << "\n";
+
+    if(N < 0) {
+        cout << "minus";
+    }
+
+    return 0;
+}
