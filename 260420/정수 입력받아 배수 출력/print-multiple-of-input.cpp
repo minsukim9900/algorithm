327a7b0592497d87ba185471bec4efba29fcc85a
Kim Minsu <mmsskksw9900@gmail.com>
2026-04-20 17:44:15 +0900

    Title: 정수 입력받아 배수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= 5; i++) {
+        cout << i * N << " ";
+    }
+    return 0;
+}
