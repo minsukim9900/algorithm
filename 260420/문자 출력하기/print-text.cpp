2a0e10ffba738315870db9effa852161c4a27b67
Kim Minsu <mmsskksw9900@gmail.com>
2026-04-20 17:31:43 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/문자 출력하기/print-text.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    char c;
+    cin >> c;
+
+    int i = 0;
+    for(i = 0; i < 8; i++) {
+        cout << c;
+    }
+
+    return 0;
+}
