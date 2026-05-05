1c545ce984c8a9e183ca69c324d1801bc5509977
Kim Minsu <mmsskksw9900@gmail.com>
2026-05-05 17:33:37 +0900

    [level 2] Title: 완전범죄, Time: 0.39 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,29 +1,33 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int K;
-    private static final int INF = 100_000;
-    private static int[][] infos;
     public int solution(int[][] info, int n, int m) {
-        int sum = 0;
-        for(int[] w : info) {
-            sum += w[0];
+        int[][] dp = new int[info.length + 1][m];
+        
+        for(int i = 0; i <= info.length; i++) {
+            Arrays.fill(dp[i], 100_000_000);
         }
         
-        int[] dp = new int[m];
-        for(int[] w : info) {
-            for(int j = m - 1; j >= w[1]; j--) {
-                dp[j] = Math.max(dp[j], dp[j - w[1]] + w[0]);
+        dp[0][0] = 0;
+        
+        for(int i = 1; i < info.length + 1; i++) {
+            int aCost = info[i - 1][0];
+            int bCost = info[i - 1][1];
+            
+            for(int j = 0; j < m; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j] + aCost, dp[i][j]);
+                
+                if(j + bCost < m) {
+                    dp[i][j + bCost] = Math.min(dp[i - 1][j], dp[i][j + bCost]);
+                }
             }
         }
         
-        int save = 0;
-        for(int w : dp) {
-            save = Math.max(save, w);
-        }
         
-        int ans = sum - save;
-        return ans >= n ? -1 : ans;
+        int answer = 100_000_000;
+        for(int j = 0; j < m; j++) {
+            answer = Math.min(dp[info.length][j], answer);
+        }
+        return answer >= n ? -1 : answer;
     }
 }
