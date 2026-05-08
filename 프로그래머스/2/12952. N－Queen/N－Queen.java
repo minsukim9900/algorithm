a02a5b3b235c58ebeea9322979c9fa428219c492
Kim Minsu <mmsskksw9900@gmail.com>
2026-05-08 10:07:13 +0900

    [level 2] Title: N-Queen, Time: 57.31 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,24 +1,33 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    private static int answer;
-    private static int N;
+    private static int fullMask, N, answer;
+    
     public int solution(int n) {
         N = n;
+        fullMask = (1 << (n + 1)) - 1;
+        
         answer = 0;
-        nQueen(0,0,0);
+        simulate(0, 0, 0, 0);
         return answer;
     }
     
-    private static void nQueen(int curr, int diag1, int diag2) {
-        if(curr == (1 << N) - 1) {
+    private static void simulate(
+        int depth, int curr, int leftSide, int rightSide) {
+        if(depth == N) {
             answer++;
             return;
         }
         
-        int possible = ~(curr | diag1 | diag2) & ((1 << N) - 1);
-        while (possible != 0) {
-            int u = possible & -possible;
-            possible -= u;
-            nQueen(curr | u, (diag1 | u) << 1, (diag2 | u) >> 1);
+        for(int i = 0; i < N; i++) {
+            int idx = 1 << i;
+            
+            if((curr & idx) != 0 || (leftSide & idx) != 0 || (rightSide & idx) != 0) {
+                continue;
+            }
+            
+            simulate(depth + 1, curr | idx, (leftSide | idx) << 1, (rightSide | idx) >> 1);
         }
     }
 }
