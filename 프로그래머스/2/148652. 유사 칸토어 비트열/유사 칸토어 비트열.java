38821769b190880b6b4020a8a22be3c196e09b2a
Kim Minsu <mmsskksw9900@gmail.com>
2026-05-26 15:22:12 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 51.36 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,17 +1,28 @@
 class Solution {
     public int solution(int n, long l, long r) {
         int answer = 0;
-        for(long i = l - 1; i < r; i++) {
-            answer += cal(i);
+        
+        for(long num = l - 1; num < r; num++) {
+            boolean flag = true;
+            
+            long x = num;
+            while(x >= 5) {
+                if (x % 5 == 2) {
+                    flag = false;
+                    break;
+                }
+                
+                x /= 5;
+            }
+            
+            if (x == 2) {
+                flag = false;
+            }
+            
+            if(flag) {
+                answer++;
+            }
         }
         return answer;
-    }
-    
-    private static long cal(long x) {
-        if(x % 5 == 2) {
-            return 0;
-        }
-        if(x == 0) return 1;
-        return cal(x / 5);
     }
 }
