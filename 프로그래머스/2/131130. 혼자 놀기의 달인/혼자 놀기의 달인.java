7f6d8d5c8ffd38d06ce55ed644a736be5e468d2c
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-06 12:40:37 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.95 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,41 +1,56 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static boolean[] visited;
+    private static int N;
+    private static List<Integer>[] adj;
     
     public int solution(int[] cards) {
-        visited = new boolean[cards.length + 1];
-        List<Integer> arr = new ArrayList<>();
+        N = cards.length;
         
-        for(int i = 0; i < cards.length; i++) {
-            if(visited[i]) continue;
-            int cnt = dfs(i, cards);
-            arr.add(cnt);
+        adj = new ArrayList[N + 1];
+        for(int i = 1; i < N + 1; i++) {
+            adj[i] = new ArrayList<>();
         }
         
-        Collections.sort(arr);
+        for(int i = 1; i < N + 1; i++) {
+            adj[i].add(cards[i - 1]);
+        }
         
-        int cnt = 0;
-        int answer = 1;
-        for(int i = arr.size() - 1; i >= 0; i--) {
-            answer *= arr.get(i);
-            cnt++;
+        List<Integer> counts = new ArrayList<>();
+        boolean[] visited = new boolean[N + 1];
+        for(int i = 1; i < N + 1; i++) {
+            if(visited[i]) continue;
             
-            if(cnt == 2) {
-                break;
-            }
-        }      
-        return cnt != 2 ? 0 : answer;
+            counts.add(bfs(i, visited));
+        }
+        
+        counts.sort((a, b) -> Integer.compare(b, a));
+        
+        int answer = 0;
+        
+        if(counts.size() > 1) {
+            answer = counts.get(0) * counts.get(1);
+        }
+        return answer;
     }
     
-    private static int dfs(int node, int[] cards) {
-        visited[node] = true;
-        int cnt = 1;
+    private int bfs(int start, boolean[] visited) {
+        visited[start] = true;
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(start);
         
-        if(!visited[cards[node] - 1]) {
-            cnt += dfs(cards[node] - 1, cards);
+        int result = 0;
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            result++;
+            
+            for(int next : adj[curr]) {
+                if(visited[next]) continue;
+                visited[next] = true;
+                q.add(next);
+            }
         }
-        return cnt;
+        
+        return result;
     }
 }
