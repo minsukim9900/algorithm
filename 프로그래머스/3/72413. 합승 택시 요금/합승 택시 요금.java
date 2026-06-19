d16fab781631f234ccd495200569ea3c016b05f6
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-19 13:41:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 18.90 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,54 +1,64 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     private static int N;
+    private static final int INF = 100_000_000;
     private static List<int[]>[] adj;
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
         N = n;
-        adj = new ArrayList[n + 1];
         
-        for(int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+        adj = new ArrayList[N + 1];
+        for(int node = 1; node < N + 1; node++) {
+            adj[node] = new ArrayList<>();
         }
         
-        for(int[] fare : fares) {
-            adj[fare[0]].add(new int[] {fare[1], fare[2]});
-            adj[fare[1]].add(new int[] {fare[0], fare[2]});
+        for(int[] fare :fares) {
+            int nodeX = fare[0];
+            int nodeY = fare[1];
+            int weight = fare[2];
+            
+            adj[nodeX].add(new int[] {nodeY, weight});
+            adj[nodeY].add(new int[] {nodeX, weight});
         }
         
-        int[] start = dijkstra(s);
-        int[] startA = dijkstra(a);
-        int[] startB = dijkstra(b);
+        int[] distS = dijkstra(s);
+        int[] distA = dijkstra(a);
+        int[] distB = dijkstra(b);
         
-        int answer = Integer.MAX_VALUE;
-        for(int i = 1; i<= n; i++) {
-            answer = Math.min(answer, start[i] + startA[i] + startB[i]);
+        int answer = INF;
+        for(int node = 1; node < N + 1; node++) {
+            answer = Math.min(answer, distA[node] + distB[node] + distS[node]);
         }
         
         return answer;
     }
     
-    private static int[] dijkstra(int start) {
+    private int[] dijkstra(int start) {
         int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(dist, INF);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
         dist[start] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.add(new int[] {start, dist[start]});
+        pq.add(new int[] {start, 0});
         
         while(!pq.isEmpty()) {
             int[] curr = pq.poll();
+            int node = curr[0];
+            int weight = curr[1];
             
-            if(dist[curr[0]] != curr[1]) continue;
+            if(dist[node] != weight) {
+                continue;
+            }
             
-            for(int[] next : adj[curr[0]]) {
-                if(dist[next[0]] > dist[curr[0]] + next[1]) {
-                    dist[next[0]] = dist[curr[0]] + next[1];
+            for(int[] next : adj[node]) {
+                if(dist[next[0]] > next[1] + weight) {
+                    dist[next[0]] = next[1] + weight;
                     pq.add(new int[] {next[0], dist[next[0]]});
                 }
             }
         }
+        
         return dist;
     }
-    
 }
