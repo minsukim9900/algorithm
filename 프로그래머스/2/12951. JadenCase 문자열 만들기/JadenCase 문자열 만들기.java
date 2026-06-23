5d051db9c6cc13a481208f72e391de12bff03d61
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-24 00:35:38 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -3,35 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         
-        String[] strs = s.split(" ", -1);
+        char[] c = s.toCharArray();
         
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i].length() == 0) {
-                if (i != strs.length - 1) {
-                    sb.append(" ");
+        boolean flag = true;
+        for(int i = 0; i < c.length; i++) {
+            if (c[i] == ' ') {
+                sb.append(c[i]);
+                flag = true;
+                continue;
+            }
+            
+            if (flag) {
+                if (!(c[i] >= '0' && c[i] <= '9')) {
+                    c[i] &= (~(1 << 5));
                 }
-                continue;
+                sb.append(c[i]);
+                flag = false;
+            } else {
+                sb.append(c[i] |= (1 << 5));
             }
-            
-            char[] temp = strs[i].toCharArray();
-            
-            if(!(temp[0] >= '0' && temp[0] <= '9')) {
-                temp[0] &= (~(1 << 5));
-            }
-            
-            sb.append(temp[0]);
-            
-            for(int j = 1; j < temp.length; j++) {
-                temp[j] |= (1 << 5);
-                sb.append(temp[j]);
-            }
-            
-            if(i == strs.length - 1) {
-                continue;
-            }
-            sb.append(" ");
         }
         
         return sb.toString();
