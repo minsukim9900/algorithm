903406cea6a5550ca605a1dfce653b228f110549
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-25 13:20:58 +0900

    [level 3] Title: 풍선 터트리기, Time: 20.59 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,41 +1,29 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    // 번호가 작은 풍선을 터트리는 행위는 1번만.
-    // 나머지는 큰 풍선이
-    private static int N;
-    private static int[] numsLeft, numsRight;
     public int solution(int[] a) {
-        N = a.length;
-        numsLeft = new int[N];
-        numsRight = new int[N];
+        int N = a.length;
         
+        int[] left = new int[N];
+        int[] right = new int[N];
         
-        numsLeft[0] = a[0];
+        left[0] = a[0];
+        right[N - 1] = a[N - 1];
+        
         for(int i = 1; i < N; i++) {
-            numsLeft[i] = Math.min(numsLeft[i - 1], a[i]);
+            left[i] = Math.min(left[i - 1], a[i]);
+            right[N - 1 - i] = Math.min(right[N - i], a[N - 1 - i]);
         }
         
-        numsRight[N - 1] = a[N - 1];
-        for(int i = N - 2; i >= 0; i--) {
-            numsRight[i] = Math.min(numsRight[i + 1], a[i]);
-        }
+        int answer = 2;
         
-        
-        int answer = 0;
-        
-        for(int i = 0; i < N; i++) {
-            if(i == 0 || i == N - 1) {
-                answer++;
+        for(int i = 1; i < N - 1; i++) {
+            if (left[i] < a[i] && right[i] < a[i]) {
                 continue;
             }
             
-            if(numsLeft[i - 1] > a[i] || numsRight[i + 1] > a[i]) {
-                answer++;
-            }
+            answer++;
         }
-        
         return answer;
     }
 }
