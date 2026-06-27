06538d4c6a68a586f103d1a13642a04466093297
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-27 16:11:55 +0900

    [Passed] 최대 증가 부분 수열 (99ms, 9MB)

---
+++ codetree/trail4/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        dp[0] = nums[0];
+        int idx = 1;
+
+        for(int i = 1; i < N; i++) {
+            if (dp[idx - 1] < nums[i]) {
+                dp[idx++] = nums[i];
+            } else {
+                int sIdx= binarySearch(0, idx, nums[i], dp);
+                dp[sIdx] = nums[i];
+            }
+        }
+
+        System.out.println(idx);
+    }
+
+    private static int binarySearch(int s, int e, int target, int[] nums) {
+        int result = e;
+
+        while(s <= e) {
+            int mid = (s + e) / 2;
+
+            if(nums[mid] >= target) {
+                result = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
