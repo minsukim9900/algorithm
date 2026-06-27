663d586bdd9cf193458dc81c00f2cf6bfafc50e2
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-27 15:01:02 +0900

    [level 3] Title: 거스름돈, Time: 13.65 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,20 +1,25 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     private static final int MOD = 1_000_000_007;
     
     public int solution(int n, int[] money) {
-        int[] dp = new int[n + 1];
+        Arrays.sort(money);
+        int m = money.length;
         
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        for(int i = 0; i < money.length; i++) {
-            for(int j = money[i]; j <= n; j++) {
-                dp[j] = dp[j] + dp[j - money[i]];
+        
+        for (int i = 0; i < m; i++) {
+            int curr = money[i];
+            
+            for(int j = curr; j < n + 1; j++) {
+                dp[j] += dp[j - curr];
                 dp[j] %= MOD;
             }
         }
         
-        return dp[n];
+        int answer = dp[n];
+        return answer;
     }
 }
