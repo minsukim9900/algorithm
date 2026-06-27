978f916c1127f01f8ccd6baea88e99ae2191b8dc
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-28 06:03:39 +0900

    [Passed] 겹치지 않게 선분 고르기 2 (253ms, 15MB)

--- codetree/trail4/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ codetree/trail4/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -23,32 +23,22 @@ public class Main {
             lines[i][1] = x2;
         }
 
-        Arrays.sort(lines, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+        Arrays.sort(lines, (a, b) -> Integer.compare(a[1], b[1]));
     }
 
     private static int cal() {
         int result = 0;
-        int[][] dp = new int[MAX][MAX];
-
+        
+        int end = -1;
         for(int i = 0; i < N; i++) {
             int x1 = lines[i][0];
             int x2 = lines[i][1];
-            dp[x1][x2] = Math.max(dp[x1][x2], 1);
 
-            for(int j = 0; j < i; j++) {
-                int cx1 = lines[j][0];
-                int cx2 = lines[j][1];
-
-                if(cx2 >= x1) {
-                    continue;
-                }
-
-                dp[x1][x2] = Math.max(dp[x1][x2], dp[cx1][cx2] + 1);
+            if(end < x1) {
+                result++;
+                end = x2;
             }
-
-            result = Math.max(result, dp[x1][x2]);
         }
-
         return result;
     }
 
