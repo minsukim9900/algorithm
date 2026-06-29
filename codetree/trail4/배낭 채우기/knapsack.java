0bb038a94e55114782401632b606724e871505ce
Kim Minsu <mmsskksw9900@gmail.com>
2026-06-29 14:15:31 +0900

    [Passed] 배낭 채우기 (112ms, 9MB)

---
+++ codetree/trail4/배낭 채우기/knapsack.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[][] items;
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        items = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            items[i][0] = w;
+            items[i][1] = v;
+        }
+    }
+
+    private static int cal() {
+        int[] dp = new int[M + 1];
+
+        for(int i = 0; i < N; i++) {
+            int w = items[i][0];
+            int v = items[i][1];
+
+            for(int weight = M; weight >= w; weight--) {
+                dp[weight] = Math.max(dp[weight], dp[weight - w] + v);
+            }
+        }
+
+        return dp[M];
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        System.out.println(cal());
+    }
+}
