856472715ef13fff6ca92b297d985859b5961c0b
Kim Minsu <mmsskksw9900@gmail.com>
2026-07-07 15:52:03 +0900

    [Passed] 신전 탐험하기 2 (121ms, 10MB)

---
+++ codetree/trail4/신전 탐험하기 2/explore-temple-2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N;
+    private static int[][] floors;
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        floors = new int[N + 1][3];
+        for(int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int l = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+            floors[i][0] = l;
+            floors[i][1] = m;
+            floors[i][2] = r;
+        }
+    }
+
+    private static int cal() {
+        int result = 0;
+
+        for(int start = 0; start < 3; start++) {
+            int[][] dp = new int[N + 1][3];
+            dp[1][start] = floors[1][start];
+
+            for(int i = 2; i < N + 1; i++) {
+                dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2]) + floors[i][0];
+                dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2]) + floors[i][1];
+                dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][0]) + floors[i][2];
+
+                if(i == N) {
+                    if (start == 0) {
+                        result = Math.max(result, Math.max(dp[N][1], dp[N][2]));
+                    } else if (start == 1) {
+                        result = Math.max(result, Math.max(dp[N][0], dp[N][2]));
+                    } else {
+                        result = Math.max(result, Math.max(dp[N][1], dp[N][0]));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        init();
+
+        System.out.println(cal());
+    }
+}
