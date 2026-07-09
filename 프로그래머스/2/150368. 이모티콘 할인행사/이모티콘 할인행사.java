53d414d38ba38b9321fe48592cadbdc37bc8afa1
Kim Minsu <mmsskksw9900@gmail.com>
2026-07-10 01:45:37 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 20.49 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,78 +1,82 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int[][] prices, usersInfo;
-    private static int min;
-    private static int[] answer = new int[2];
+    private static int N, M;
+    private static int plusCount, salesAmount;
+    private static int[][] users;
+    private static int[] emoticons;
     
-    public int[] solution(int[][] users, int[] emoticons) {
-        usersInfo = users;
-        prices = calEmoticonsPrices(emoticons);
-        min = 40;
+    private static final int[] PERCENTS = {10, 20, 30, 40};
+    
+    public int[] solution(int[][] userInfos, int[] emoticonInfos) {
+        N = userInfos.length;
+        M = emoticonInfos.length;
+        plusCount = 0;
+        salesAmount = 0;
         
-        for(int[] user : users) {
-            min = Math.min(user[0], min);
-        }
+        users = userInfos;
+        emoticons = emoticonInfos;
+        dfs(0, new int[N], new int[N]);
         
-        if(min % 10 > 0) {
-            min = (min / 10) * 10 + 10;
-        }
-        
-        perm(0, new int[emoticons.length]);
+        int[] answer = new int[] {plusCount, salesAmount};
         return answer;
     }
-    private static int[] calPurchasePrice(int[] infos) {
-        int price = 0;
-        int subScribe = 0;
-        
-        for(int i = 0; i < usersInfo.length; i++) {
-            int sum = 0;
+    
+    private void dfs(int depth, int[] userPlus, int[] userAmount) {
+        if(depth == M) {
+            int pCount = 0;
+            int sAmount = 0;
             
-            for(int j = 0; j < infos.length; j++) {
-                if(infos[j] < usersInfo[i][0]) continue;
+            for(int i = 0; i < N; i++) {
+                pCount += userPlus[i];
+                sAmount += userAmount[i];
+            }
+            
+            if(plusCount < pCount || (plusCount == pCount && salesAmount < sAmount)) {
+                plusCount = pCount;
+                salesAmount = sAmount;
+            }
+            
+            return;
+        }
+        
+        int price = emoticons[depth];
+        
+        for(int i = 0; i < 4; i++) {
+            int percent = PERCENTS[i];
+            int productPrice = getDiscountPrice(price, percent);
+            
+            int[] tempPlus = userPlus.clone();
+            int[] tempAmount = userAmount.clone();
+            
+            for(int user = 0; user < N; user++) {
+                int refPercent = users[user][0];
+                int refAmount = users[user][1];
                 
-                sum += prices[j][infos[j] / 10 - 1];
+                
+                if(refPercent > percent) {
+                    continue;
+                }
+                
+                if(userPlus[user] > 0) {
+                    continue;
+                }
+                
+                if (productPrice + userAmount[user] >= refAmount) {
+                    userPlus[user] += 1;
+                    userAmount[user] = 0;
+                }else {
+                    userAmount[user] += productPrice;
+                }
             }
+            dfs(depth + 1, userPlus, userAmount);
             
-            if(sum >= usersInfo[i][1]) {
-                subScribe++;
-                sum = 0;
-            }
-            price += sum;
-        }
-        return new int[] {subScribe, price};
-    }
-    
-    private static void perm(int depth, int[] infos) {
-        if(depth == prices.length) {
-            int[] curr = calPurchasePrice(infos);
-            
-            if(curr[0] > answer[0]) {
-                answer = curr;
-            } else if(curr[0] == answer[0] && curr[1] > answer[1]) {
-                answer = curr;
-            }            
-        } else {
-            for(int i = min; i <= 40; i += 10) {
-                infos[depth] = i;
-                perm(depth + 1, infos);
-            }
+            userPlus = tempPlus;
+            userAmount = tempAmount;
         }
     }
     
-    private static int[][] calEmoticonsPrices(int[] emoticons) {
-        int[][] result = new int[emoticons.length][4];
-        
-        for(int i = 0; i < emoticons.length; i++) {
-            int curr = emoticons[i];
-            
-            for(int j = 10; j <= 40; j += 10) {
-                result[i][j / 10 - 1] = curr - ((curr / 100) * j);
-            }
-        }
-        return result;
+    private int getDiscountPrice(int price, int percent) {
+        return price - (price * percent / 100);
     }
-    
-    
 }
