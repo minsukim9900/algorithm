7c5ea2d2621935078c8b280681ac170ee08c55c6
Kim Minsu <mmsskksw9900@gmail.com>
2026-07-12 14:02:26 +0900

    [level 3] Title: 양과 늑대, Time: 0.23 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,48 +1,58 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int N, answer;
-    private static boolean[] used;
-    private static int[] nums;
-    private static List<Integer>[] adj;
+    private static int N, M;
+    private static int[] animals;
+    private static int[] adj;
     
     public int solution(int[] info, int[][] edges) {
         N = info.length;
-        answer = 0;
-        nums = info;
-        used = new boolean[1 << N];
-        adj = new ArrayList[N];
+        adj = new int[N];
+        animals = info;
         
-        for(int i = 0; i < N; i++) {
-            adj[i] = new ArrayList<>();
+        for(int[] edge : edges) {
+            int x = edge[0];
+            int y = edge[1];
+            
+            adj[x] |= (1 << y);
+            adj[y] |= (1 << x);
         }
         
-        for(int[] e : edges) {
-            adj[e[0]].add(e[1]);
-            adj[e[1]].add(e[0]);
-        }
-        used[1] = true;
-        dfs(1, 1, 0);
+        int answer = dfs(1, new boolean[1 << N]);
         return answer;
     }
     
-    private static void dfs(int mask, int sheep, int wolf) {
-        if(sheep <= wolf) return;
-        answer = Math.max(answer, sheep);
+    private static int dfs(int bitmask, boolean[] visited) {
+        int sheep = 0;
+        int wolf = 0;
         
-        for(int i = 0; i < N; i++) {
-            if((mask & (1 << i)) == 0) continue;
+        for (int i = 0; i < N; i++) {
+            if ((bitmask & (1 << i)) == 0) {
+                continue;
+            }
             
-            for(int v : adj[i]) {
-                if((mask & (1 << v)) != 0) continue;
-                int nmask = mask | (1 << v);
-                int ns = sheep + (nums[v] == 0 ? 1 : 0);
-                int nw = wolf + (nums[v] == 1 ? 1 : 0);
-                
-                if(!used[nmask])
-                    dfs(nmask, ns, nw);
+            if (animals[i] == 0) {
+                sheep++;
+            } else {
+                wolf++;
             }
         }
+        
+        if (sheep <= wolf || visited[bitmask]) {
+            return sheep;
+        }
+        
+        visited[bitmask] = true;
+        int best = sheep;
+        
+        for(int i = 0; i < N; i++) {
+            if((bitmask & (1 << i)) != 0 || (adj[i] & (bitmask)) == 0) {
+                continue;
+            }
+            
+            best = Math.max(best, dfs(bitmask | (1 << i), visited));
+        }
+        
+        return best;
     }
 }
