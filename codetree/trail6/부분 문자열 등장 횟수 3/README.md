35dcd8d1fd0f9e1a5420a90039f905c85f7e542f
Kim Minsu <mmsskksw9900@gmail.com>
2026-07-16 13:46:59 +0900

    [Passed] 부분 문자열 등장 횟수 3 (124ms, 12MB)

---
+++ codetree/trail6/부분 문자열 등장 횟수 3/README.md
@@ -0,0 +1,8 @@
+# [[테스트]부분 문자열 등장 횟수 3](https://www.codetree.ai/trails/complete/curated-cards/test-substring-occurrence-count-3)
+
+| 항목 | 내용 |
+|---|---|
+| 분류 | Trail |
+| 커리큘럼 | [Trail 6 / String / KMP](https://www.codetree.ai/trail-info/intermediate-high/) |
+| 난이도 | 쉬움 |
+| 경험치 | 30 XP |
---
+++ codetree/trail6/부분 문자열 등장 횟수 3/substring-occurrence-count-3.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static String T, P;
+    private static int N, M;
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = "#" + br.readLine();
+        P = "#" + br.readLine();
+        N = T.length();
+        M = P.length();
+    }
+
+    private static int[] getFailure() {
+        int[] f = new int[M];
+        f[0] = -1;
+
+        for (int i = 1; i < M; i++) {
+            int j = f[i - 1];
+
+            while (j >= 0 && P.charAt(j + 1) != P.charAt(i)) {
+                j = f[j];
+            }
+
+            f[i] = j + 1;
+        }
+
+        return f;
+    }
+
+    private static int cal(int[] f) {
+        int result = 0;
+        int j = 0;
+
+        for (int i = 1; i < N; i++) {
+
+            while(j >= 0 && P.charAt(j + 1) != T.charAt(i)) {
+                j = f[j];
+            }
+            
+            j++;
+
+            if (j == M - 1) {
+                result++;
+                i += f[j];
+                j = 0;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        init();
+
+        System.out.println(cal(getFailure()));
+    }
+}
