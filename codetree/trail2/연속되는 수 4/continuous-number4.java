74b94424c45c0e5cf1f833f73bb777aeac0543e0
Kim Minsu <mmsskksw9900@gmail.com>
2026-07-20 00:24:24 +0900

    [Passed] 연속되는 수 4 (92ms, 9MB)

---
+++ codetree/trail2/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N;
+    private static int[] nums;
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    private static int cal() {
+        int answer = 0;
+
+        int count = 1;
+        int pre = nums[0];
+        
+        for (int i = 1; i < N; i++) {
+            int curr = nums[i];
+
+            if (pre >= curr) {
+                answer = Math.max(answer, count);
+                count = 0;
+            }
+
+            count++;
+            pre = curr;
+        }
+
+        answer = Math.max(answer, count);
+
+        return answer;
+    }
+    public static void main(String[] args) throws Exception {
+        init();
+
+        System.out.println(cal());
+    }
+}
