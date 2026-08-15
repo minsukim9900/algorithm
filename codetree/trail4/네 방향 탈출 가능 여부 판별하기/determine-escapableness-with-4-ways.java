93c929f29b19627fda1475a0948270aad52d169b
Kim Minsu <mmsskksw9900@gmail.com>
2026-08-15 15:22:08 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (94ms, 9MB)

---
+++ codetree/trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[][] board;
+
+    private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int c = 0; c < M; c++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                board[r][c] = num;
+            }
+        }
+    }
+
+    private static boolean isRange(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
+    }
+
+    private static int bfs() {
+        boolean[][] visited = new boolean[N][M];
+
+        visited[0][0] = true;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {0, 0});
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            int r = curr[0];
+            int c = curr[1];
+
+            if (r == N - 1 && c == M - 1) {
+                return 1;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + delta[i][0];
+                int nc = c + delta[i][1];
+
+                if (isRange(nr, nc) && board[nr][nc] == 1 && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.add(new int[] {nr, nc});
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        System.out.println(bfs());
+    }
+}
