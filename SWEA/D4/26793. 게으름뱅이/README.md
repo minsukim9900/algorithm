875bfeffa2a49cb81212375ebcdd691f43aa1592
Kim Minsu <mmsskksw9900@gmail.com>
2026-08-20 12:04:17 +0900

    [D4] Title: 게으름뱅이, Time: 1,563 ms, Memory: 166,768 KB -BaekjoonHub

---
+++ SWEA/D4/26793. 게으름뱅이/README.md
@@ -0,0 +1,15 @@
+# [D4] 게으름뱅이 - 26793 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ3XsxFqSCvHBIPV) 
+
+### 성능 요약
+
+메모리: 166,768 KB, 시간: 1,563 ms, 코드길이: 1,215 Bytes
+
+### 제출 일자
+
+2026-08-20 12:04
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/26793. 게으름뱅이/게으름뱅이.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static int totalTestCase, N;
+	private static int[][] infos;
+
+	private static int simulate() {
+		Arrays.sort(infos, (a, b) -> Integer.compare(b[1], a[1]));
+
+		int result = infos[0][1];
+
+		for (int i = 0; i < N; i++) {
+			int deadLine = Math.min(result, infos[i][1]);
+
+			result = deadLine -= infos[i][0];
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		totalTestCase = Integer.parseInt(br.readLine());
+
+		for (int testCase = 1; testCase < totalTestCase + 1; testCase++) {
+			N = Integer.parseInt(br.readLine());
+
+			infos = new int[N][2];
+
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				int d = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(st.nextToken());
+
+				infos[i][0] = d;
+				infos[i][1] = t;
+			}
+
+			sb.append(simulate()).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
