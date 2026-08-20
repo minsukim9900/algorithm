bda61f59628948815e3a45770ee7207eefc6ab75
Kim Minsu <mmsskksw9900@gmail.com>
2026-08-20 12:25:12 +0900

    [D4] Title: [Professional] 운동, Time: 655 ms, Memory: 48,092 KB -BaekjoonHub

---
+++ SWEA/D4/5684. ［Professional］ 운동/README.md
@@ -0,0 +1,15 @@
+# [D4] [Professional] 운동 - 5684 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRxnnah2sDFAUo) 
+
+### 성능 요약
+
+메모리: 48,092 KB, 시간: 655 ms, 코드길이: 1,584 Bytes
+
+### 제출 일자
+
+2026-08-20 12:25
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5684. ［Professional］ 운동/［Professional］ 운동.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static int N, M;
+
+	private static final int INF = 1_000_000_000;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t < T + 1; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			int[][] dist = new int[N + 1][N + 1];
+
+			for (int node = 1; node < N + 1; node++) {
+				Arrays.fill(dist[node], INF);
+			}
+
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+
+				dist[s][e] = Math.min(dist[s][e], c);
+			}
+
+			for (int k = 1; k < N + 1; k++) {
+				for (int i = 1; i < N + 1; i++) {
+					if (i == k) {
+						continue;
+					}
+
+					for (int j = 1; j < N + 1; j++) {
+						if (j == k) {
+							continue;
+						}
+
+						dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+					}
+				}
+			}
+
+			int result = INF;
+
+			for (int node = 1; node < N + 1; node++) {
+				result = Math.min(result, dist[node][node]);
+			}
+
+			sb.append("#").append(t).append(" ").append(result).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
