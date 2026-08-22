f8fb2b18cb4a71f5bb01e4be7ca516b51d74d0ab
Kim Minsu <mmsskksw9900@gmail.com>
2026-08-22 12:42:58 +0900

    [Passed] 마을 구분하기 (248ms, 14MB)

---
+++ codetree/trail4/마을 구분하기/seperate-village.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N;
+    private static int[][] board;
+
+    private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    private static boolean isRange(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
+    }
+
+    private static int bfs(int sr, int sc, boolean[][] visited) {
+        Queue<int[]> q = new ArrayDeque<>();
+        visited[sr][sc] = true;
+
+        int count = 0;
+
+        q.add(new int[] {sr, sc});
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            int r = curr[0];
+            int c = curr[1];
+            
+            count++;
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + delta[i][0];
+                int nc = c + delta[i][1];
+
+                if (isRange(nr, nc) && board[nr][nc] == 1 && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.add(new int[] {nr, nc});
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        board = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int c = 0; c < N; c++) {
+                int state = Integer.parseInt(st.nextToken());
+
+                board[r][c] = state;
+            }
+        }
+
+        boolean[][] visited = new boolean[N][N];
+
+        List<Integer> arr = new ArrayList<>();
+
+        int count = 0;
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (visited[r][c] || board[r][c] == 0) {
+                    continue;
+                }
+
+                arr.add(bfs(r, c, visited));
+                count++;
+            }
+        }
+
+        arr.sort((a, b) -> Integer.compare(a, b));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+
+        for (int num : arr) {
+            sb.append(num).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
