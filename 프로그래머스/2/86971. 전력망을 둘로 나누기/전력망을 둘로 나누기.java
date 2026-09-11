e23a7a8bb2132375a3d1973eab2194335e23ee86
Kim Minsu <mmsskksw9900@gmail.com>
2026-09-11 11:09:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.39 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,66 +1,54 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    
-    private static int[] visited = new int[4];
-    private static ArrayList<Integer>[] adj;
-    private static int min;
+    private static int N;
+    private static int[] count;
+    private static List<Integer>[] adj;
     
     public int solution(int n, int[][] wires) {
+        N = n;
+        count = new int[N + 1];
+        adj = new ArrayList[N + 1];
         
-        adj = new ArrayList[n+1];
-        
-        min = 987654321;
-        
-      
-        for(int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+        for (int node = 1; node < N + 1; node++) {
+            adj[node] = new ArrayList<>();
         }
-
-        for(int i = 0; i < n-1; i++) {
-            adj[wires[i][0]].add(wires[i][1]);
-            adj[wires[i][1]].add(wires[i][0]);
+        
+        for (int i = 0; i < N -1; i++) {
+            int[] wire = wires[i];
+            
+            int from = wire[0];
+            int to = wire[1];
+            
+            adj[from].add(to);
+            adj[to].add(from);
         }
+        
+        dfs(1, new boolean[N + 1]);
+        
+        int answer = Integer.MAX_VALUE;
+        
+        for (int node = 2; node < N + 1; node++) {
+            int diff = Math.abs(count[1] - count[node] * 2);
             
-        dfs(1, n);
-            
-        return min;
+            answer = Math.min(answer, diff);
+        }
+        
+        return answer;
     }
     
-    private static int dfs(int st, int n) {
-        visit(st);
+    private static int dfs(int node, boolean[] visited) {
+        visited[node] = true;
         int cnt = 1;
         
-            
-        for(int c : adj[st]) {
-            
-            if(!isVisit(c)) {
-                visit(c);
-                cnt+= dfs(c, n);
+        for (int next : adj[node]) {
+            if (visited[next]) {
+                continue;
             }
-                
+            
+            cnt += dfs(next, visited);
         }
         
-        min = Math.min(min, Math.abs(cnt - (n - cnt)));
-        
-        return cnt;
-        
+        return count[node] = cnt;
     }
-    
-    private static boolean isVisit(int i) {
-        
-        
-        if((visited[i / 32] & (1 << (i % 32))) == 0) return false;
-        
-        return true;
-        
-    }
-    
-    private static void visit(int i) {
-        
-        visited[i / 32] |= ( 1 << (i % 32));
-        
-    }
-    
 }
