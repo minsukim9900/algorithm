640812a2e9e2d0e131de413d3882c4ba192161ce
Kim Minsu <mmsskksw9900@gmail.com>
2026-09-19 16:48:28 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 1.94 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,58 +1,78 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static int[][] delta = {{-1, 0}, {0,-1}, {1, 0},{0, 1}};
+    private static int N;
+    private static int[][] board;
+    private static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     
-    public int solution(int[][] board) {
-        int answer = simulation(board);
-        return answer;
+    private static final int INF = 1_000_000_000;
+    
+    private static boolean isRange(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
     }
     
-    private int simulation(int[][] board) {
-        int N = board.length;
-        int[][] dist = new int[N][N];
-        for(int i = 0; i < N; i++) {
-            Arrays.fill(dist[i], 987654321);
-        }
-        Queue<int[]> q = new ArrayDeque<>();
+    private static int bfs() {
+        int[][][] dist = new int[4][N][N];
         
-        for(int i = 0; i < 4; i++) {
-            int nr = 0 + delta[i][0];
-            int nc = 0 + delta[i][1];
-            
-            if(isRange(nr, nc, N) && board[nr][nc] != 1) {
-                dist[nr][nc] = 100;
-                q.add(new int[] {nr, nc, 100, i});
+        for (int i = 0; i < 4; i++) {
+            for (int r = 0; r < N; r++) {
+                Arrays.fill(dist[i][r], INF);
             }
         }
         
-        while(!q.isEmpty()) {
+        Queue<int[]> q = new ArrayDeque<>();
+        
+        for (int i = 0; i < 4; i++) {
+            q.add(new int[] {0, 0, 0, i});
+            dist[i][0][0] = 0;
+        }
+        
+        while (!q.isEmpty()) {
             int[] curr = q.poll();
+            
             int r = curr[0];
             int c = curr[1];
-            int w = curr[2];
+            int cost = curr[2];
             int dir = curr[3];
             
-            for(int i = 0; i < 4; i++) {
+            if (dist[dir][r][c] != cost) {
+                continue;
+            }
+            
+            for (int i = 0; i < 4; i++) {
                 int nr = r + delta[i][0];
                 int nc = c + delta[i][1];
                 
-                if(isRange(nr, nc, N) && board[nr][nc] != 1) {
-                    int nw = w + ((dir == i) ? 100 : 600);
-                    if(dist[nr][nc] + 500 >= w) {
-                        if(dist[nr][nc] > nw) {
-                            dist[nr][nc] = nw;
+                if (isRange(nr, nc) && board[nr][nc] != 1) {
+                    if (i == dir) {
+                       if (dist[i][nr][nc] > dist[dir][r][c] + 1) {
+                           dist[i][nr][nc] = dist[dir][r][c] + 1;
+                           q.add(new int[] {nr, nc, dist[i][nr][nc], i});
+                       }
+                    } else {
+                        if (dist[i][nr][nc] > dist[dir][r][c] + 6) {
+                            dist[i][nr][nc] = dist[dir][r][c] + 6;
+                            q.add(new int[] {nr, nc, dist[i][nr][nc], i});
                         }
-                        q.add(new int[] {nr, nc, nw, i});
                     }
                 }
             }
         }
-        return dist[N - 1][N - 1];
+        
+        int answer = INF;
+        
+        for (int i = 0; i < 4; i++) {
+            answer = Math.min(answer, dist[i][N - 1][N - 1]);
+        }
+        
+        return answer * 100;
     }
     
-    private static boolean isRange(int r, int c, int N) {
-        return r >= 0 && r < N && c >= 0 && c < N;
+    public int solution(int[][] b) {
+        N = b.length;
+        board = b;
+        
+        int answer = bfs();
+        return answer;
     }
 }
