ee64ca996c5839faaadf537e9ab09bd80d8670bd
Kim Minsu <mmsskksw9900@gmail.com>
2026-09-23 10:52:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 19.71 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,58 +1,45 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private static List<Integer>[] adj;
-    private static Map<String, Integer> map;
-    private static int N;
-    private static int[] count;
-    
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        N = enroll.length + 1;
-        adj = new ArrayList[N + 1];
-        
-        map = new HashMap<>();
-        
-        for(int i = 0; i <= N; i++) {
-            adj[i] = new ArrayList<>();
+
+    public int[] solution(String[] enroll, String[] referral,
+                          String[] seller, int[] amount) {
+
+        int n = enroll.length;
+
+        Map<String, Integer> map = new HashMap<>();
+        int[] parent = new int[n];
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            map.put(enroll[i], i);
         }
-        
-        int idx = 1;
-        
-        for(String name : enroll) {
-            map.put(name, idx++);
-        }
-        
-        for(int i = 1; i < N; i++) {
-            if(referral[i - 1].equals("-")) {
-                adj[i].add(0);
-            }else {
-                int p = map.get(referral[i - 1]);
-                adj[i].add(p);
+
+        for (int i = 0; i < n; i++) {
+            if (referral[i].equals("-")) {
+                parent[i] = -1;
+            } else {
+                parent[i] = map.get(referral[i]);
             }
         }
-        
-        count = new int[N];
-        for(int i = 0; i < seller.length; i++) {
-            int index = map.get(seller[i]);
-            dfs(index, amount[i] * 100);
-        }
-        
-        int[] answer = new int[N - 1];
-        for(int i = 1; i < N; i++) {
-            answer[i - 1] = count[i];
-        }
-        return answer;
-    }
-    
-    private static void dfs(int node, int amount) {
-        count[node] += amount;
-        
-        for(int next : adj[node]) {
-            if(amount * 10 / 100 > 0) {
-                dfs(next, amount * 10 / 100);
-                count[node] -= (amount * 10 / 100);
+
+        for (int i = 0; i < seller.length; i++) {
+
+            int current = map.get(seller[i]);
+            int money = amount[i] * 100;
+
+            while (current != -1 && money > 0) {
+
+                int commission = money / 10;
+                int myMoney = money - commission;
+
+                result[current] += myMoney;
+
+                current = parent[current];
+                money = commission;
             }
         }
+
+        return result;
     }
 }
