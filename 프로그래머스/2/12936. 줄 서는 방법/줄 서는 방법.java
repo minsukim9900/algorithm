2bddbc2bf398bfeb2fba98732a40224ecc9d967b
Kim Minsu <mmsskksw9900@gmail.com>
2026-09-25 07:49:10 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+            numbers.add(i);
+        }
+
+        k--;
+
+        for (int i = 0; i < n; i++) {
+
+            factorial /= (n - i);
+
+            int index = (int) (k / factorial);
+
+            answer[i] = numbers.remove(index);
+
+            k %= factorial;
+        }
+
+        return answer;
+    }
+}
